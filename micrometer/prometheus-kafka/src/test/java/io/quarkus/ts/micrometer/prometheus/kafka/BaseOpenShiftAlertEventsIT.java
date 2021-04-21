b6605432b87ac73fbf9a41607a0bdd4ca5bac4bb
Jose <josecarvajalhilario@gmail.com>
2021-04-21 16:26:20 +0200

    Migrated micrometer modules
    | micrometer/prometheus | micrometer/prometheus |
    | micrometer/prometheus-kafka | micrometer/prometheus-kafka |

---
+++ micrometer/prometheus-kafka/src/test/java/io/quarkus/ts/micrometer/prometheus/kafka/BaseOpenShiftAlertEventsIT.java
@@ -0,0 +1,132 @@
+package io.quarkus.ts.micrometer.prometheus.kafka;
+
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
+
+import javax.inject.Inject;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.sse.SseEventSource;
+
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.bootstrap.RestService;
+import io.quarkus.test.bootstrap.Service;
+import io.quarkus.test.bootstrap.inject.OpenShiftClient;
+
+public abstract class BaseOpenShiftAlertEventsIT {
+
+    static final String PATH = "/monitor/stream";
+    static final String KAFKA_CONSUMER_COUNT_METRIC = "kafka_consumer_response_total";
+    static final String KAFKA_PRODUCER_COUNT_METRIC = "kafka_producer_response_total";
+    static final int WAIT_FOR_ALERTS_COUNT = 1;
+
+    static final String PROMETHEUS_NAMESPACE = "openshift-user-workload-monitoring";
+    static final String PROMETHEUS_POD = "prometheus-user-workload-0";
+    static final String PROMETHEUS_CONTAINER = "prometheus";
+    static final int ASSERT_PROMETHEUS_TIMEOUT_MINUTES = 5;
+    static final int ASSERT_SERVICE_TIMEOUT_MINUTES = 1;
+
+    static final int TIMEOUT_SEC = 25;
+
+    @Inject
+    static OpenShiftClient client;
+
+    private List<String> receive = new CopyOnWriteArrayList<>();
+
+    @Test
+    public void testAlertMonitorEventStream() throws Exception {
+        whenWaitUntilReceiveSomeAlerts();
+        thenExpectedAlertsHaveBeenConsumed();
+        thenKafkaProducerMetricsAreFound();
+        thenKafkaConsumerMetricsAreFound();
+    }
+
+    protected abstract RestService getApp();
+
+    private void whenWaitUntilReceiveSomeAlerts() throws InterruptedException {
+        final CountDownLatch latch = new CountDownLatch(WAIT_FOR_ALERTS_COUNT);
+
+        WebTarget target = ClientBuilder.newClient().target(getApp().getHost() + ":" + getApp().getPort() + PATH);
+        SseEventSource source = SseEventSource.target(target).build();
+        source.register(inboundSseEvent -> {
+            receive.add(inboundSseEvent.readData(String.class, MediaType.APPLICATION_JSON_TYPE));
+            latch.countDown();
+        });
+
+        source.open();
+        latch.await(TIMEOUT_SEC, TimeUnit.SECONDS);
+        source.close();
+    }
+
+    private void thenExpectedAlertsHaveBeenConsumed() {
+        assertEquals(WAIT_FOR_ALERTS_COUNT, receive.size(), "Unexpected number of alerts consumed");
+    }
+
+    private void thenKafkaProducerMetricsAreFound() throws Exception {
+        thenMetricIsExposedInServiceEndpoint(KAFKA_PRODUCER_COUNT_METRIC, greater(0));
+        thenMetricIsExposedInPrometheus(KAFKA_PRODUCER_COUNT_METRIC, any());
+    }
+
+    private void thenKafkaConsumerMetricsAreFound() throws Exception {
+        thenMetricIsExposedInServiceEndpoint(KAFKA_CONSUMER_COUNT_METRIC, greater(0));
+        thenMetricIsExposedInPrometheus(KAFKA_CONSUMER_COUNT_METRIC, any());
+    }
+
+    private void thenMetricIsExposedInPrometheus(String name, Predicate<String> valueMatcher) throws Exception {
+        await().ignoreExceptions().atMost(ASSERT_PROMETHEUS_TIMEOUT_MINUTES, TimeUnit.MINUTES).untilAsserted(() -> {
+            String output = client.execOnPod(PROMETHEUS_NAMESPACE, PROMETHEUS_POD, PROMETHEUS_CONTAINER, "curl",
+                    "http://localhost:9090/api/v1/query?query=" + name);
+
+            assertTrue(output.contains("\"status\":\"success\""), "Verify the status was ok");
+            assertTrue(output.contains("\"__name__\":\"" + name + "\""), "Verify the metrics is found");
+            assertTrue(valueMatcher.test(output), "Verify the metrics contains the correct number");
+        });
+    }
+
+    private void thenMetricIsExposedInServiceEndpoint(String name, Predicate<Double> valueMatcher) {
+        await().ignoreExceptions().atMost(ASSERT_SERVICE_TIMEOUT_MINUTES, TimeUnit.MINUTES).untilAsserted(() -> {
+            String response = getApp().given().get("/q/metrics").then()
+                    .statusCode(HttpStatus.SC_OK)
+                    .extract().asString();
+
+            boolean matches = false;
+            for (String line : response.split("[\r\n]+")) {
+                if (line.startsWith(name)) {
+                    Double value = extractValueFromMetric(line);
+                    assertTrue(valueMatcher.test(value), "Metric value is not expected. Found: " + value);
+                    matches = true;
+                    break;
+                }
+            }
+
+            assertTrue(matches, "Metric " + name + " not found in " + response);
+        });
+    }
+
+    private <T> Predicate<T> any() {
+        return actual -> true;
+    }
+
+    private Predicate<Double> greater(double expected) {
+        return actual -> actual > expected;
+    }
+
+    private Double extractValueFromMetric(String line) {
+        return Double.parseDouble(line.substring(line.lastIndexOf(" ")));
+    }
+
+    protected static void loadServiceMonitor(Service app) {
+        client.apply(app, Paths.get("target/test-classes/service-monitor.yaml"));
+    }
+}
---
+++ micrometer/prometheus/src/test/java/io/quarkus/ts/micrometer/prometheus/PrimeNumberResourceOpenShiftIT.java
@@ -0,0 +1,117 @@
+package io.quarkus.ts.micrometer.prometheus;
+
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.bootstrap.RestService;
+import io.quarkus.test.bootstrap.Service;
+import io.quarkus.test.bootstrap.inject.OpenShiftClient;
+import io.quarkus.test.scenarios.OpenShiftDeploymentStrategy;
+import io.quarkus.test.scenarios.OpenShiftScenario;
+import io.quarkus.test.scenarios.annotations.EnabledIfOpenShiftScenarioPropertyIsTrue;
+import io.quarkus.test.services.QuarkusApplication;
+
+/**
+ * The application contains a `PrimeNumberResource` resource that generates a few metrics:
+ * - `prime_number_max_{uniqueId}`: max prime number that is found.
+ * - `prime_number_test_{uniqueId}`: with information about the calculation of the prime number.
+ */
+@OpenShiftScenario(deployment = OpenShiftDeploymentStrategy.UsingOpenShiftExtension)
+@EnabledIfOpenShiftScenarioPropertyIsTrue
+public class PrimeNumberResourceOpenShiftIT {
+
+    static final String PRIME_NUMBER_MAX = "prime_number_max_%s";
+
+    static final String PRIME_NUMBER_TEST_COUNT = "prime_number_test_%s_seconds_count";
+    static final String PRIME_NUMBER_TEST_MAX = "prime_number_test_%s_seconds_max";
+    static final String PRIME_NUMBER_TEST_SUM = "prime_number_test_%s_seconds_sum";
+
+    static final String PROMETHEUS_NAMESPACE = "openshift-user-workload-monitoring";
+    static final String PROMETHEUS_POD = "prometheus-user-workload-0";
+    static final String PROMETHEUS_CONTAINER = "prometheus";
+
+    static final int ASSERT_PROMETHEUS_TIMEOUT_MINUTES = 5;
+    static final int ASSERT_SERVICE_TIMEOUT_MINUTES = 1;
+    static final int THREE = 3;
+    static final int FOUR = 4;
+    static final Integer ANY_VALUE = null;
+
+    @QuarkusApplication
+    static RestService app = new RestService().onPostStart(PrimeNumberResourceOpenShiftIT::loadServiceMonitor);
+
+    @Inject
+    static OpenShiftClient client;
+
+    private String uniqueId;
+
+    @BeforeEach
+    public void setup() {
+        uniqueId = app.given().get("/uniqueId").then().statusCode(HttpStatus.SC_OK).extract().asString();
+    }
+
+    @Test
+    public void primeNumberCustomMetricsShouldBeExposed() throws Exception {
+        whenCheckPrimeNumber(THREE); // It's prime, so it should set the prime.number.max metric.
+        whenCheckPrimeNumber(FOUR); // It's not prime, so It's ignored.
+
+        thenMetricIsExposedInServiceEndpoint(PRIME_NUMBER_MAX, THREE);
+        thenMetricIsExposedInServiceEndpoint(PRIME_NUMBER_TEST_COUNT, 1);
+        thenMetricIsExposedInServiceEndpoint(PRIME_NUMBER_TEST_MAX, ANY_VALUE);
+        thenMetricIsExposedInServiceEndpoint(PRIME_NUMBER_TEST_SUM, ANY_VALUE);
+
+        thenMetricIsExposedInPrometheus(PRIME_NUMBER_MAX, THREE);
+        thenMetricIsExposedInPrometheus(PRIME_NUMBER_TEST_COUNT, 1);
+        thenMetricIsExposedInPrometheus(PRIME_NUMBER_TEST_MAX, ANY_VALUE);
+        thenMetricIsExposedInPrometheus(PRIME_NUMBER_TEST_SUM, ANY_VALUE);
+    }
+
+    private void whenCheckPrimeNumber(int number) {
+        app.given().get("/check/" + number).then().statusCode(HttpStatus.SC_OK);
+    }
+
+    private void thenMetricIsExposedInPrometheus(String name, Integer expected) throws Exception {
+        await().ignoreExceptions().atMost(ASSERT_PROMETHEUS_TIMEOUT_MINUTES, TimeUnit.MINUTES).untilAsserted(() -> {
+            String output = client.execOnPod(PROMETHEUS_NAMESPACE, PROMETHEUS_POD, PROMETHEUS_CONTAINER, "curl",
+                    "http://localhost:9090/api/v1/query?query=" + primeNumberCustomMetricName(name));
+
+            assertTrue(output.contains("\"status\":\"success\""), "Verify the status was ok");
+            assertTrue(output.contains("\"__name__\":\"" + primeNumberCustomMetricName(name) + "\""),
+                    "Verify the metrics is found");
+            if (expected != null) {
+                assertTrue(output.contains("\"" + expected + "\""), "Verify the metrics contains the correct number");
+            }
+
+        });
+    }
+
+    private void thenMetricIsExposedInServiceEndpoint(String name, Integer expected) {
+        await().ignoreExceptions().atMost(ASSERT_SERVICE_TIMEOUT_MINUTES, TimeUnit.MINUTES).untilAsserted(() -> {
+            String shouldContain = primeNumberCustomMetricName(name);
+            if (expected != null) {
+                shouldContain += " " + expected;
+            }
+
+            app.given().get("/q/metrics").then()
+                    .statusCode(HttpStatus.SC_OK)
+                    .body(containsString(shouldContain));
+        });
+    }
+
+    private String primeNumberCustomMetricName(String metricName) {
+        return String.format(metricName, uniqueId);
+    }
+
+    private static void loadServiceMonitor(Service app) {
+        client.apply(app, Paths.get("target/test-classes/service-monitor.yaml"));
+    }
+}
