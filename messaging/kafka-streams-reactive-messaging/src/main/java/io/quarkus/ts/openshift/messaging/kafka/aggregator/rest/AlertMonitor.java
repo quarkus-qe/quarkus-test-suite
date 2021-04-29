e332e416785ce119df83a9397625a38ed0fb0dcb
Jose <josecarvajalhilario@gmail.com>
2021-04-29 11:23:25 +0200

    Make Kafka examples more resillient
    - Use drop messages instead of buffer to avoid `java.util.ConcurrentModificationException`.
    - Send event every 1 sec instead of 100 millis in Kafka to not stress the service.
    - Change Kafka Streams condition to assert the events, not the lock
    - Change List by AtomicInteger
    - Overall refactor solution

--- messaging/kafka-streams-reactive-messaging/src/main/java/io/quarkus/ts/openshift/messaging/kafka/aggregator/rest/AlertMonitor.java
+++ messaging/kafka-streams-reactive-messaging/src/main/java/io/quarkus/ts/openshift/messaging/kafka/aggregator/rest/AlertMonitor.java
@@ -1,27 +1,23 @@
 package io.quarkus.ts.openshift.messaging.kafka.aggregator.rest;
 
-import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.jboss.resteasy.annotations.SseElementType;
-import org.reactivestreams.Publisher;
-
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.jboss.resteasy.annotations.SseElementType;
+import org.reactivestreams.Publisher;
+
+import io.quarkus.ts.openshift.messaging.kafka.aggregator.streams.WindowedLoginDeniedStream;
 
 @Path("/")
 public class AlertMonitor {
 
     @Inject
-    @Channel("login-alerts")
+    @Channel(WindowedLoginDeniedStream.LOGIN_ALERTS_TOPIC)
     Publisher<String> alerts;
-
-    @GET
-    public Response ok() {
-        return Response.ok().build();
-    }
 
     @GET
     @Path("/monitor/stream")
