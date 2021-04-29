3a416b304e0628d90bf5ec016499925eaaa389a8
Jose <josecarvajalhilario@gmail.com>
2021-04-29 14:00:07 +0200

    Refactor QPID project to make it more resillient

---
+++ messaging/qpid/src/main/java/io/quarkus/ts/messaging/qpid/PriceConsumer.java
@@ -0,0 +1,37 @@
+package io.quarkus.ts.messaging.qpid;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
+import javax.jms.Message;
+import javax.jms.Session;
+
+/**
+ * A bean consuming prices from the JMS queue.
+ * https://quarkus.io/guides/jms#qpid-jms-amqp.
+ */
+@ApplicationScoped
+public class PriceConsumer {
+
+    @Inject
+    ConnectionFactory connectionFactory;
+
+    public String getLastPrice() {
+        try (JMSContext context = connectionFactory.createContext(Session.AUTO_ACKNOWLEDGE);
+                JMSConsumer consumer = context.createConsumer(context.createTopic("prices"))) {
+            while (true) {
+                Message message = consumer.receive();
+                if (message == null) {
+                    // receive returns `null` if the JMSConsumer is closed
+                    return "";
+                }
+
+                return message.getBody(String.class);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
