a57b7f7aa94cebdc42eecc3589dd41be4e5eb600
Jose <josecarvajalhilario@gmail.com>
2021-04-30 08:31:03 +0200

    Disable H2 Sql Test in Native

--- sql-db/sql-app/src/test/java/io/quarkus/ts/openshift/sqldb/H2SqlDatabaseIT.java
+++ sql-db/sql-app/src/test/java/io/quarkus/ts/openshift/sqldb/H2SqlDatabaseIT.java
@@ -1,10 +1,13 @@
 package io.quarkus.ts.openshift.sqldb;
 
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+
 import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
 import io.quarkus.test.services.QuarkusApplication;
 
 @QuarkusScenario
+@DisabledIfSystemProperty(named = "quarkus.package.type", matches = "native", disabledReason = "H2 database compiled into a native-image is only functional as a client")
 public class H2SqlDatabaseIT extends AbstractSqlDatabaseIT {
 
     @QuarkusApplication
