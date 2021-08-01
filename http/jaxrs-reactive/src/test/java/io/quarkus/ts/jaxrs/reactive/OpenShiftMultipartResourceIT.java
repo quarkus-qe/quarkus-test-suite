4c2b1bfebd55416963291c322ebbcc719140a72f
Josef Smrcka <jsmrcka@redhat.com>
2021-08-01 08:25:39 +0200

    Add RESTEasy Reactive JAX-RS scenario
    Add `http/jaxrs-reactive`, which is a RESTEasy reactive equivalent of
    `http/jaxrs`.
    Notable differences and limitations:
    - In a multipart POJO, Java type for `image` and `octet-stream` were
      changed from `InputStream` to `File` - `InputStream` is not supported
      by `resteasy-reactive`.

---
+++ http/jaxrs-reactive/src/test/java/io/quarkus/ts/jaxrs/reactive/OpenShiftMultipartResourceIT.java
@@ -0,0 +1,7 @@
+package io.quarkus.ts.jaxrs.reactive;
+
+import io.quarkus.test.scenarios.OpenShiftScenario;
+
+@OpenShiftScenario
+public class OpenShiftMultipartResourceIT extends MultipartResourceIT {
+}
