e46a1c2ecbaa5c1c0beb2ed94041366ef3898e4f
Jose <josecarvajalhilario@gmail.com>
2021-08-11 12:45:55 +0200

    Add module to covers `quarkus-logging-json` dependency
    Module that covers the logging functionality using JBoss Logging Manager. The following scenarios are covered:
    - Usage of `quarkus-logging-json` extension
    - Inject the `Logger` instance in beans
    - Inject a `Logger` instance using a custom category
    - Setting up the log level property for logger instances

---
+++ logging/jboss/src/main/java/io/quarkus/ts/logging/jboss/LogResource.java
@@ -0,0 +1,48 @@
+package io.quarkus.ts.logging.jboss;
+
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+
+import org.jboss.logging.Logger;
+
+import io.quarkus.arc.log.LoggerName;
+
+@Path("/log")
+public class LogResource {
+
+    public static final String CUSTOM_CATEGORY = "foo";
+
+    private static final Logger LOG = Logger.getLogger(LogResource.class);
+
+    @Inject
+    Logger log;
+
+    @LoggerName(CUSTOM_CATEGORY)
+    Logger customCategoryLog;
+
+    @POST
+    @Path("/static/{level}")
+    public void addLogMessageInStaticLogger(@PathParam("level") String level, @QueryParam("message") String message) {
+        addLogMessage(LOG, level, message);
+    }
+
+    @POST
+    @Path("/field/{level}")
+    public void addLogMessageInFieldLogger(@PathParam("level") String level, @QueryParam("message") String message) {
+        addLogMessage(log, level, message);
+    }
+
+    @POST
+    @Path("/field-with-custom-category/{level}")
+    public void addLogMessageInFieldWithCustomCategoryLogger(@PathParam("level") String level,
+            @QueryParam("message") String message) {
+        addLogMessage(customCategoryLog, level, message);
+    }
+
+    private void addLogMessage(Logger logger, String level, String message) {
+        logger.log(Logger.Level.valueOf(level), message);
+    }
+}
