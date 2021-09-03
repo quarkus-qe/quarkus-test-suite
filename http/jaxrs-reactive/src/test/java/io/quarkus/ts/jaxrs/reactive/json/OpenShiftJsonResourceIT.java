986bf00192baa2c850598938fe004a31d91d94a1
Josef Smrcka <jsmrcka@redhat.com>
2021-09-03 08:41:24 +0200

    Add JSON serialization scenario
    Part of test development to QUARKUS-1075.
    Covers one of the advanced topics of the test plan.
    https://github.com/quarkus-qe/quarkus-test-plans/blob/main/QUARKUS-1075.md

---
+++ http/jaxrs-reactive/src/test/java/io/quarkus/ts/jaxrs/reactive/json/OpenShiftJsonResourceIT.java
@@ -0,0 +1,7 @@
+package io.quarkus.ts.jaxrs.reactive.json;
+
+import io.quarkus.test.scenarios.OpenShiftScenario;
+
+@OpenShiftScenario
+public class OpenShiftJsonResourceIT extends JsonResourceIT {
+}
