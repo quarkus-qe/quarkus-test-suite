5b957338a10510f40de9a9e1a8f714741df2c043
Josef Smrcka <jsmrcka@redhat.com>
2021-09-08 09:36:51 +0200

    Add MongoDB scenarios
    Adds 2 scenarios:
    - `nosql-db/mongodb`
    - `nosql-db/mongodb-reactive` - reactive equivalent of the prevoius one
    
    Part of test development for QUARKUS-1061.

---
+++ nosql-db/mongodb/src/main/java/io/quarkus/ts/nosqldb/mongodb/AbstractCodecMongoDao.java
@@ -0,0 +1,36 @@
+package io.quarkus.ts.nosqldb.mongodb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.bson.conversions.Bson;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+
+public abstract class AbstractCodecMongoDao<E> implements MongoDaoInterface {
+
+    @Inject
+    MongoClient mongoClient;
+
+    protected List<E> find(String collection, Bson filter, Bson projection, Class<E> clazz) {
+        List<E> list = new ArrayList<>();
+        try (MongoCursor<E> cursor = getCollection(collection, clazz).find(filter).projection(projection).iterator()) {
+            while (cursor.hasNext()) {
+                list.add(cursor.next());
+            }
+        }
+        return list;
+    }
+
+    protected void add(String collection, Class<E> clazz, E document) {
+        getCollection(collection, clazz).insertOne(document);
+    }
+
+    private MongoCollection<E> getCollection(String collection, Class<E> clazz) {
+        return mongoClient.getDatabase(FRUIT_DB_NAME).getCollection(collection, clazz);
+    }
+}
---
+++ nosql-db/mongodb/src/test/java/io/quarkus/ts/nosqldb/mongodb/OpenShiftMongoDbIT.java
@@ -0,0 +1,7 @@
+package io.quarkus.ts.nosqldb.mongodb;
+
+import io.quarkus.test.scenarios.OpenShiftScenario;
+
+@OpenShiftScenario
+public class OpenShiftMongoDbIT extends MongoDbIT {
+}
