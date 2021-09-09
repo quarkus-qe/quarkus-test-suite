ab4d29a8195c31c6bfcafead37ee4b8e49ac3307
Josef Smrcka <jsmrcka@redhat.com>
2021-09-09 10:53:23 +0200

    Add Oracle DB scenario
    Part of test development for QUARKUS-1164.

---
+++ sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/OracleDatabaseIT.java
@@ -0,0 +1,25 @@
+package io.quarkus.ts.sqldb.sqlapp;
+
+import io.quarkus.test.bootstrap.OracleService;
+import io.quarkus.test.bootstrap.RestService;
+import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.scenarios.annotations.DisabledOnNative;
+import io.quarkus.test.services.Container;
+import io.quarkus.test.services.QuarkusApplication;
+
+// TODO: https://github.com/quarkusio/quarkus/issues/19055, see related info in pom.xml (sqldb-sqlapp)
+@DisabledOnNative(reason = "https://github.com/quarkusio/quarkus/issues/19055")
+@QuarkusScenario
+public class OracleDatabaseIT extends AbstractSqlDatabaseIT {
+
+    static final int ORACLE_PORT = 1521;
+
+    @Container(image = "${oracle.image}", port = ORACLE_PORT, expectedLog = "DATABASE IS READY TO USE!")
+    static OracleService database = new OracleService();
+
+    @QuarkusApplication
+    static RestService app = new RestService().withProperties("oracle.properties")
+            .withProperty("quarkus.datasource.username", database.getUser())
+            .withProperty("quarkus.datasource.password", database.getPassword())
+            .withProperty("quarkus.datasource.jdbc.url", database::getJdbcUrl);
+}
