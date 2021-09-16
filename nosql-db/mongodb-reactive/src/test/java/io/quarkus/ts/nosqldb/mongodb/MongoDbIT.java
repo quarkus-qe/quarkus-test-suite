d8d2844b8b43a1df51acaffc3bf4f7ccc86101fe
Josef Smrcka <jsmrcka@redhat.com>
2021-09-16 14:38:27 +0200

    Use Mutiny in reactive MongoDB tests

--- nosql-db/mongodb-reactive/src/test/java/io/quarkus/ts/nosqldb/mongodb/MongoDbIT.java
+++ nosql-db/mongodb-reactive/src/test/java/io/quarkus/ts/nosqldb/mongodb/MongoDbIT.java
@@ -1,6 +1,7 @@
 package io.quarkus.ts.nosqldb.mongodb;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.http.HttpStatus;
 import org.assertj.core.api.Assertions;
@@ -12,9 +13,13 @@ import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
 import io.quarkus.test.services.Container;
 import io.quarkus.test.services.QuarkusApplication;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.path.json.JsonPath;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.helpers.test.UniAssertSubscriber;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.Json;
+import io.vertx.mutiny.core.buffer.Buffer;
+import io.vertx.mutiny.ext.web.client.HttpRequest;
+import io.vertx.mutiny.ext.web.client.HttpResponse;
 
 @QuarkusScenario
 public class MongoDbIT {
@@ -32,19 +37,16 @@ public class MongoDbIT {
         final Fruit fruit1 = new Fruit("fruit1", "fruit description 1");
         final Fruit fruit2 = new Fruit("fruit2", "fruit description 2");
 
-        List<Fruit> fruits1 = postFruit(path, fruit1);
+        List<Fruit> fruits1 = postEntity(path, fruit1, Fruit.class);
         Assertions.assertThat(fruits1).isNotNull();
         Assertions.assertThat(fruits1.size()).isEqualTo(1);
         Assertions.assertThat(fruits1).contains(fruit1);
 
-        fruits1 = postFruit(path, fruit2);
+        fruits1 = postEntity(path, fruit2, Fruit.class);
         Assertions.assertThat(fruits1.size()).isEqualTo(2);
         Assertions.assertThat(fruits1).contains(fruit1, fruit2);
 
-        List<Fruit> fruits2 = RestAssured.get(path)
-                .then()
-                .statusCode(HttpStatus.SC_OK)
-                .extract().body().jsonPath().getList(".", Fruit.class);
+        List<Fruit> fruits2 = getEntities(path, Fruit.class);
         Assertions.assertThat(fruits2).isEqualTo(fruits1);
     }
 
@@ -57,34 +59,28 @@ public class MongoDbIT {
         final FruitBasket fruitBasket2 = new FruitBasket("empty", List.of());
         final FruitBasket fruitBasket3 = new FruitBasket("full", List.of(fruit1, fruit2));
 
-        List<FruitBasket> fruitBaskets1 = postFruitBasket(path, fruitBasket1);
+        List<FruitBasket> fruitBaskets1 = postEntity(path, fruitBasket1, FruitBasket.class);
         Assertions.assertThat(fruitBaskets1).isNotNull();
         Assertions.assertThat(fruitBaskets1.size()).isEqualTo(1);
         Assertions.assertThat(fruitBaskets1).contains(fruitBasket1);
         Assertions.assertThat(fruitBaskets1.get(0).getItems()).isNull();
 
-        fruitBaskets1 = postFruitBasket(path, fruitBasket2);
+        fruitBaskets1 = postEntity(path, fruitBasket2, FruitBasket.class);
         Assertions.assertThat(fruitBaskets1).isNotNull();
         Assertions.assertThat(fruitBaskets1.size()).isEqualTo(2);
         Assertions.assertThat(fruitBaskets1).contains(fruitBasket1, fruitBasket2);
         Assertions.assertThat(fruitBaskets1.get(1).getItems()).isEmpty();
 
-        fruitBaskets1 = postFruitBasket(path, fruitBasket3);
+        fruitBaskets1 = postEntity(path, fruitBasket3, FruitBasket.class);
         Assertions.assertThat(fruitBaskets1).isNotNull();
         Assertions.assertThat(fruitBaskets1.size()).isEqualTo(3);
         Assertions.assertThat(fruitBaskets1).contains(fruitBasket1, fruitBasket2, fruitBasket3);
         Assertions.assertThat(fruitBaskets1.get(2).getItems()).contains(fruit1, fruit2);
 
-        List<FruitBasket> fruitBaskets2 = RestAssured.get(path)
-                .then()
-                .statusCode(HttpStatus.SC_OK)
-                .extract().body().jsonPath().getList(".", FruitBasket.class);
+        List<FruitBasket> fruitBaskets2 = getEntities(path, FruitBasket.class);
         Assertions.assertThat(fruitBaskets2).isEqualTo(fruitBaskets1);
 
-        List<FruitBasket> fruitBaskets3 = RestAssured.get(path + "/find-items/" + fruitBasket3.getName())
-                .then()
-                .statusCode(HttpStatus.SC_OK)
-                .extract().body().jsonPath().getList(".", FruitBasket.class);
+        List<FruitBasket> fruitBaskets3 = getEntities(path + "/find-items/" + fruitBasket3.getName(), FruitBasket.class);
         Assertions.assertThat(fruitBaskets3).isNotNull();
         Assertions.assertThat(fruitBaskets3.size()).isEqualTo(1);
         Assertions.assertThat(fruitBaskets3.get(0).getName()).isNull();
@@ -92,21 +88,30 @@ public class MongoDbIT {
         Assertions.assertThat(fruitBaskets3.get(0).getItems()).isEqualTo(fruitBasket3.getItems());
     }
 
-    private List<Fruit> postFruit(String path, Fruit fruit) {
-        return postEntity(path, fruit).getList(".", Fruit.class);
+    private <T> List<T> postEntity(String path, T entity, Class<T> clazz) {
+        final HttpRequest<Buffer> request = createRequest(path, HttpMethod.POST);
+        final HttpResponse<Buffer> response = getResponse(request.sendJson(entity));
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_OK);
+        return parseJsonArrayResponse(response, clazz);
     }
 
-    private List<FruitBasket> postFruitBasket(String path, FruitBasket fruitBasket) {
-        return postEntity(path, fruitBasket).getList(".", FruitBasket.class);
+    private <T> List<T> getEntities(String path, Class<T> clazz) {
+        final HttpRequest<Buffer> request = createRequest(path, HttpMethod.GET);
+        final HttpResponse<Buffer> response = getResponse(request.send());
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_OK);
+        return parseJsonArrayResponse(response, clazz);
     }
 
-    private <T> JsonPath postEntity(String path, T entity) {
-        return RestAssured.given()
-                .contentType(ContentType.JSON)
-                .body(entity)
-                .post(path)
-                .then()
-                .statusCode(HttpStatus.SC_OK)
-                .extract().body().jsonPath();
+    private HttpRequest<Buffer> createRequest(String path, HttpMethod httpMethod) {
+        return app.mutiny().request(httpMethod, path);
+    }
+
+    private HttpResponse<Buffer> getResponse(Uni<HttpResponse<Buffer>> responseUni) {
+        return responseUni.subscribe().withSubscriber(UniAssertSubscriber.create()).awaitItem().assertCompleted().getItem();
+    }
+
+    private <T> List<T> parseJsonArrayResponse(HttpResponse<Buffer> response, Class<T> clazz) {
+        return response.bodyAsJsonArray().stream().map(value -> Json.decodeValue(value.toString(), clazz))
+                .collect(Collectors.toList());
     }
 }
