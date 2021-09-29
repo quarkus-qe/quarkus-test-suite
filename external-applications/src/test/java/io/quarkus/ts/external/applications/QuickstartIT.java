eb55a4f128bb54e14a0a174acebb5ab8259a414a
Jose <josecarvajalhilario@gmail.com>
2021-09-29 11:06:02 +0200

    Disable QuickstartIT on Windows due to long file paths

--- external-applications/src/test/java/io/quarkus/ts/external/applications/QuickstartIT.java
+++ external-applications/src/test/java/io/quarkus/ts/external/applications/QuickstartIT.java
@@ -2,12 +2,15 @@ package io.quarkus.ts.external.applications;
 
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
 import io.quarkus.test.services.GitRepositoryQuarkusApplication;
 
 @QuarkusScenario
+@DisabledOnOs(value = OS.WINDOWS, disabledReason = "Windows does not support long file paths")
 public class QuickstartIT {
     @GitRepositoryQuarkusApplication(repo = "https://github.com/quarkusio/quarkus-quickstarts.git", contextDir = "getting-started")
     static final RestService app = new RestService();
