96aafeb8c5bb40b594fac7992497524ad14e3293
Jose <josecarvajalhilario@gmail.com>
2021-09-30 13:33:32 +0200

    Fix OpenShift scaling test
    This is a test framework issue reported in https://github.com/quarkus-qe/quarkus-test-framework/issues/290

--- scaling/src/test/java/io/quarkus/ts/openshift/scaling/OpenShiftScalingIT.java
+++ scaling/src/test/java/io/quarkus/ts/openshift/scaling/OpenShiftScalingIT.java
@@ -30,18 +30,15 @@ import io.restassured.response.ValidatableResponse;
 @TestMethodOrder(OrderAnnotation.class)
 public class OpenShiftScalingIT {
 
+    private static final int TIMEOUT_SEC = 60;
+    private static final int DELAY_BETWEEN_REQUEST_MS = 100;
+    private static final int POLL_INTERVAL_MS = 1000;
+
     @Inject
     static OpenShiftClient openShiftUtil;
 
     @QuarkusApplication
     static RestService app = new RestService();
-
-    private static final int TIMEOUT_SEC = 60;
-    private static final int DELAY_BETWEEN_REQUEST_MS = 100;
-    private static final int POLL_INTERVAL_MS = 1000;
-    private static final int FIRST = 1;
-    private static final int SECOND = 2;
-    private static final int THIRD = 3;
 
     @AfterEach
     public void scaleBack() {
@@ -55,7 +52,7 @@ public class OpenShiftScalingIT {
      * * Wait for their readiness and verify that both of the replicas are responding.
      */
     @Test
-    @Order(FIRST)
+    @Order(1)
     public void scaleUpTest() {
         int replicas = 2;
         givenResourcePath("/scaling");
@@ -66,13 +63,27 @@ public class OpenShiftScalingIT {
 
     /**
      * Workflow:
+     * * Scale down to zero replicas.
+     * * Execute an arbitrary minimal sample of requests and verify that all get HTTP 503 response.
+     */
+    @Test
+    @Order(2)
+    public void scaleToZero() {
+        givenResourcePath("/scaling");
+        whenScaleTo(0);
+        thenCheckReplicasAmount(0);
+        makeHttpScalingRequest("/scaling", SERVICE_UNAVAILABLE.getStatusCode());
+    }
+
+    /**
+     * Workflow:
      * * Scale to two replicas and verify that both of the replicas are responding.
      * * Scale down to a single replica.
      * * Execute an arbitrary minimal sample of requests and verify that all are served by the same replica.
      */
     @Test
-    @Order(SECOND)
-    public void scaleDownTest() throws InterruptedException {
+    @Order(3)
+    public void scaleDownTest() {
         int replicas = 2;
         givenResourcePath("/scaling");
         whenScaleTo(2);
@@ -83,20 +94,6 @@ public class OpenShiftScalingIT {
         whenScaleTo(1);
         thenCheckReplicasAmount(1);
         whenMakeRequestTo("/scaling", replicas);
-    }
-
-    /**
-     * Workflow:
-     * * Scale down to zero replicas.
-     * * Execute an arbitrary minimal sample of requests and verify that all get HTTP 503 response.
-     */
-    @Test
-    @Order(THIRD)
-    public void scaleToZero() throws InterruptedException {
-        givenResourcePath("/scaling");
-        whenScaleTo(0);
-        thenCheckReplicasAmount(0);
-        makeHttpScalingRequest("/scaling", SERVICE_UNAVAILABLE.getStatusCode());
     }
 
     private void givenResourcePath(String path) {
