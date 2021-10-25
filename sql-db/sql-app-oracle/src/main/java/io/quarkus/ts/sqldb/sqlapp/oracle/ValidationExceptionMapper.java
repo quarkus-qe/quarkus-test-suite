07d4638c1e9f6dc1140f481025d054f6c0cfa446
Josef Smrcka <jsmrcka@redhat.com>
2021-10-25 08:34:38 +0200

    Add separate SQL module for Oracle
    Original RFE: https://issues.redhat.com/browse/QUARKUS-1164.
    Related bug: https://issues.redhat.com/browse/QUARKUS-1305.

---
+++ sql-db/sql-app-oracle/src/main/java/io/quarkus/ts/sqldb/sqlapp/oracle/ValidationExceptionMapper.java
@@ -0,0 +1,36 @@
+package io.quarkus.ts.sqldb.sqlapp.oracle;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
+@Provider
+public class ValidationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
+
+    public static final int UNPROCESSABLE_ENTITY = 422;
+
+    @Override
+    public Response toResponse(ConstraintViolationException exception) {
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayNode errors = mapper.createArrayNode();
+
+        for (ConstraintViolation<?> constraintViolation : exception.getConstraintViolations()) {
+            errors.addObject()
+                    .put("path", constraintViolation.getPropertyPath().toString())
+                    .put("message", constraintViolation.getMessage());
+        }
+
+        return Response.status(UNPROCESSABLE_ENTITY)
+                .type(MediaType.APPLICATION_JSON)
+                .entity(mapper.createObjectNode()
+                        .put("code", UNPROCESSABLE_ENTITY)
+                        .set("error", errors))
+                .build();
+    }
+}
