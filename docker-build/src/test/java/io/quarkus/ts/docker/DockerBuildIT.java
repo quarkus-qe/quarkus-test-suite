91861171e07f8726f64b749908eaf25cf2f1fd8b
pablo gonzalez granados <pagonzal@redhat.com>
2022-01-03 16:34:33 +0100

    Add docker image name with empty spaces scenario

---
+++ docker-build/src/test/java/io/quarkus/ts/docker/DockerBuildIT.java
@@ -0,0 +1,33 @@
+package io.quarkus.ts.docker;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Objects;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import com.github.dockerjava.api.model.Image;
+
+import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.scenarios.annotations.DisabledOnNative;
+import io.quarkus.test.utils.DockerUtils;
+
+@QuarkusScenario
+@DisabledOnNative(reason = "Due to reduce native execution time")
+public class DockerBuildIT {
+
+    private static final String DOCKER_IMG_NAME = "hello-world-app";
+    private static final String DOCKER_IMG_VERSION = "1.0.0";
+
+    @AfterAll
+    public static void tearDown() {
+        DockerUtils.removeImage(DOCKER_IMG_NAME, DOCKER_IMG_VERSION);
+    }
+
+    @Test
+    public void buildDockerImage() {
+        Image dockerImg = DockerUtils.getImage(DOCKER_IMG_NAME, DOCKER_IMG_VERSION);
+        assertTrue(Objects.nonNull(dockerImg.getId()) && !dockerImg.getId().isEmpty(), "Docker image was not created");
+    }
+}
