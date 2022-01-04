40262f393b0cc3cf0772e65d124c114f0a3e1d8b
Josef Smrcka <jsmrcka@redhat.com>
2022-01-04 10:42:57 +0100

    Add coverage for case-insensitive HTTP `Accept` header

---
+++ http/http-minimum-reactive/src/test/java/io/quarkus/ts/http/minimum/reactive/HttpCustomHeadersIT.java
@@ -0,0 +1,25 @@
+package io.quarkus.ts.http.minimum.reactive;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.scenarios.QuarkusScenario;
+
+@Tag("QUARKUS-1574")
+@QuarkusScenario
+public class HttpCustomHeadersIT {
+
+    @Test
+    public void caseInsensitiveAcceptHeader() {
+        given()
+                .accept("Application/json")
+                .get("/api/hello")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("content", is("Hello, World!"));
+    }
+}
