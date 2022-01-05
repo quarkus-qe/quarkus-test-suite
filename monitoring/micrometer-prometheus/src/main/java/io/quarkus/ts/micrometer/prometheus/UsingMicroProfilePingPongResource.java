938aa1625acbb81d86259037ea16c9e59048c034
pablo gonzalez granados <pagonzal@redhat.com>
2022-01-05 17:17:36 +0100

    Gauge annotation is not registered or showing up

--- monitoring/micrometer-prometheus/src/main/java/io/quarkus/ts/micrometer/prometheus/UsingMicroProfilePingPongResource.java
+++ monitoring/micrometer-prometheus/src/main/java/io/quarkus/ts/micrometer/prometheus/UsingMicroProfilePingPongResource.java
@@ -5,17 +5,33 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Gauge;
 
 @Path("/using-microprofile-pingpong")
 public class UsingMicroProfilePingPongResource {
 
     private static final String PING_PONG = "ping pong";
+    private static final long DEFAULT_GAUGE_VALUE = 100L;
 
     @GET
-    @Counted(name = "simple_mp", absolute = true)
+    @Counted(name = "simple_counter_mp", absolute = true)
     @Produces(MediaType.TEXT_PLAIN)
+    @Path("/counter")
     public String simpleScenario() {
         return PING_PONG;
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/gauge")
+    public long highestPrimeNumberSoFar() {
+        return getDefaultGauge();
+    }
+
+    @Gauge(name = "simple_gauge_mp", unit = MetricUnits.NONE)
+    public long getDefaultGauge() {
+        return DEFAULT_GAUGE_VALUE;
     }
 }
--- monitoring/micrometer-prometheus/src/test/java/io/quarkus/ts/micrometer/prometheus/UsingMicroProfilePingPongResourceIT.java
+++ monitoring/micrometer-prometheus/src/test/java/io/quarkus/ts/micrometer/prometheus/UsingMicroProfilePingPongResourceIT.java
@@ -13,25 +13,40 @@ import io.quarkus.test.scenarios.QuarkusScenario;
 @QuarkusScenario
 public class UsingMicroProfilePingPongResourceIT {
 
+    private static final String PING_PONG = "ping pong";
     private static final String PING_PONG_ENDPOINT = "/using-microprofile-pingpong";
-    private static final String COUNTER_FORMAT = "simple_mp_total{scope=\"application\",} %s.0";
+    private static final String COUNTER_FORMAT = "simple_counter_mp_total{scope=\"application\",} %s.0";
+    private static final String GAUGE_FORMAT = "simple_gauge_mp{scope=\"application\",} %s";
+    private static final long DEFAULT_GAUGE_VALUE = 100;
 
     @Test
     public void testShouldReturnCountOne() {
-        whenCallPingPong();
+        whenCallPingPong("/counter", PING_PONG);
         thenCounterIs(1);
     }
 
-    private void whenCallPingPong() {
+    @Test
+    public void testShouldReturnDefaultGauge() {
+        whenCallPingPong("/gauge", "" + DEFAULT_GAUGE_VALUE);
+        thenGaugeIs(DEFAULT_GAUGE_VALUE);
+    }
+
+    private void whenCallPingPong(String path, String expectedBody) {
         given()
-                .when().get(PING_PONG_ENDPOINT)
+                .when().get(PING_PONG_ENDPOINT + path)
                 .then().statusCode(HttpStatus.SC_OK)
-                .body(is("ping pong"));
+                .body(is(expectedBody));
     }
 
     private void thenCounterIs(int expectedCounter) {
         when().get("/q/metrics").then()
                 .statusCode(HttpStatus.SC_OK)
                 .body(containsString(String.format(COUNTER_FORMAT, expectedCounter)));
+    }
+
+    private void thenGaugeIs(double expectedGauge) {
+        when().get("/q/metrics").then()
+                .statusCode(HttpStatus.SC_OK)
+                .body(containsString(String.format(GAUGE_FORMAT, expectedGauge)));
     }
 }
