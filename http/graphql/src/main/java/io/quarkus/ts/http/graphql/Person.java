39cb4c8ab6c7cf7774629fef59ed146a67dda73e
Fedor Dudinskiy <fdudinsk@redhat.com>
2022-01-06 17:10:16 +0100

    Coverage for quarkusio/quarkus#20293

---
+++ http/graphql/src/main/java/io/quarkus/ts/http/graphql/Person.java
@@ -0,0 +1,22 @@
+package io.quarkus.ts.http.graphql;
+
+public class Person {
+    public String name;
+    private Person friend;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setFriend(Person friend) {
+        this.friend = friend;
+    }
+
+    public Person getFriend() {
+        return friend;
+    }
+}
