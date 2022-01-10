b9b24cb0020347eab8c5c6cc85628d8b15158334
pablo gonzalez granados <pagonzal@redhat.com>
2022-01-10 12:57:56 +0100

    Add pathParam as BeanParam scenario

--- http/rest-client-reactive/src/main/java/io/quarkus/ts/http/restclient/reactive/json/BookAsJsonResource.java
+++ http/rest-client-reactive/src/main/java/io/quarkus/ts/http/restclient/reactive/json/BookAsJsonResource.java
@@ -1,18 +1,23 @@
 package io.quarkus.ts.http.restclient.reactive.json;
 
+import java.util.Map;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import io.smallrye.mutiny.Uni;
+import io.vertx.core.json.Json;
 
 @Produces(MediaType.APPLICATION_JSON)
-@Path("/book/json")
+@Path("/book/{id}/json")
 public class BookAsJsonResource {
 
     @GET
-    public Uni<String> get() {
-        return Uni.createFrom().item("{\"title\":\"Title in Json\"}");
+    public Uni<String> get(@PathParam("id") String id) {
+        Map<String, String> body = Map.of("title", "Title in Json: " + id);
+        return Uni.createFrom().item(Json.encode(body));
     }
 }
