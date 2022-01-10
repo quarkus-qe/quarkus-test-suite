e71496b53a0b9d672f0ade777ab73aa9bc4e76de
pablo gonzalez granados <pagonzal@redhat.com>
2022-01-10 12:57:56 +0100

    Add pathParam as BeanParam scenario

--- http/rest-client-reactive/src/main/java/io/quarkus/ts/http/restclient/reactive/ReactiveClientBookResource.java
+++ http/rest-client-reactive/src/main/java/io/quarkus/ts/http/restclient/reactive/ReactiveClientBookResource.java
@@ -3,6 +3,7 @@ package io.quarkus.ts.http.restclient.reactive;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -11,7 +12,7 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import io.quarkus.ts.http.restclient.reactive.json.Book;
 import io.smallrye.mutiny.Uni;
 
-@Path("/client/book")
+@Path("/client/{id}/book")
 public class ReactiveClientBookResource {
 
     @Inject
@@ -21,7 +22,14 @@ public class ReactiveClientBookResource {
     @GET
     @Path("/json")
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<Book> getAsJson() {
-        return restInterface.getAsJson();
+    public Uni<Book> getAsJson(@PathParam("id") String id) {
+        return restInterface.getAsJson(id);
+    }
+
+    @GET
+    @Path("/jsonByBeanParam")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<Book> getAsJsonByBeanParam(@PathParam("id") String id) {
+        return restInterface.getWithBeanParam(new IdBeanParam(id));
     }
 }
