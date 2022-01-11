3dcd5d32955952330928a1c613cc2fb49243b814
pablo gonzalez granados <pagonzal@redhat.com>
2022-01-11 19:21:19 +0100

    patch Qute multimode scenario

--- qute/multimodule/qute-api/src/main/java/io/quarkus/qe/qute/api/AppMessagesProvider.java
+++ qute/multimodule/qute-api/src/main/java/io/quarkus/qe/qute/api/AppMessagesProvider.java
@@ -1,8 +1,12 @@
 package io.quarkus.qe.qute.api;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.inject.Instance;
+import java.util.List;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Any;
+import javax.inject.Inject;
+
+import io.quarkus.arc.All;
 import io.quarkus.qute.i18n.Localized;
 
 @ApplicationScoped
@@ -11,10 +15,11 @@ public class AppMessagesProvider {
     private final AlertMessages enAppMessages;
     private final AlertMessages elAppMessages;
 
-    public AppMessagesProvider(@Localized("en") Instance<AlertMessages> enAppMessages,
-            @Localized("el") Instance<AlertMessages> elAppMessages) {
-        this.enAppMessages = enAppMessages.iterator().next();
-        this.elAppMessages = elAppMessages.iterator().next();
+    @Inject
+    public AppMessagesProvider(@Localized("en") @All List<AlertMessages> enAppMessages,
+            @Localized("el") @Any @All List<AlertMessages> elAppMessages) {
+        this.enAppMessages = enAppMessages.stream().findFirst().get();
+        this.elAppMessages = elAppMessages.stream().findFirst().get();
     }
 
     public AlertMessages appMessages(String lang) {
