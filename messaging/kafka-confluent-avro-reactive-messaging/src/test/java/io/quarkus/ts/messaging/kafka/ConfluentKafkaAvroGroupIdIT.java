7ca033124ebd7414bc1ef5329e74878bc0df2229
pablo gonzalez granados <pagonzal@redhat.com>
2022-01-12 13:02:07 +0100

    Add apicurio devServices coverage

--- messaging/kafka-confluent-avro-reactive-messaging/src/test/java/io/quarkus/ts/messaging/kafka/ConfluentKafkaAvroGroupIdIT.java
+++ messaging/kafka-confluent-avro-reactive-messaging/src/test/java/io/quarkus/ts/messaging/kafka/ConfluentKafkaAvroGroupIdIT.java
@@ -1,5 +1,7 @@
 package io.quarkus.ts.messaging.kafka;
 
+import org.junit.jupiter.api.Tag;
+
 import io.quarkus.test.bootstrap.KafkaService;
 import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
@@ -7,20 +9,32 @@ import io.quarkus.test.services.KafkaContainer;
 import io.quarkus.test.services.QuarkusApplication;
 import io.quarkus.test.services.containers.model.KafkaVendor;
 
+@Tag("QUARKUS-1089")
 @QuarkusScenario
-public class ConfluentKafkaAvroIT extends BaseKafkaAvroIT {
+public class ConfluentKafkaAvroGroupIdIT extends BaseKafkaAvroGroupIdIT {
 
     @KafkaContainer(vendor = KafkaVendor.CONFLUENT, withRegistry = true)
     static final KafkaService kafka = new KafkaService();
 
     @QuarkusApplication
-    static final RestService app = new RestService()
-            .withProperties("confluent-application.properties")
+    static RestService appGroupIdA = new RestService()
+            .withProperty("cron.expr", "0 0 0 ? * * *")
+            .withProperty("kafka.bootstrap.servers", kafka::getBootstrapUrl)
+            .withProperty("confluent.registry.url", kafka::getRegistryUrl);
+
+    @QuarkusApplication
+    static RestService appGroupIdB = new RestService()
+            .withProperty("cron.expr", "0 0 0 ? * * *")
             .withProperty("kafka.bootstrap.servers", kafka::getBootstrapUrl)
             .withProperty("confluent.registry.url", kafka::getRegistryUrl);
 
     @Override
-    public RestService getApp() {
-        return app;
+    public RestService getAppA() {
+        return appGroupIdA;
+    }
+
+    @Override
+    public RestService getAppB() {
+        return appGroupIdB;
     }
 }
---
+++ messaging/kafka-confluent-avro-reactive-messaging/src/test/java/io/quarkus/ts/messaging/kafka/ConfluentKafkaAvroIT.java
@@ -0,0 +1,25 @@
+package io.quarkus.ts.messaging.kafka;
+
+import io.quarkus.test.bootstrap.KafkaService;
+import io.quarkus.test.bootstrap.RestService;
+import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.services.KafkaContainer;
+import io.quarkus.test.services.QuarkusApplication;
+import io.quarkus.test.services.containers.model.KafkaVendor;
+
+@QuarkusScenario
+public class ConfluentKafkaAvroIT extends BaseKafkaAvroIT {
+
+    @KafkaContainer(vendor = KafkaVendor.CONFLUENT, withRegistry = true)
+    static final KafkaService kafka = new KafkaService();
+
+    @QuarkusApplication
+    static final RestService app = new RestService()
+            .withProperty("kafka.bootstrap.servers", kafka::getBootstrapUrl)
+            .withProperty("confluent.registry.url", kafka::getRegistryUrl);
+
+    @Override
+    public RestService getApp() {
+        return app;
+    }
+}
