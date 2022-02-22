36c720b0194e6ba6c25c48ef8612500d8d37149e
pablo gonzalez granados <pagonzal@redhat.com>
2022-02-22 13:53:41 +0100

    Disable Native external app scenario because S2I is not supported

--- external-applications/src/test/java/io/quarkus/ts/external/applications/OpenShiftQuickstartIT.java
+++ external-applications/src/test/java/io/quarkus/ts/external/applications/OpenShiftQuickstartIT.java
@@ -1,9 +1,11 @@
 package io.quarkus.ts.external.applications;
 
 import io.quarkus.test.scenarios.OpenShiftScenario;
+import io.quarkus.test.scenarios.annotations.DisabledOnNative;
 import io.quarkus.test.scenarios.annotations.DisabledOnQuarkusSnapshot;
 
 @DisabledOnQuarkusSnapshot(reason = "999-SNAPSHOT is not available in the Maven repositories in OpenShift")
+@DisabledOnNative(reason = "Native + s2i not supported")
 @OpenShiftScenario
 public class OpenShiftQuickstartIT extends QuickstartIT {
 }
