c371ca1b55ed7cd2154c9699e5568084af52bbb2
Rostislav Svoboda <rsvoboda@redhat.com>
2022-03-02 22:13:26 +0100

    Qute experiments

--- qute/multimodule/qute-test/src/test/java/io/quarkus/qe/qute/LocalizedMessagesTest.java
+++ qute/multimodule/qute-test/src/test/java/io/quarkus/qe/qute/LocalizedMessagesTest.java
@@ -3,7 +3,6 @@ package io.quarkus.qe.qute;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -16,8 +15,6 @@ class LocalizedMessagesTest {
     @Tag("QUARKUS-1547")
     @ParameterizedTest
     @CsvSource({ "en, Hello. Hello Nikos", "el, Γειά. Γειά σου Nikos" })
-    //TODO https://github.com/quarkusio/quarkus/issues/19617
-    @Disabled("@MessageBundle is not packaged and throw an exception")
     void useLocalizedMessageInstance(String lang, String output) {
         given()
                 .when().get("/hello/" + lang)
