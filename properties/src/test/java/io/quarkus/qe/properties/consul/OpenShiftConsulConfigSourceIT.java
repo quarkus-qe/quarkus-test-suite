4ac35857f8849967a96f80a9c713d8a4ca0ca386
pablo gonzalez granados <pagonzal@redhat.com>
2022-03-14 11:02:38 +0100

    Disable Consul Openshift scenario OpenShiftConsulConfigSourceIT

--- properties/src/test/java/io/quarkus/qe/properties/consul/OpenShiftConsulConfigSourceIT.java
+++ properties/src/test/java/io/quarkus/qe/properties/consul/OpenShiftConsulConfigSourceIT.java
@@ -1,7 +1,11 @@
 package io.quarkus.qe.properties.consul;
 
+import org.junit.jupiter.api.Disabled;
+
 import io.quarkus.test.scenarios.OpenShiftScenario;
 
 @OpenShiftScenario
+@Disabled
+//TODO https://github.com/quarkusio/quarkus/issues/24277
 public class OpenShiftConsulConfigSourceIT extends ConsulConfigSourceIT {
 }
