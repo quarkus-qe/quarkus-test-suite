d959169de446ceebd36846265a3a6188984659b0
Fedor Dudinskiy <fdudinsk@redhat.com>
2022-03-15 11:48:37 +0100

    QUARKUS-1409 Add Cloud Events support to the AMQP connector

---
+++ messaging/cloud-events/amqp-json/src/main/java/io/quarkus/ts/messaging/amqpreactive/Resource.java
@@ -0,0 +1,29 @@
+package io.quarkus.ts.messaging.amqpreactive;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class Resource {
+
+    @Inject
+    Consumer consumer;
+
+    @GET
+    @Path("/result")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response result() {
+        return Response.ok(consumer.getResults()).build();
+    }
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String root() {
+        return "All good.";
+    }
+}
