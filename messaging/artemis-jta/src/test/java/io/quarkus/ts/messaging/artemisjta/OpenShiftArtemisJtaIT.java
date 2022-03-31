53726f5727be7b65fbc0404157ea88ca91289761
Rostislav Svoboda <rsvoboda@redhat.com>
2022-03-31 15:48:55 +0200

    quarkus-artemis-jms extension removal
    quarkus-artemis-jms extension is not priority for the product and also not for community
    Extension was moved to Quarkiverse repository community based evolution

--- messaging/artemis-jta/src/test/java/io/quarkus/ts/messaging/artemisjta/OpenShiftArtemisJtaIT.java
+++
@@ -1,7 +0,0 @@
-package io.quarkus.ts.messaging.artemisjta;
-
-import io.quarkus.test.scenarios.OpenShiftScenario;
-
-@OpenShiftScenario
-public class OpenShiftArtemisJtaIT extends ArtemisJtaIT {
-}
