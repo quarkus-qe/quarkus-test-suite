a8ba11b04339823d59a76e025a33761fdb15de34
Josef Smrcka <jsmrcka@redhat.com>
2022-04-26 10:38:08 +0200

    Enable disabled rest-client-reactive multipart test

--- http/jaxrs-reactive/src/test/java/io/quarkus/ts/jaxrs/reactive/client/MultipartClientResourceIT.java
+++ http/jaxrs-reactive/src/test/java/io/quarkus/ts/jaxrs/reactive/client/MultipartClientResourceIT.java
@@ -3,15 +3,12 @@ package io.quarkus.ts.jaxrs.reactive.client;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.scenarios.QuarkusScenario;
 
-// TODO: Failing because of https://github.com/quarkusio/quarkus/issues/19892, enable after the issue is resolved
 @Tag("QUARKUS-1225")
-@Disabled
 @QuarkusScenario
 public class MultipartClientResourceIT {
 
