5a7c1390a7be70fd2eeab42e406d98cbcb62f858
Fedor Dudinskiy <fdudinsk@redhat.com>
2022-06-03 15:55:43 +0200

    Add checks for the following issues:
    https://github.com/quarkusio/quarkus/issues/24402
    https://github.com/quarkusio/quarkus/issues/24405
    https://github.com/quarkusio/quarkus/issues/24415
    https://github.com/quarkusio/quarkus/issues/25028
    Additionally: delete temporary files to speed up local execution
    (cherry picked from commit 565a6fdf0a2a84ec6012a0ab6a6ed6eed98cc509)

--- http/rest-client-reactive/src/main/java/io/quarkus/ts/http/restclient/reactive/files/FileWrapper.java
+++ http/rest-client-reactive/src/main/java/io/quarkus/ts/http/restclient/reactive/files/FileWrapper.java
@@ -2,13 +2,20 @@ package io.quarkus.ts.http.restclient.reactive.files;
 
 import java.io.File;
 
-import javax.ws.rs.FormParam;
 import javax.ws.rs.core.MediaType;
 
 import org.jboss.resteasy.reactive.PartType;
+import org.jboss.resteasy.reactive.RestForm;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
 public class FileWrapper {
-    @FormParam("file")
+    @RestForm("file")
     @PartType(MediaType.APPLICATION_OCTET_STREAM)
     public File file;
+
+    @RestForm("name")
+    @PartType(MediaType.TEXT_PLAIN)
+    public String name;
 }
