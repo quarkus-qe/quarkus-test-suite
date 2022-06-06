dc9fae4dcdca17562b3c5c1ece7e3a506cb8b5d2
pablo gonzalez granados <pagonzal@redhat.com>
2022-06-06 13:23:01 +0200

    CI/stability: kafka-strimzi-avro-reactive-messaging

--- messaging/kafka-strimzi-avro-reactive-messaging/src/test/java/io/quarkus/ts/messaging/strimzi/kafka/reactive/StrimziKafkaAvroGroupIdIT.java
+++ messaging/kafka-strimzi-avro-reactive-messaging/src/test/java/io/quarkus/ts/messaging/strimzi/kafka/reactive/StrimziKafkaAvroGroupIdIT.java
@@ -5,12 +5,15 @@ import org.junit.jupiter.api.Tag;
 import io.quarkus.test.bootstrap.KafkaService;
 import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.scenarios.annotations.DisabledOnQuarkusSnapshot;
 import io.quarkus.test.services.KafkaContainer;
 import io.quarkus.test.services.QuarkusApplication;
 import io.quarkus.test.services.containers.model.KafkaVendor;
 
 @Tag("QUARKUS-1089")
 @QuarkusScenario
+// TODO https://github.com/quarkusio/quarkus/issues/25814
+@DisabledOnQuarkusSnapshot(reason = "Quarkus upstream is using Apicurio 2.2.3.Final that is not backward compatible with Apicurio 2.1.5 used previously")
 public class StrimziKafkaAvroGroupIdIT extends BaseKafkaAvroGroupIdIT {
 
     @KafkaContainer(vendor = KafkaVendor.STRIMZI, withRegistry = true, registryPath = "/apis/registry/v2")
