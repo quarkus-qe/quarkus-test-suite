95fcdde9472c1621a4cdf63f2b2442e95f87b45c
Josef Smrcka <jsmrcka@redhat.com>
2022-06-08 13:09:33 +0200

    Test ArC @All list injection
    Test dependency injection using ArC `@All` with `List`.
    See also https://github.com/quarkusio/quarkus/pull/25784.
    
    (cherry picked from commit ad4f60ef9d4f3b1687aae7a040ff745f7ba9f270)

---
+++ spring/spring-data/src/main/java/io/quarkus/ts/spring/data/di/ListInjectionBean.java
@@ -0,0 +1,31 @@
+package io.quarkus.ts.spring.data.di;
+
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import io.quarkus.arc.All;
+import io.quarkus.arc.properties.IfBuildProperty;
+
+@ApplicationScoped
+// Bean causes build error on unpatched Quarkus => enable only on demand to avoid interference with other tests.
+@IfBuildProperty(name = ListInjectionBean.LIST_INJECTION_BEAN_ENABLED, stringValue = ListInjectionBean.LIST_INJECTION_BEAN_ENABLED_VALUE)
+public class ListInjectionBean {
+    public static final String LIST_INJECTION_BEAN_ENABLED = "io.quarkus.ts.spring.data.di.ListInjectionBean.enabled";
+    public static final String LIST_INJECTION_BEAN_ENABLED_VALUE = "true";
+
+    List<Service> constructorInjectServices;
+
+    @Autowired
+    @All
+    List<Service> fieldInjectServices;
+
+    ListInjectionBean(@All List<Service> constructorInjectServices) {
+        this.constructorInjectServices = constructorInjectServices;
+    }
+
+    interface Service {
+    }
+}
