708be0ec914304a494f6714d274dd7f746ace2fc
Rostislav Svoboda <rsvoboda@redhat.com>
2022-06-08 13:45:09 +0200

    Ensure no NPE  is thrown when using RestClient with Micrometer disabled

--- http/rest-client/src/test/java/io/quarkus/ts/http/restclient/ClientBookResourceIT.java
+++ http/rest-client/src/test/java/io/quarkus/ts/http/restclient/ClientBookResourceIT.java
@@ -8,29 +8,43 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.services.QuarkusApplication;
 
 @QuarkusScenario
 public class ClientBookResourceIT {
 
+    @QuarkusApplication
+    static RestService app = new RestService();
+
+    @QuarkusApplication
+    static RestService appWithMicrometerDisabled = new RestService().withProperty("quarkus.micrometer.enabled", "false");
+
     @Test
     public void shouldGetBookFromRestClientXml() {
-        given().get("/client/book/xml").then().statusCode(HttpStatus.SC_OK)
+        app.given().get("/client/book/xml").then().statusCode(HttpStatus.SC_OK)
                 .body(is(
                         "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><book><title>Title in Xml</title></book>"));
     }
 
     @Test
     public void shouldGetBookFromRestClientJson() {
-        given().get("/client/book/json").then().statusCode(HttpStatus.SC_OK)
+        app.given().get("/client/book/json").then().statusCode(HttpStatus.SC_OK)
                 .body(is("{\"title\":\"Title in Json\"}"));
     }
 
     @Test
     @Tag("QUARKUS-1376")
     public void notFoundShouldNotReturnAnyResteasyImplementationDetails() {
-        String body = given().get("/notFound").then().statusCode(HttpStatus.SC_NOT_FOUND).extract().body().asString();
+        String body = app.given().get("/notFound").then().statusCode(HttpStatus.SC_NOT_FOUND).extract().body().asString();
         Assertions.assertFalse(body.contains("RESTEASY"),
                 "Not found resource should not return any Resteasy implementation details, but was: " + body);
+    }
+
+    @Test
+    @Tag("QUARKUS-2127")
+    public void noNullPointerExceptionForRestClientUsageWithDisabledMicrometer() {
+        appWithMicrometerDisabled.given().get("/client/book/xml").then().statusCode(HttpStatus.SC_OK);
     }
 }
