18adc85e1f78aa590f1d2dab09d41239074d15a7
Rostislav Svoboda <rsvoboda@redhat.com>
2022-06-16 09:37:29 +0200

    Revert "Disable Quarkus-cli create extension scenario on Quarkus upstream"
    This reverts commit fb912d2af7c6a987ee3626620b8e09d69806df6b.
    
    Fixed in https://github.com/quarkusio/quarkus/pull/26130, we do not need to wait for Quarkus 2.10 release

--- quarkus-cli/src/test/java/io/quarkus/ts/quarkus/cli/QuarkusCliCreateExtensionIT.java
+++ quarkus-cli/src/test/java/io/quarkus/ts/quarkus/cli/QuarkusCliCreateExtensionIT.java
@@ -11,12 +11,9 @@ import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import io.quarkus.test.bootstrap.QuarkusCliClient;
 import io.quarkus.test.bootstrap.QuarkusCliDefaultService;
 import io.quarkus.test.scenarios.QuarkusScenario;
-import io.quarkus.test.scenarios.annotations.DisabledOnQuarkusSnapshot;
 
 @QuarkusScenario
 @DisabledIfSystemProperty(named = "profile.id", matches = "native", disabledReason = "Only for JVM mode")
-// TODO https://github.com/quarkusio/quarkus/pull/25880#issuecomment-1149671224
-@DisabledOnQuarkusSnapshot(reason = "quarkus-extension-maven-plugin introduced for Quarkus 2.10, re-enable once 2.10 is out")
 public class QuarkusCliCreateExtensionIT {
 
     @Inject
