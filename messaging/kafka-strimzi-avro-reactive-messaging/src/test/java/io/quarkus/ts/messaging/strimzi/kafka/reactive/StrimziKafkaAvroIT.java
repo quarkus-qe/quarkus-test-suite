e7ef6e7d91843b3cd963ff879a05f704433e1b20
pablo gonzalez granados <pagonzal@redhat.com>
2022-08-01 20:01:17 +0200

    Re-enable some Kafka strimzi scenarios

--- messaging/kafka-strimzi-avro-reactive-messaging/src/test/java/io/quarkus/ts/messaging/strimzi/kafka/reactive/StrimziKafkaAvroIT.java
+++ messaging/kafka-strimzi-avro-reactive-messaging/src/test/java/io/quarkus/ts/messaging/strimzi/kafka/reactive/StrimziKafkaAvroIT.java
@@ -3,14 +3,11 @@ package io.quarkus.ts.messaging.strimzi.kafka.reactive;
 import io.quarkus.test.bootstrap.KafkaService;
 import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
-import io.quarkus.test.scenarios.annotations.DisabledOnQuarkusSnapshot;
 import io.quarkus.test.services.KafkaContainer;
 import io.quarkus.test.services.QuarkusApplication;
 import io.quarkus.test.services.containers.model.KafkaVendor;
 
 @QuarkusScenario
-// TODO https://github.com/quarkusio/quarkus/issues/25814
-@DisabledOnQuarkusSnapshot(reason = "Quarkus upstream is using Apicurio 2.2.3.Final that is not backward compatible with Apicurio 2.1.5 used previously")
 public class StrimziKafkaAvroIT extends BaseKafkaAvroIT {
 
     @KafkaContainer(vendor = KafkaVendor.STRIMZI, withRegistry = true, registryPath = "/apis/registry/v2")
