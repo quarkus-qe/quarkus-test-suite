9e5b1b62dc1c9ab516a96309ea033b60eb9f78b2
Pablo Gonzalez Granados <pagonzal@redhat.com>
2022-08-16 23:57:46 +0200

    Add missing Mssql required property to 'application.property' file (#792)

--- sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/OpenShiftMssqlDatabaseIT.java
+++ sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/OpenShiftMssqlDatabaseIT.java
@@ -1,7 +1,10 @@
 package io.quarkus.ts.sqldb.sqlapp;
 
 import io.quarkus.test.scenarios.OpenShiftScenario;
+import io.quarkus.test.scenarios.annotations.DisabledOnQuarkusSnapshot;
 
+//TODO https://github.com/quarkus-qe/quarkus-test-suite/issues/793
+@DisabledOnQuarkusSnapshot(reason = "Required property is ignored on OCP")
 @OpenShiftScenario
 public class OpenShiftMssqlDatabaseIT extends MssqlDatabaseIT {
 
