8a0176ef45b6a4303745ec4cd4dd3eda711dcd5e
pablo gonzalez granados <pagonzal@redhat.com>
2022-08-17 12:05:33 +0200

    Mssql trustservercertificate required property must be in lower case

--- sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/OpenShiftMssqlDatabaseIT.java
+++ sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/OpenShiftMssqlDatabaseIT.java
@@ -1,10 +1,7 @@
 package io.quarkus.ts.sqldb.sqlapp;
 
 import io.quarkus.test.scenarios.OpenShiftScenario;
-import io.quarkus.test.scenarios.annotations.DisabledOnQuarkusSnapshot;
 
-//TODO https://github.com/quarkus-qe/quarkus-test-suite/issues/793
-@DisabledOnQuarkusSnapshot(reason = "Required property is ignored on OCP")
 @OpenShiftScenario
 public class OpenShiftMssqlDatabaseIT extends MssqlDatabaseIT {
 
