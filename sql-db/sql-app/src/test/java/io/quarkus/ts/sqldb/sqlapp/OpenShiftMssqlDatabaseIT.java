4baf1f9a4eebc31769d2635edfd716018e67e774
Fedor Dudinskiy <fdudinsk@redhat.com>
2022-08-26 15:57:16 +0200

    Disabled MSSQL tests on openshift due to a known issue

--- sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/OpenShiftMssqlDatabaseIT.java
+++ sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/OpenShiftMssqlDatabaseIT.java
@@ -1,8 +1,11 @@
 package io.quarkus.ts.sqldb.sqlapp;
 
+import org.junit.jupiter.api.Disabled;
+
 import io.quarkus.test.scenarios.OpenShiftScenario;
 
 @OpenShiftScenario
+@Disabled("https://github.com/microsoft/mssql-docker/issues/769")
 public class OpenShiftMssqlDatabaseIT extends MssqlDatabaseIT {
 
 }
