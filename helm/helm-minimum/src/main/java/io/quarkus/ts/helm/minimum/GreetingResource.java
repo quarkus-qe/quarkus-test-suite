6a194a44210b64616d22c664c00c4a2c032d94ac
pablo gonzalez granados <pagonzal@redhat.com>
2022-09-13 19:34:00 +0200

    Add Helm coverage - base usecase

---
+++ helm/helm-minimum/src/main/java/io/quarkus/ts/helm/minimum/GreetingResource.java
@@ -0,0 +1,15 @@
+package io.quarkus.ts.helm.minimum;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/greeting")
+public class GreetingResource {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayHello() {
+        return "Hello Helm!";
+    }
+}
