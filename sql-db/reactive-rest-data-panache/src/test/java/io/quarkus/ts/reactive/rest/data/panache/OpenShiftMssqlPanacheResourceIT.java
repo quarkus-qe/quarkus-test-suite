7a4d44c4f3e4c4e366e096b908a2c932e657de52
Pablo Gonzalez Granados <pagonzal@redhat.com>
2022-10-20 11:12:10 +0200

    Re-enable mssql scenario (#866)

--- sql-db/reactive-rest-data-panache/src/test/java/io/quarkus/ts/reactive/rest/data/panache/OpenShiftMssqlPanacheResourceIT.java
+++ sql-db/reactive-rest-data-panache/src/test/java/io/quarkus/ts/reactive/rest/data/panache/OpenShiftMssqlPanacheResourceIT.java
@@ -1,10 +1,7 @@
 package io.quarkus.ts.reactive.rest.data.panache;
 
-import org.junit.jupiter.api.Disabled;
-
 import io.quarkus.test.scenarios.OpenShiftScenario;
 
 @OpenShiftScenario
-@Disabled("https://github.com/microsoft/mssql-docker/issues/769")
 public class OpenShiftMssqlPanacheResourceIT extends MssqlPanacheResourceIT {
 }
