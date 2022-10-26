99c9e1c0eac2debce0d3d425424659a181d85590
pablo gonzalez granados <pagonzal@redhat.com>
2022-10-26 20:18:46 +0200

    Disable MultipartClientIT#testMultipartDataIsSent scenario due to an upstream issue
    (cherry picked from commit 9b45fe25935788ddb4e0a1471799bbdbabae7721)

--- http/jaxrs-reactive/src/test/java/io/quarkus/ts/http/jaxrs/reactive/client/MultipartClientIT.java
+++ http/jaxrs-reactive/src/test/java/io/quarkus/ts/http/jaxrs/reactive/client/MultipartClientIT.java
@@ -7,12 +7,15 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.scenarios.annotations.DisabledOnQuarkusSnapshot;
 
 @Tag("QUARKUS-1225")
 @QuarkusScenario
 public class MultipartClientIT {
 
     @Test
+    //TODO: https://github.com/quarkusio/quarkus/issues/28782
+    @DisabledOnQuarkusSnapshot(reason = "multiplart form data request body is always null")
     public void testMultipartDataIsSent() {
         given()
                 .when().post("/client/multipart")
