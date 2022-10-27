71b24a3b643a9c0fc212b0e424a7b182e7d9ebcf
Fedor Dudinskiy <fdudinsk@redhat.com>
2022-10-27 10:14:15 +0200

    Add support for marking configs as secret
    Issue: https://issues.redhat.com/browse/QUARKUS-2479
    Test plans: https://github.com/quarkus-qe/quarkus-test-plans/pull/105

---
+++ config/src/main/java/io/quarkus/ts/configmap/api/server/secrets/BuiltConfigResource.java
@@ -0,0 +1,25 @@
+package io.quarkus.ts.configmap.api.server.secrets;
+
+import javax.ws.rs.Path;
+
+import io.smallrye.config.SmallRyeConfig;
+import io.smallrye.config.SmallRyeConfigBuilder;
+
+@Path("/built")
+public class BuiltConfigResource extends SecretResource {
+
+    private final SmallRyeConfig config;
+
+    public BuiltConfigResource() {
+        config = new SmallRyeConfigBuilder()
+                .addDefaultSources()
+                .addDefaultInterceptors()
+                .withSecretKeys("secret.ip")
+                .build();
+    }
+
+    @Override
+    public String getProperty(String key) {
+        return config.getRawValue(key);
+    }
+}
---
+++ config/src/main/java/io/quarkus/ts/configmap/api/server/secrets/InjectedProperitesResource.java
@@ -0,0 +1,31 @@
+package io.quarkus.ts.configmap.api.server.secrets;
+
+import javax.ws.rs.Path;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Path("/properties")
+public class InjectedProperitesResource extends SecretResource {
+    @ConfigProperty(name = "secret.password")
+    String password;
+
+    @ConfigProperty(name = "the.answer")
+    String answer;
+
+    @ConfigProperty(name = "secret.ip")
+    String ip;
+
+    @Override
+    public String getProperty(String key) {
+        switch (key) {
+            case "secret.password":
+                return password;
+            case "the.answer":
+                return answer;
+            case "secret.ip":
+                return ip;
+            default:
+                throw new IllegalStateException("Unexpected value: " + key);
+        }
+    }
+}
