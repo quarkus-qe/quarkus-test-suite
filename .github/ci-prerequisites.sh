7847c62743c13e1924d6f9b79f36dfd7c6799972
Rostislav Svoboda <rsvoboda@redhat.com>
2022-11-15 16:40:02 +0100

    Remove more unused docker images to free up space

--- .github/ci-prerequisites.sh
+++ .github/ci-prerequisites.sh
@@ -1,10 +1,10 @@
 # Reclaim disk space, otherwise we only have 13 GB free at the start of a job
 echo "Reclaim disk space."
 df -h
-time docker rmi node:10 node:12 mcr.microsoft.com/azure-pipelines/node8-typescript:latest
-# That is 18 GB
+docker images
+time docker rmi node:10 node:12 node:14 node:16 buildpack-deps:stretch buildpack-deps:buster buildpack-deps:bullseye mcr.microsoft.com/azure-pipelines/node8-typescript:latest
 time sudo rm -rf /usr/share/dotnet
-# That is 1.2 GB
 time sudo rm -rf /usr/share/swift
 echo "Reclaim disk space end."
 df -h
+docker images
