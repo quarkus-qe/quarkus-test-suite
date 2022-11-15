b3f5da8b2a693c248838d7b561e5a8a32052c972
Rostislav Svoboda <rsvoboda@redhat.com>
2022-11-15 17:08:44 +0100

    More cleanup v3

--- .github/ci-prerequisites.sh
+++ .github/ci-prerequisites.sh
@@ -4,15 +4,19 @@ df -h
 docker images
 time docker rmi node:12 node:14 node:16 buildpack-deps:stretch buildpack-deps:buster buildpack-deps:bullseye ubuntu:18.04 ubuntu:16.04 debian:10 debian:11 debian:9 moby/buildkit node:16-alpine node:14-alpine node:12-alpine alpine:3.14 alpine:3.15 alpine:3.16
 
-du -cskh /usr/share/dotnet /usr/share/swift
-time sudo rm -rf /usr/share/dotnet
-time sudo rm -rf /usr/share/swift
+du -cskh /usr/share/dotnet /usr/share/swift /usr/share/gradle-7.5.1
+sudo rm -rf /usr/share/dotnet
+sudo rm -rf /usr/share/swift
+sudo rm -rf /usr/share/gradle-7.5.1
 
 
-du -cskh /usr/share/*
-du -cskh /usr/*
+du -cskh /usr/bin/*
 
-du -cskh /*
+du -cskh /usr/local/*
+
+du -cskh /opt*
+
+
 
 echo "Reclaim disk space end."
 df -h
