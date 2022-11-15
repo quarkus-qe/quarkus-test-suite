a9b207027a88cb7f945261773002442376489457
Rostislav Svoboda <rsvoboda@redhat.com>
2022-11-15 17:10:47 +0100

    More cleanup v4

--- .github/ci-prerequisites.sh
+++ .github/ci-prerequisites.sh
@@ -1,6 +1,6 @@
 # Reclaim disk space, otherwise we only have 13 GB free at the start of a job
 echo "Reclaim disk space."
-df -h
+df -h /
 docker images
 time docker rmi node:12 node:14 node:16 buildpack-deps:stretch buildpack-deps:buster buildpack-deps:bullseye ubuntu:18.04 ubuntu:16.04 debian:10 debian:11 debian:9 moby/buildkit node:16-alpine node:14-alpine node:12-alpine alpine:3.14 alpine:3.15 alpine:3.16
 
@@ -19,5 +19,5 @@ du -cskh /opt/*
 
 
 echo "Reclaim disk space end."
-df -h
+df -h /
 docker images
