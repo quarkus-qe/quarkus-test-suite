a9a976a632dc66628e159e17f197fbc919ec7586
Josef Smrcka <jsmrcka@redhat.com>
2023-01-09 14:14:44 +0100

    Disable OpenShiftLifecycleApplicationIT test because of quarkus#29451
    OpenShift deployment doesn't generate command when arguments are
    provided.

--- lifecycle-application/src/test/java/io/quarkus/ts/lifecycle/OpenShiftLifecycleApplicationIT.java
+++ lifecycle-application/src/test/java/io/quarkus/ts/lifecycle/OpenShiftLifecycleApplicationIT.java
@@ -7,11 +7,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.scenarios.OpenShiftDeploymentStrategy;
 import io.quarkus.test.scenarios.OpenShiftScenario;
 
+@Disabled("https://github.com/quarkusio/quarkus/issues/29451")
 @OpenShiftScenario(deployment = OpenShiftDeploymentStrategy.UsingOpenShiftExtension)
 public class OpenShiftLifecycleApplicationIT extends LifecycleApplicationIT {
 
