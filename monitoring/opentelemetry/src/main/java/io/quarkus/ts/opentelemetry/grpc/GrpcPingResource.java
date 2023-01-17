c66ab26288553882dbfe3e2fb330873ad43f54fa
Rostislav Svoboda <rsvoboda@redhat.com>
2023-01-17 15:49:39 +0100

    gRPC and SSE coverage for OpenTelemetry
    (cherry picked from commit f327c602ba563ec01cc8297fc7554bd7c9b07ed7)

---
+++ monitoring/opentelemetry/src/main/java/io/quarkus/ts/opentelemetry/grpc/GrpcPingResource.java
@@ -0,0 +1,28 @@
+package io.quarkus.ts.opentelemetry.grpc;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import io.quarkus.example.PongRequest;
+import io.quarkus.example.PongServiceGrpc;
+import io.quarkus.grpc.GrpcClient;
+import io.quarkus.ts.opentelemetry.traceable.TraceableResource;
+
+@Path("/grpc-ping")
+public class GrpcPingResource extends TraceableResource {
+
+    @Inject
+    @GrpcClient("pong")
+    PongServiceGrpc.PongServiceBlockingStub pongClient;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getPing() {
+        recordTraceId();
+
+        return "ping " + pongClient.sayPong(PongRequest.newBuilder().build()).getMessage();
+    }
+}
