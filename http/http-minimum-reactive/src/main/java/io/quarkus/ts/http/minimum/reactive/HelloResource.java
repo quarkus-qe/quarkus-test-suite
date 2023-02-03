d473b4bcfa5105f47b68160e886bccd2ca442ce8
Rostislav Svoboda <rsvoboda@redhat.com>
2023-02-03 15:16:52 +0100

    Coverage for Path Param group name containing a dash (#1031)

--- http/http-minimum-reactive/src/main/java/io/quarkus/ts/http/minimum/reactive/HelloResource.java
+++ http/http-minimum-reactive/src/main/java/io/quarkus/ts/http/minimum/reactive/HelloResource.java
@@ -4,9 +4,11 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import io.smallrye.mutiny.Uni;
 
@@ -26,6 +28,12 @@ public class HelloResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Uni<Hello> getJson() {
         return Uni.createFrom().item(new Hello("hello"));
+    }
+
+    @GET
+    @Path("/foo/{something-with-dash:[A-Z0-9]{4}}")
+    public Response doSomething(@PathParam("something-with-dash") String param) {
+        return Response.noContent().build();
     }
 
 }
