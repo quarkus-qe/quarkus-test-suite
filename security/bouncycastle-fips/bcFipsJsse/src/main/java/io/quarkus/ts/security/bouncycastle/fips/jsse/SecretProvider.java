46aeacabc5edafbf87524387f2eb9ef44b1d580f
pablo gonzalez granados <pagonzal@redhat.com>
2023-02-08 18:46:06 +0100

    Add coverage for credential provider

---
+++ security/bouncycastle-fips/bcFipsJsse/src/main/java/io/quarkus/ts/security/bouncycastle/fips/jsse/SecretProvider.java
@@ -0,0 +1,23 @@
+package io.quarkus.ts.security.bouncycastle.fips.jsse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+
+import io.quarkus.arc.Unremovable;
+import io.quarkus.credentials.CredentialsProvider;
+
+@ApplicationScoped
+@Unremovable
+@Named("custom-secret-provider")
+public class SecretProvider implements CredentialsProvider {
+    @Override
+    public Map<String, String> getCredentials(String credentialsProviderName) {
+        Map<String, String> creds = new HashMap<>();
+        creds.put("key-store-password", "password");
+        creds.put("trust-store-password", "password");
+        return creds;
+    }
+}
