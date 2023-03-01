557d280a19d1e49fa30f431261416ff621eaaa2f
pablo gonzalez granados <pagonzal@redhat.com>
2023-03-01 18:04:59 +0100

    Due to a test framework issue we are going to reuse postgres docker container image

--- sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/PostgresqlDatabaseIT.java
+++ sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/PostgresqlDatabaseIT.java
@@ -11,8 +11,10 @@ public class PostgresqlDatabaseIT extends AbstractSqlDatabaseIT {
 
     static final int POSTGRESQL_PORT = 5432;
 
+    // TODO rename "postgresql" database to "database" once this issue is fixed
+    // quarkus-qe/quarkus-test-framework#641
     @Container(image = "${postgresql.latest.image}", port = POSTGRESQL_PORT, expectedLog = "listening on IPv4 address")
-    static PostgresqlService database = new PostgresqlService()
+    static PostgresqlService postgresql = new PostgresqlService()
             //fixme https://github.com/quarkus-qe/quarkus-test-framework/issues/455
             .withProperty("POSTGRES_USER", "user")
             .withProperty("POSTGRES_PASSWORD", "user")
@@ -20,7 +22,7 @@ public class PostgresqlDatabaseIT extends AbstractSqlDatabaseIT {
 
     @QuarkusApplication
     static RestService app = new RestService().withProperties("postgresql.properties")
-            .withProperty("quarkus.datasource.username", database.getUser())
-            .withProperty("quarkus.datasource.password", database.getPassword())
-            .withProperty("quarkus.datasource.jdbc.url", database::getJdbcUrl);
+            .withProperty("quarkus.datasource.username", postgresql.getUser())
+            .withProperty("quarkus.datasource.password", postgresql.getPassword())
+            .withProperty("quarkus.datasource.jdbc.url", postgresql::getJdbcUrl);
 }
