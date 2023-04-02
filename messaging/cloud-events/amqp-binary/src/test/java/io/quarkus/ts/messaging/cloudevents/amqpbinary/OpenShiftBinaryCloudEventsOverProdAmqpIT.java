36aab6dc891603cf36ee546274b38f037f19adb5
Michal Jurč <mjurc@redhat.com>
2023-04-02 19:48:46 +0200

    Disabling tests using RH Streams Kafka on ARM
    * Disabling tests using RH Streams Kafka on ARM on OpenShift as the
      standaolne container doesn't yet have a build supporting the
      architecture.

--- messaging/cloud-events/amqp-binary/src/test/java/io/quarkus/ts/messaging/cloudevents/amqpbinary/OpenShiftBinaryCloudEventsOverProdAmqpIT.java
+++ messaging/cloud-events/amqp-binary/src/test/java/io/quarkus/ts/messaging/cloudevents/amqpbinary/OpenShiftBinaryCloudEventsOverProdAmqpIT.java
@@ -1,7 +1,10 @@
 package io.quarkus.ts.messaging.cloudevents.amqpbinary;
 
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+
 import io.quarkus.test.scenarios.OpenShiftScenario;
 
 @OpenShiftScenario
+@DisabledIfSystemProperty(named = "ts.arm.missing.services.excludes", matches = "true", disabledReason = "https://github.com/quarkus-qe/quarkus-test-suite/issues/1144")
 public class OpenShiftBinaryCloudEventsOverProdAmqpIT extends BinaryCloudEventsOverProdAmqpIT {
 }
