9296b2c8b702f7ba32e03b115fb1d8e2123672f2
Michal Vavřík <mvavrik@redhat.com>
2023-04-21 19:54:07 +0200

    Expect remote DEV UI to be avaialable

--- http/http-advanced-reactive/src/test/java/io/quarkus/ts/http/advanced/reactive/RemoteDevModeHttpAdvancedReactiveIT.java
+++ http/http-advanced-reactive/src/test/java/io/quarkus/ts/http/advanced/reactive/RemoteDevModeHttpAdvancedReactiveIT.java
@@ -4,7 +4,6 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.bootstrap.RestService;
@@ -25,9 +24,8 @@ public class RemoteDevModeHttpAdvancedReactiveIT {
         given().get("/api/hello").then().statusCode(HttpStatus.SC_OK).body("content", is("Hello, World!"));
     }
 
-    @Tag("QUARKUS-834")
     @Test
-    public void devUiShouldBeNotFound() {
-        app.given().get("/q/dev").then().statusCode(HttpStatus.SC_NOT_FOUND);
+    public void devUiShouldBeAvailable() {
+        app.given().get("/q/dev-ui").then().statusCode(HttpStatus.SC_OK);
     }
 }
