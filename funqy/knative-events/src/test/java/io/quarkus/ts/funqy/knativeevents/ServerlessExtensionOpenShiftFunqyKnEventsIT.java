d5398e61173e5a3c8d1edeb83efa932f328df6c4
Michal Vavřík <mvavrik@redhat.com>
2023-04-23 21:54:41 +0200

    Enable and refactor Funqy Knative test coverage

--- funqy/knative-events/src/test/java/io/quarkus/ts/funqy/knativeevents/ServerlessExtensionOpenShiftFunqyKnEventsIT.java
+++ funqy/knative-events/src/test/java/io/quarkus/ts/funqy/knativeevents/ServerlessExtensionOpenShiftFunqyKnEventsIT.java
@@ -1,32 +1,24 @@
 package io.quarkus.ts.funqy.knativeevents;
 
 import static io.quarkus.test.utils.AwaitilityUtils.untilIsTrue;
-import static io.quarkus.test.utils.AwaitilityUtils.AwaitilitySettings.usingTimeout;
 import static io.quarkus.ts.funqy.knativeevents.Constants.ENV_VAR_NAME;
 import static io.quarkus.ts.funqy.knativeevents.Constants.ENV_VAR_VALUE;
 import static io.quarkus.ts.funqy.knativeevents.Constants.PING_RESPONSE_TYPE;
 import static io.quarkus.ts.funqy.knativeevents.Constants.PING_TRIGGER;
 import static java.lang.String.format;
-import static java.time.Duration.ofSeconds;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
-import java.util.Base64;
 import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.inject.Inject;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 
-import io.fabric8.knative.sources.v1.PingSourceBuilder;
-import io.quarkus.test.bootstrap.Action;
-import io.quarkus.test.bootstrap.Service;
-import io.quarkus.test.bootstrap.inject.OpenShiftClient;
 import io.quarkus.test.scenarios.OpenShiftDeploymentStrategy;
 import io.quarkus.test.scenarios.OpenShiftScenario;
 import io.quarkus.test.services.QuarkusApplication;
@@ -35,44 +27,16 @@ import io.quarkus.test.services.knative.eventing.OpenShiftExtensionFunqyKnativeE
 import io.quarkus.test.services.knative.eventing.spi.ForwardResponseDTO;
 import io.restassured.common.mapper.TypeRef;
 
+@DisabledIfSystemProperty(named = "ts.arm.missing.services.excludes", matches = "true", disabledReason = "https://github.com/quarkus-qe/quarkus-test-suite/issues/1142")
 @Tag("use-quarkus-openshift-extension")
 @Tag("serverless")
 @OpenShiftScenario(deployment = OpenShiftDeploymentStrategy.UsingOpenShiftExtension)
 public class ServerlessExtensionOpenShiftFunqyKnEventsIT {
 
-    private static final String IN_AS_BASE_64 = Base64.getEncoder().encodeToString(new byte[] { 0x12, 0x34, 0x56, 0x78 });
-
-    @Inject
-    static OpenShiftClient openShiftClient;
-
     @QuarkusApplication
     static FunqyKnativeEventsService service = new OpenShiftExtensionFunqyKnativeEventsService()
-            .onPreStart(new Action() {
-                @Override
-                public void handle(Service service) {
-                    // create PingSource with sink binding to our Knative service
-                    // PingSource will send a message that will be received by "fallback" function
-                    openShiftClient
-                            .getKnClient()
-                            .pingSources()
-                            .create(new PingSourceBuilder()
-                                    .withNewMetadata().withName("test-ping-source").endMetadata()
-                                    .withNewSpec()
-                                    .withDataBase64(IN_AS_BASE_64)
-                                    .withContentType("application/octet-stream")
-                                    .withNewSink()
-                                    .withNewRef()
-                                    .withApiVersion("v1")
-                                    .withKind("Service")
-                                    .withName(service.getName())
-                                    .endRef()
-                                    .endSink()
-                                    .endSpec()
-                                    .build());
-                }
-            })
             .withDefaultBroker()
-            .withTrigger().name("fallback").defaultBroker().filterCloudEventType("*").endTrigger()
+            .withTrigger().name("unknown").defaultBroker().filterCloudEventType("unknown").endTrigger()
             .withTrigger().name(PING_TRIGGER).defaultBroker().filterCloudEventType(PING_TRIGGER).endTrigger()
             .withTrigger().name("pong").defaultBroker().filterCloudEventType(PING_RESPONSE_TYPE).endTrigger()
             .withTrigger().name("pung").defaultBroker().filterCloudEventType("pong-type").endTrigger()
@@ -82,6 +46,7 @@ public class ServerlessExtensionOpenShiftFunqyKnEventsIT {
     @Test
     public void testComplexChain() {
         final Set<ValidationResult> validationResults = new HashSet<>();
+        final var expected = new HashSet<>(EnumSet.allOf(ValidationResult.Functions.class));
 
         // request validation results
         untilIsTrue(() -> {
@@ -100,13 +65,10 @@ public class ServerlessExtensionOpenShiftFunqyKnEventsIT {
                 validationResults.addAll(response);
             }
 
-            return !validationResults.isEmpty();
-        },
-                // Knative Eventing PingSource triggers every minute (worst case scenario), rest is for chain processing
-                usingTimeout(ofSeconds(100)));
+            return validationResults.size() >= expected.size();
+        });
 
         // assert results
-        final var expected = new HashSet<>(EnumSet.allOf(ValidationResult.Functions.class));
         validationResults.forEach(result -> {
             // assert no function was invoked more than once
             if (!expected.contains(result.getInvokedFunction())) {
