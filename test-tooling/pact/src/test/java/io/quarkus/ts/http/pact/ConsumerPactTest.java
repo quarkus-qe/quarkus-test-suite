65a72915f3b3fc807deae7abf058a6d85a4e2a75
Michal Vavřík <mvavrik@redhat.com>
2023-05-18 14:06:28 +0200

    Migrate Test Tooling Pact to Quarkiverse extension

--- test-tooling/pact/src/test/java/io/quarkus/ts/http/pact/ConsumerPactTest.java
+++ test-tooling/pact/src/test/java/io/quarkus/ts/http/pact/ConsumerPactTest.java
@@ -1,23 +1,22 @@
 package io.quarkus.ts.http.pact;
 
 import java.util.HashMap;
-import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import io.pactfoundation.consumer.dsl.LambdaDsl;
 import io.quarkus.test.junit.QuarkusTest;
 
 import au.com.dius.pact.consumer.MessagePactBuilder;
+import au.com.dius.pact.consumer.dsl.LambdaDsl;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.consumer.junit5.ProviderType;
+import au.com.dius.pact.core.model.V4Interaction;
+import au.com.dius.pact.core.model.V4Pact;
 import au.com.dius.pact.core.model.annotations.Pact;
-import au.com.dius.pact.core.model.messaging.Message;
-import au.com.dius.pact.core.model.messaging.MessagePact;
 
 @QuarkusTest
 @Tag("QUARKUS-1024")
@@ -26,7 +25,7 @@ import au.com.dius.pact.core.model.messaging.MessagePact;
 public class ConsumerPactTest {
 
     @Pact(consumer = "TheConsumer")
-    public MessagePact createPact(MessagePactBuilder builder) {
+    public V4Pact createPact(MessagePactBuilder builder) {
         var body = LambdaDsl.newJsonBody((it) -> {
             it.stringType("payload", "Hello there");
         }).build();
@@ -35,12 +34,11 @@ public class ConsumerPactTest {
                 .expectsToReceive("a message with a payload")
                 .withMetadata(new HashMap<>())
                 .withContent(body)
-                .toPact();
+                .toPact(V4Pact.class);
     }
 
     @Test
-    void test(List<Message> messages) {
-        Assertions.assertEquals(1, messages.size());
-        Assertions.assertEquals("{\"payload\":\"Hello there\"}", messages.get(0).contentsAsString());
+    void test(V4Interaction.AsynchronousMessage messages) {
+        Assertions.assertEquals("{\"payload\":\"Hello there\"}", messages.contentsAsString());
     }
 }
--- test-tooling/pact/src/test/java/io/quarkus/ts/http/pact/ProviderPactTest.java
+++ test-tooling/pact/src/test/java/io/quarkus/ts/http/pact/ProviderPactTest.java
@@ -1,7 +1,6 @@
 package io.quarkus.ts.http.pact;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,10 +14,6 @@ import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvide
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
 
-// TODO: enable test once we migrate to Quarkus Pact extension, which will be possible after Quarkus 3 is released
-// I believe nature of the failure is identical to https://github.com/quarkiverse/quarkus-pact/issues/73
-// and failure goes down to class loading changes in Quarkus 3
-@Disabled("Disabled until Quarkus Pact 3 is released and we migrate pact module to that extension")
 @QuarkusTest
 @Tag("QUARKUS-1024")
 @Provider("TheProvider")
