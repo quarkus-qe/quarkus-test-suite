db275e192843edbb15a8439db385acd505473c94
Georgii Troitskii <gtroitsk@redhat.com>
2023-05-24 18:54:31 +0200

    Remove PostgreSQL image workaround
    Issue https://github.com/quarkus-qe/quarkus-test-framework/issues/641 was solved

--- sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/XAPostgresIT.java
+++ sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/XAPostgresIT.java
@@ -11,18 +11,16 @@ public class XAPostgresIT extends AbstractSqlDatabaseIT {
 
     static final int POSTGRESQL_PORT = 5432;
 
-    // TODO rename "postgresql" database to "database" once this issue is fixed
-    // quarkus-qe/quarkus-test-framework#641
     @Container(image = "${postgresql.latest.image}", port = POSTGRESQL_PORT, expectedLog = "listening on IPv4 address")
-    static PostgresqlService postgresql = new PostgresqlService()
+    static PostgresqlService database = new PostgresqlService()
             .withUser("user")
             .withPassword("user")
             .withDatabase("mydb");
 
     @QuarkusApplication
     static RestService app = new RestService().withProperties("postgresql.properties")
-            .withProperty("quarkus.datasource.username", postgresql.getUser())
-            .withProperty("quarkus.datasource.password", postgresql.getPassword())
+            .withProperty("quarkus.datasource.username", database.getUser())
+            .withProperty("quarkus.datasource.password", database.getPassword())
             .withProperty("quarkus.datasource.jdbc.transactions", "xa")
-            .withProperty("quarkus.datasource.jdbc.url", postgresql::getJdbcUrl);
+            .withProperty("quarkus.datasource.jdbc.url", database::getJdbcUrl);
 }
