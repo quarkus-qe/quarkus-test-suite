4d97262485f666b3e661cac896dcc2e9999d4e9d
Fedor Dudinskiy <fdudinsk@redhat.com>
2023-07-17 13:28:26 +0200

    Fix daily and weekly runs
    - Disable test with a known bug
    - Distribute the load of native runs of sql-db module between two actions

--- service-binding/postgresql-crunchy-classic/src/test/java/io/quarkus/ts/sb/postgresql/OpenShiftPostgreSqlSbIT.java
+++ service-binding/postgresql-crunchy-classic/src/test/java/io/quarkus/ts/sb/postgresql/OpenShiftPostgreSqlSbIT.java
@@ -11,6 +11,7 @@ import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.bootstrap.RestService;
@@ -21,6 +22,7 @@ import io.quarkus.test.services.QuarkusApplication;
 import io.quarkus.test.utils.Command;
 
 @OpenShiftScenario(deployment = OpenShiftDeploymentStrategy.UsingOpenShiftExtensionAndDockerBuildStrategy)
+@Disabled("https://github.com/quarkusio/quarkus/issues/34759")
 public class OpenShiftPostgreSqlSbIT {
 
     @Inject
