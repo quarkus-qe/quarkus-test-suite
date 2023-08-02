ce82773c3ea60f0bc826725650634d1a9bf20b08
Rostislav Svoboda <rsvoboda@redhat.com>
2023-08-02 11:50:49 +0200

    .

--- .github/ci-prerequisites.sh
+++ .github/ci-prerequisites.sh
@@ -2,27 +2,29 @@
 echo "Reclaim disk space."
 df -h /
 docker images
-time docker rmi node:12 node:14 node:16 buildpack-deps:stretch buildpack-deps:buster buildpack-deps:bullseye ubuntu:18.04 ubuntu:16.04 debian:10 debian:11 debian:9 moby/buildkit node:16-alpine node:14-alpine node:12-alpine alpine:3.14 alpine:3.15 alpine:3.16
+time docker rmi node:12 node:14 node:16 buildpack-deps:buster buildpack-deps:bullseye ubuntu:18.04 ubuntu:20.04 debian:10 debian:11 moby/buildkit node:16-alpine node:14-alpine alpine:3.16 alpine:3.17
 
 du -cskh /usr/share/dotnet /usr/share/swift /usr/share/gradle-*
 sudo rm -rf /usr/share/dotnet
 sudo rm -rf /usr/share/swift
 sudo rm -rf /usr/share/gradle-*
 
-du -cskh /opt/az /opt/google /opt/hhvm /opt/hostedtoolcache/CodeQL /opt/microsoft /usr/local/graalvm /usr/local/julia* /usr/local/lib/android /opt/ghc /opt/pipx
+du -cskh /opt/az /opt/google /opt/hostedtoolcache/CodeQL /opt/microsoft /usr/local/julia* /usr/local/lib/android /opt/ghc
 sudo rm -rf /opt/az
 sudo rm -rf /opt/google
-sudo rm -rf /opt/hhvm
 sudo rm -rf /opt/hostedtoolcache/CodeQL
 sudo rm -rf /opt/microsoft
-sudo rm -rf /usr/local/graalvm
 sudo rm -rf /usr/local/julia*
+
+du -cskh /usr/local/lib/android /opt/ghc
 sudo rm -rf /usr/local/lib/android
-sudo rm -rf /opt/ghc
 sudo rm -rf /opt/pipx
 
 echo "Reclaim disk space end."
 df -h /
 docker images
 
-du -cskh /* 2>/dev/null
+du -cskh /home
+du -cskh /imagegeneration
+du -cskh /mnt
+du -cskh /opt
