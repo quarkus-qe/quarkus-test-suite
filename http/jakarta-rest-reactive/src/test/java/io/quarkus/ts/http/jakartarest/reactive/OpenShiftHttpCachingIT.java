f3dd42c5b58261efb4edaa7e51109d19298ec524
mocenas <mocenas@redhat.com>
2023-08-16 08:47:06 +0200

    Disable test affected by issue 35344 (#1363)
    Issue https://github.com/quarkusio/quarkus/issues/35344.

--- http/jakarta-rest-reactive/src/test/java/io/quarkus/ts/http/jakartarest/reactive/OpenShiftHttpCachingIT.java
+++ http/jakarta-rest-reactive/src/test/java/io/quarkus/ts/http/jakartarest/reactive/OpenShiftHttpCachingIT.java
@@ -1,5 +1,6 @@
 package io.quarkus.ts.http.jakartarest.reactive;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 
 import io.quarkus.test.scenarios.OpenShiftDeploymentStrategy;
@@ -7,5 +8,6 @@ import io.quarkus.test.scenarios.OpenShiftScenario;
 
 @Tag("use-quarkus-openshift-extension")
 @OpenShiftScenario(deployment = OpenShiftDeploymentStrategy.UsingOpenShiftExtension)
+@Disabled("https://github.com/quarkusio/quarkus/issues/35344")
 public class OpenShiftHttpCachingIT extends HttpCachingIT {
 }
