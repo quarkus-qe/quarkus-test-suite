016ab9b0c5e93611ec71a44932a2c91af58ddff8
Michal Jurč <mjurc@redhat.com>
2023-08-30 22:10:53 +0200

    Disabling registry tests on OCP on aarch64
    * It is currently impossible to run registry tests on aarch64. The test
      builds the application container on machine where the test is ran,
      pushes it to quay.io and deploys the container to OpenShift. So, in
      in our case it means the container is built for x86_64 arch, but we
      need aarch64.

--- http/management/src/test/java/io/quarkus/qe/extension/OpenShiftRegistryIT.java
+++ http/management/src/test/java/io/quarkus/qe/extension/OpenShiftRegistryIT.java
@@ -1,8 +1,11 @@
 package io.quarkus.qe.extension;
 
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+
 import io.quarkus.test.scenarios.OpenShiftDeploymentStrategy;
 import io.quarkus.test.scenarios.OpenShiftScenario;
 
+@DisabledIfSystemProperty(named = "ts.arm.missing.services.excludes", matches = "true", disabledReason = "Impossible to deploy container built on x86_64 on aarch64.")
 @OpenShiftScenario(deployment = OpenShiftDeploymentStrategy.UsingContainerRegistry)
 public class OpenShiftRegistryIT extends OpenShiftBaseDeploymentIT {
 }
