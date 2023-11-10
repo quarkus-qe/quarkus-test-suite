3aa0a3db8e05e88dc2ce9d7554fd5089d75190fb
Rostislav Svoboda <rsvoboda@redhat.com>
2023-11-10 09:55:53 +0100

    Disable ServerSentEventsTraceOpentracingIT in monitoring/opentracing-reactive-grpc

--- monitoring/opentracing-reactive-grpc/src/test/java/io/quarkus/ts/monitoring/opentracing/reactive/grpc/ServerSentEventsTraceOpentracingIT.java
+++ monitoring/opentracing-reactive-grpc/src/test/java/io/quarkus/ts/monitoring/opentracing/reactive/grpc/ServerSentEventsTraceOpentracingIT.java
@@ -1,8 +1,11 @@
 package io.quarkus.ts.monitoring.opentracing.reactive.grpc;
 
+import org.junit.jupiter.api.Disabled;
+
 import io.quarkus.test.scenarios.QuarkusScenario;
 
 @QuarkusScenario
+@Disabled("Hitting RESTEasy Classic limits with SSE, details https://github.com/quarkusio/quarkus/issues/36979")
 public class ServerSentEventsTraceOpentracingIT extends AbstractTraceIT {
 
     @Override
