23c55be357d10a2da5a7048ced16b04585799eed
Michal Vavřík <mvavrik@redhat.com>
2023-11-28 10:45:48 +0100

    Fix PG service binding on OCP 4.14

--- service-binding/postgresql-crunchy-reactive/src/test/java/io/quarkus/ts/sb/reactive/OpenShiftPostgreSqlReactiveSbIT.java
+++ service-binding/postgresql-crunchy-reactive/src/test/java/io/quarkus/ts/sb/reactive/OpenShiftPostgreSqlReactiveSbIT.java
@@ -1,9 +1,13 @@
 package io.quarkus.ts.sb.reactive;
 
+import static io.quarkus.test.utils.AwaitilityUtils.untilAsserted;
+import static io.quarkus.test.utils.AwaitilityUtils.AwaitilitySettings.using;
+import static java.time.Duration.ofSeconds;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import jakarta.inject.Inject;
 
@@ -11,7 +15,6 @@ import org.apache.http.HttpStatus;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.bootstrap.RestService;
@@ -51,22 +54,23 @@ public class OpenShiftPostgreSqlReactiveSbIT {
                 .body("title", Matchers.equalTo("Finish the blog post"));
     }
 
-    private static boolean areRequiredOperatorsInstalled() {
-        List<String> output = new ArrayList<>();
-        try {
-            // TODO: figure out a better way to wait for this - this wait is necessary as it takes some time for API to
-            //       populate new namespace with objects
-            Thread.sleep(2000);
-            new Command("oc", "get", "csv").outputToLines(output).runAndWait();
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage(), e);
-        }
-        String outputString = output.stream().collect(Collectors.joining(System.lineSeparator()));
-        return (outputString.contains("postgresoperator") && outputString.contains("service-binding-operator"));
+    private static void assertRequiredOperatorsInstalled() {
+        untilAsserted(() -> {
+            List<String> output = new ArrayList<>();
+            try {
+                // TODO: figure out a better way to wait for this - this wait is necessary as it takes some
+                //  time for API to populate new namespace with objects
+                new Command("oc", "get", "csv").outputToLines(output).runAndWait();
+            } catch (Exception e) {
+                throw new RuntimeException(e.getMessage(), e);
+            }
+            assertTrue(output.stream().anyMatch(str -> str.contains("postgresoperator")));
+            assertTrue(output.stream().anyMatch(str -> str.contains("service-binding-operator")));
+        }, using(ofSeconds(2), ofSeconds(60)));
     }
 
     private static void createPostgresCluster() {
-        Assumptions.assumeTrue(areRequiredOperatorsInstalled());
+        assertRequiredOperatorsInstalled();
         applyCustomResourceDefinition();
         try {
             // TODO: figure out a better way to wait for this - sometimes operator takes a while to create object
