f43cf385d06a873d3a76cfd6a2d9cf02fc557ab8
Martin Ocenas <mocenas@redhat.com>
2024-01-12 11:00:18 +0100

    Disable tests affected by issue 38003
    Link to the issue https://github.com/quarkusio/quarkus/issues/38003.

--- sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/DevModeOracleIT.java
+++ sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/DevModeOracleIT.java
@@ -1,5 +1,6 @@
 package io.quarkus.ts.sqldb.sqlapp;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.bootstrap.RestService;
@@ -7,6 +8,7 @@ import io.quarkus.test.scenarios.QuarkusScenario;
 import io.quarkus.test.services.DevModeQuarkusApplication;
 
 @QuarkusScenario
+@Tag("podman-incompatible") //TODO: https://github.com/quarkusio/quarkus/issues/38003
 public class DevModeOracleIT extends AbstractSqlDatabaseIT {
 
     @DevModeQuarkusApplication
