56c07922218634662fedb5263eec9d53eeb5d082
Jakub Jedlicka <jjedlick@redhat.com>
2024-02-21 13:44:11 +0100

    Add MySQL XA_RECOVER_ADMIN permission to baremetal naryana tests

--- sql-db/narayana-transactions/src/test/java/io/quarkus/ts/transactions/MysqlTransactionGeneralUsageIT.java
+++ sql-db/narayana-transactions/src/test/java/io/quarkus/ts/transactions/MysqlTransactionGeneralUsageIT.java
@@ -1,6 +1,11 @@
 package io.quarkus.ts.transactions;
 
+import static io.quarkus.test.services.containers.DockerContainerManagedResource.DOCKER_INNER_CONTAINER;
+
+import java.io.IOException;
+
 import org.junit.jupiter.api.Tag;
+import org.testcontainers.containers.GenericContainer;
 
 import io.quarkus.test.bootstrap.MySqlService;
 import io.quarkus.test.bootstrap.RestService;
@@ -15,9 +20,22 @@ import io.quarkus.ts.transactions.recovery.TransactionExecutor;
 public class MysqlTransactionGeneralUsageIT extends TransactionCommons {
 
     static final int MYSQL_PORT = 3306;
-
     @Container(image = "${mysql.80.image}", port = MYSQL_PORT, expectedLog = "port: " + MYSQL_PORT)
-    static final MySqlService database = new MySqlService();
+    static final MySqlService database = new MySqlService().onPostStart(service -> {
+        // enable transactions recovery for user
+        var self = (MySqlService) service;
+        // RH MySQL image allow login root user locally without using password and ignoring set root password
+        String passwordString = System.getProperty("mysql.80.image").contains("redhat") ? "" : "-p" + self.getPassword();
+        try {
+            self
+                    .<GenericContainer<?>> getPropertyFromContext(DOCKER_INNER_CONTAINER)
+                    .execInContainer(
+                            "/bin/mysql", "-u", "root", passwordString, "-e",
+                            "grant XA_RECOVER_ADMIN on *.* to '" + self.getUser() + "'@'%';");
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    });
 
     @QuarkusApplication
     static RestService app = new RestService().withProperties("mysql.properties")
