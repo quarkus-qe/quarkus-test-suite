b25899795182d1080496e00602557105b31871f8
Fedor Dudinsky <fdudinsk@redhat.com>
2024-02-26 13:41:57 +0100

    Add new module, which covers GRPS, expecially with TLS
    https://issues.redhat.com/browse/QQE-404

---
+++ http/grpc/src/test/java/io/quarkus/ts/http/grpc/OpenShiftExtensionGRPCIT.java
@@ -0,0 +1,25 @@
+package io.quarkus.ts.http.grpc;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+
+import io.grpc.Channel;
+import io.quarkus.test.bootstrap.RestService;
+import io.quarkus.test.scenarios.OpenShiftDeploymentStrategy;
+import io.quarkus.test.scenarios.OpenShiftScenario;
+
+@Tag("use-quarkus-openshift-extension")
+@OpenShiftScenario(deployment = OpenShiftDeploymentStrategy.UsingOpenShiftExtension)
+@Disabled("https://github.com/quarkus-qe/quarkus-test-framework/issues/1052+1053")
+public class OpenShiftExtensionGRPCIT implements GRPCIT {
+
+    @Override
+    public RestService app() {
+        return null;
+    }
+
+    @Override
+    public Channel getChannel() {
+        return null;
+    }
+}
---
+++ http/grpc/src/test/java/io/quarkus/ts/http/grpc/TLSIT.java
@@ -0,0 +1,59 @@
+package io.quarkus.ts.http.grpc;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.jupiter.api.AfterAll;
+
+import io.grpc.Channel;
+import io.grpc.ChannelCredentials;
+import io.grpc.Grpc;
+import io.grpc.ManagedChannel;
+import io.grpc.TlsChannelCredentials;
+import io.quarkus.test.bootstrap.GrpcService;
+import io.quarkus.test.bootstrap.Protocol;
+import io.quarkus.test.bootstrap.RestService;
+import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.services.QuarkusApplication;
+
+@QuarkusScenario
+public class TLSIT implements GRPCIT {
+
+    private static ManagedChannel channel;
+    @QuarkusApplication(grpc = true, ssl = true)
+    // See https://github.com/quarkusio/quarkus/issues/38965 to learn, why we use these parameters
+    static final GrpcService app = (GrpcService) new GrpcService()
+            .withProperty("quarkus.grpc.clients.plain.port", "${quarkus.http.ssl-port}")
+            .withProperty("quarkus.http.ssl.certificate.files", "tls/server.pem")
+            .withProperty("quarkus.http.ssl.certificate.key-files", "tls/server.key")
+            .withProperty("quarkus.grpc.server.ssl.certificate", "tls/server.pem")
+            .withProperty("quarkus.grpc.server.ssl.key", "tls/server.key")
+            .withProperty("quarkus.grpc.clients.plain.ssl.trust-store", "tls/ca.pem");
+
+    public Channel getChannel() {
+        if (channel != null) {
+            return channel;
+        }
+        try (InputStream caCertificate = app.getClass().getClassLoader().getResourceAsStream("tls/ca.pem")) {
+            ChannelCredentials credentials = TlsChannelCredentials.newBuilder()
+                    .trustManager(caCertificate)
+                    .build();
+            channel = Grpc.newChannelBuilderForAddress(app().getURI(Protocol.GRPC).getHost(),
+                    app().getURI(Protocol.HTTPS).getPort(), credentials)
+                    .build();
+            return channel;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public RestService app() {
+        return app;
+    }
+
+    @AfterAll
+    static void afterAll() {
+        channel.shutdown();
+    }
+}
