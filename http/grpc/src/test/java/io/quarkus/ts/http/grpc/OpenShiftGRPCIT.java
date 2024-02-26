89626fb8b642c81948a5a1c09eb00352498b12d6
Fedor Dudinsky <fdudinsk@redhat.com>
2024-02-26 17:25:35 +0100

    Coverage for GRPC streaming scenarios
    https://issues.redhat.com/browse/QQE-404

--- http/grpc/src/test/java/io/quarkus/ts/http/grpc/OpenShiftGRPCIT.java
+++ http/grpc/src/test/java/io/quarkus/ts/http/grpc/OpenShiftGRPCIT.java
@@ -8,7 +8,7 @@ import io.quarkus.test.scenarios.OpenShiftScenario;
 
 @OpenShiftScenario
 @Disabled("https://github.com/quarkus-qe/quarkus-test-framework/issues/1052+1053")
-public class OpenShiftGRPCIT implements GRPCIT {
+public class OpenShiftGRPCIT implements GRPCIT, StreamingHttpIT {
 
     @Override
     public RestService app() {
