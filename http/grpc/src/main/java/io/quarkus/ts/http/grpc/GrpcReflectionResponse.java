370d455509dee452dfa88ffb2e22d3fdb75c50ab
Fedor Dudinsky <fdudinsk@redhat.com>
2024-02-27 09:28:15 +0100

    Move ssl-specific parameters from test properties to a separate profile
    This is needed to fix native failures in our CI.
    Also, fix issues from comments to the PR: https://github.com/quarkus-qe/quarkus-test-suite/pull/1691

--- http/grpc/src/main/java/io/quarkus/ts/http/grpc/GrpcReflectionResponse.java
+++ http/grpc/src/main/java/io/quarkus/ts/http/grpc/GrpcReflectionResponse.java
@@ -2,9 +2,6 @@ package io.quarkus.ts.http.grpc;
 
 import java.util.List;
 
-import io.quarkus.runtime.annotations.RegisterForReflection;
-
-@RegisterForReflection
 public final class GrpcReflectionResponse {
     private final int serviceCount;
     private final List<String> serviceList;
