1b4583be2373ff972bd5ec42d1f3b1d5cc5b6aea
Fedor Dudinsky <fdudinsk@redhat.com>
2024-03-01 17:39:58 +0100

    Add ability to grants rights on OpenShift
    https://issues.redhat.com/browse/QQE-492
    Fixes https://github.com/quarkus-qe/quarkus-test-suite/issues/1397

---
+++ sql-db/narayana-transactions/src/main/java/io/quarkus/ts/service/ServiceResource.java
@@ -0,0 +1,28 @@
+package io.quarkus.ts.service;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+import io.agroal.api.AgroalDataSource;
+
+@Path("/service")
+public class ServiceResource {
+    @Inject
+    AgroalDataSource defaultDataSource;
+
+    @Path("/grant/{user}")
+    @POST
+    public Response grant(String user) throws SQLException {
+        try (Statement statement = defaultDataSource.getConnection().createStatement()) {
+            Response.Status status = statement.execute("grant XA_RECOVER_ADMIN on *.* to '" + user + "'@'%';")
+                    ? Response.Status.CREATED
+                    : Response.Status.NO_CONTENT;
+            return Response.status(status).build();
+        }
+    }
+}
