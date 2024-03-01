de455783f74a903c2e2a63c55b921cfdc8aaae5d
Fedor Dudinsky <fdudinsk@redhat.com>
2024-03-01 17:39:58 +0100

    Add ability to grants rights on OpenShift
    https://issues.redhat.com/browse/QQE-492
    Fixes https://github.com/quarkus-qe/quarkus-test-suite/issues/1397

--- sql-db/narayana-transactions/src/test/java/io/quarkus/ts/transactions/OpenShiftMysqlTransactionGeneralUsageIT.java
+++ sql-db/narayana-transactions/src/test/java/io/quarkus/ts/transactions/OpenShiftMysqlTransactionGeneralUsageIT.java
@@ -1,5 +1,6 @@
 package io.quarkus.ts.transactions;
 
+import org.jboss.logging.Logger;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 
 import io.quarkus.test.bootstrap.MySqlService;
@@ -12,10 +13,21 @@ import io.quarkus.ts.transactions.recovery.TransactionExecutor;
 @OpenShiftScenario
 @EnabledIfSystemProperty(named = "ts.redhat.registry.enabled", matches = "true")
 public class OpenShiftMysqlTransactionGeneralUsageIT extends TransactionCommons {
+    private static final Logger LOG = Logger.getLogger(OpenShiftMysqlTransactionGeneralUsageIT.class);
     static final int MYSQL_PORT = 3306;
 
     @Container(image = "${mysql.80.image}", port = MYSQL_PORT, expectedLog = "Only MySQL server logs after this point")
     static MySqlService database = new MySqlService();
+
+    @QuarkusApplication
+    static RestService granter = new RestService().onPostStart(service -> {
+        RestService app = (RestService) service;
+        app.given().post("/service/grant/" + database.getUser());
+    })
+            .withProperty("quarkus.datasource.db-kind", "mysql")
+            .withProperty("quarkus.datasource.username", "root")
+            .withProperty("quarkus.datasource.password", database.getPassword())
+            .withProperty("quarkus.datasource.jdbc.url", database::getJdbcUrl);
 
     @QuarkusApplication
     static RestService app = new RestService().withProperties("mysql.properties")
@@ -32,10 +44,5 @@ public class OpenShiftMysqlTransactionGeneralUsageIT extends TransactionCommons 
     @Override
     protected TransactionExecutor getTransactionExecutorUsedForRecovery() {
         return TransactionExecutor.STATIC_USER_TRANSACTION;
-    }
-
-    @Override
-    protected void testTransactionRecoveryInternal() {
-        // FIXME: enable when https://github.com/quarkus-qe/quarkus-test-suite/issues/1397 gets fixed
     }
 }
