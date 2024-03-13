e1868d7f2be7d5e1b465d9095d28e88b97e27974
Fedor Dudinsky <fdudinsk@redhat.com>
2024-03-13 13:55:24 +0100

    Change waiting rules for operators
    These tests wait for operators to be ready.
    Earlier, they used Thread.sleep, which sometimes was not enough.
    These tests fail several times on OCP 4-15, so now they use awaitility

--- service-binding/postgresql-crunchy-classic/src/test/java/io/quarkus/ts/sb/postgresql/OpenShiftPostgreSqlSbIT.java
+++ service-binding/postgresql-crunchy-classic/src/test/java/io/quarkus/ts/sb/postgresql/OpenShiftPostgreSqlSbIT.java
@@ -1,6 +1,7 @@
 package io.quarkus.ts.sb.postgresql;
 
 import java.nio.file.Paths;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,7 +11,6 @@ import jakarta.inject.Inject;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.bootstrap.RestService;
@@ -18,6 +18,7 @@ import io.quarkus.test.bootstrap.inject.OpenShiftClient;
 import io.quarkus.test.scenarios.OpenShiftDeploymentStrategy;
 import io.quarkus.test.scenarios.OpenShiftScenario;
 import io.quarkus.test.services.QuarkusApplication;
+import io.quarkus.test.utils.AwaitilityUtils;
 import io.quarkus.test.utils.Command;
 
 @OpenShiftScenario(deployment = OpenShiftDeploymentStrategy.UsingOpenShiftExtensionAndDockerBuildStrategy)
@@ -50,9 +51,6 @@ public class OpenShiftPostgreSqlSbIT {
     private static boolean areRequiredOperatorsInstalled() {
         List<String> output = new ArrayList<>();
         try {
-            // TODO: figure out a better way to wait for this - this wait is necessary as it takes some time for API to
-            //       populate new namespace with objects
-            Thread.sleep(2000);
             new Command("oc", "get", "csv").outputToLines(output).runAndWait();
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage(), e);
@@ -61,13 +59,30 @@ public class OpenShiftPostgreSqlSbIT {
         return (outputString.contains("postgresoperator") && outputString.contains("service-binding-operator"));
     }
 
-    private static void createPostgresCluster() {
-        Assumptions.assumeTrue(areRequiredOperatorsInstalled());
-        applyCustomResourceDefinition("pg-cluster.yml");
+    private static boolean arePodsInstalled() {
         try {
-            // TODO: figure out a better way to wait for this - sometimes operator takes a while to create object
-            //       maybe condition of the generated postgres object would help, but this is not a documented API
-            Thread.sleep(2000);
+            List<String> output = new ArrayList<>();
+            new Command("oc", "get", "pods").outputToLines(output).runAndWait();
+            return output.stream()
+                    .anyMatch(line -> line.contains("hippo"));
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    private static void createPostgresCluster() {
+        // this wait is necessary as it takes some time for API to populate new namespace with objects
+        AwaitilityUtils
+                .untilIsTrue(OpenShiftPostgreSqlSbIT::areRequiredOperatorsInstalled,
+                        AwaitilityUtils.AwaitilitySettings.using(Duration.ofSeconds(5),
+                                Duration.ofSeconds(60)));
+        applyCustomResourceDefinition("pg-cluster.yml");
+        // sometimes operator takes a while to create an object
+        AwaitilityUtils
+                .untilIsTrue(OpenShiftPostgreSqlSbIT::arePodsInstalled,
+                        AwaitilityUtils.AwaitilitySettings.using(Duration.ofSeconds(2),
+                                Duration.ofSeconds(30)));
+        try {
             new Command("oc", "-n", ocClient.project(), "wait", "--for", "condition=Ready", "--timeout=300s",
                     "pods", "--all").runAndWait();
         } catch (Exception e) {
