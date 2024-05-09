112dd429b71aa09e0dcce778ebc8f21e92f27e16
Jose CN <75727563+jcarranzan@users.noreply.github.com>
2024-05-09 16:50:07 +0200

    Add Brotli4J compression tests (#1717)
    add @DisabledOnNative also on baremetal

---
+++ http/http-advanced-reactive/src/main/java/io/quarkus/ts/http/advanced/reactive/Brotli4JHttpServerConfig.java
@@ -0,0 +1,28 @@
+package io.quarkus.ts.http.advanced.reactive;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+import io.netty.handler.codec.compression.BrotliOptions;
+import io.netty.handler.codec.compression.StandardCompressionOptions;
+import io.quarkus.vertx.http.HttpServerOptionsCustomizer;
+import io.vertx.core.http.HttpServerOptions;
+
+@ApplicationScoped
+public class Brotli4JHttpServerConfig implements HttpServerOptionsCustomizer {
+    // It depends on compression level that we want to apply, in this case we use level 4, but we won't test the level compression features.
+    private static final int compressionLevel = 4;
+
+    @Override
+    public void customizeHttpServer(HttpServerOptions options) {
+        options.addCompressor(getBrotliOptions(compressionLevel));
+    }
+
+    @Override
+    public void customizeHttpsServer(HttpServerOptions options) {
+        options.addCompressor(getBrotliOptions(compressionLevel));
+    }
+
+    private static BrotliOptions getBrotliOptions(int compressionLevel) {
+        return StandardCompressionOptions.brotli();
+    }
+}
---
+++ http/http-advanced-reactive/src/main/java/io/quarkus/ts/http/advanced/reactive/Brotli4JResource.java
@@ -0,0 +1,66 @@
+package io.quarkus.ts.http.advanced.reactive;
+
+import java.util.HashMap;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/compression")
+public class Brotli4JResource {
+
+    public final static String DEFAULT_TEXT_PLAIN = "In life, you have to trust that every little bit helps. As you know," +
+            " every small step forward counts." +
+            " It's the accumulation of these efforts that ultimately leads to success." +
+            " So, don't underestimate the power of persistence and determination in achieving your dreams";
+
+    @Inject
+    Brotli4JRestMock brotli4JRestMock;
+
+    @GET
+    @Path("/brotli/json")
+    @Produces(MediaType.APPLICATION_JSON)
+    public HashMap<String, Object> jsonHttpCompressionResponse() {
+        return brotli4JRestMock.returnResponse(Brotli4JRestMock.ResponseType.JSON);
+    }
+
+    @GET
+    @Path("/brotli/xml")
+    @Produces(MediaType.APPLICATION_XML)
+    public String xmlHttpCompressionResponse() {
+        return brotli4JRestMock.returnResponse(Brotli4JRestMock.ResponseType.XML).get("xml").toString();
+    }
+
+    @POST
+    @Path("/decompression")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String decompressionHttpResponse(byte[] compressedData) {
+        String decompressedData = new String(compressedData);
+        return decompressedData;
+    }
+
+    @GET
+    @Path("/default/text")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String textPlainDefaultHttpCompressionResponse() {
+        return DEFAULT_TEXT_PLAIN;
+    }
+
+    @GET
+    @Path("/text")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String textPlainHttpCompressionResponse() {
+        return brotli4JRestMock.returnTextPlainResponse(Brotli4JRestMock.ResponseType.TEXT);
+    }
+
+    @GET
+    @Path("/text/big")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String textPlainBigHttpCompressionResponse() {
+        return brotli4JRestMock.returnTextPlainResponse(Brotli4JRestMock.ResponseType.BIG_TEXT);
+    }
+
+}
