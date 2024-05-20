8463bc552fccc058c00c440d1733111720283371
Michal Vavřík <mvavrik@redhat.com>
2024-05-20 19:44:41 +0200

    Fix and enable HTTP module tests in FIPS-enabled environment

--- http/http-advanced-reactive/src/test/java/io/quarkus/ts/http/advanced/reactive/DevModeHttpsIT.java
+++ http/http-advanced-reactive/src/test/java/io/quarkus/ts/http/advanced/reactive/DevModeHttpsIT.java
@@ -6,13 +6,14 @@ import io.quarkus.test.bootstrap.DevModeQuarkusService;
 import io.quarkus.test.bootstrap.Protocol;
 import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.services.Certificate;
 import io.quarkus.test.services.DevModeQuarkusApplication;
 import io.quarkus.test.services.URILike;
 
 @QuarkusScenario
 public class DevModeHttpsIT extends AbstractDevModeIT {
 
-    @DevModeQuarkusApplication(ssl = true)
+    @DevModeQuarkusApplication(ssl = true, certificates = @Certificate(configureKeystore = true))
     static RestService app = new DevModeQuarkusService()
             .withProperty("quarkus.oidc.enabled", "false")
             .withProperty("quarkus.keycloak.policy-enforcer.enable", "false")
