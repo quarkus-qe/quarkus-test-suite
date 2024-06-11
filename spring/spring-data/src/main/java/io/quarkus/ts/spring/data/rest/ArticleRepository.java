0de4506ca2524546af10efb613905cb5e272c69c
Michal Vavřík <mvavrik@redhat.com>
2024-06-11 23:04:25 +0200

    Fix Spring Data compilation and SpringDataCompositeEntityIT

--- spring/spring-data/src/main/java/io/quarkus/ts/spring/data/rest/ArticleRepository.java
+++ spring/spring-data/src/main/java/io/quarkus/ts/spring/data/rest/ArticleRepository.java
@@ -4,12 +4,13 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(exported = false, path = "articles", collectionResourceRel = "articles")
-public interface ArticleRepository extends PagingAndSortingRepository<Article, Long> {
+public interface ArticleRepository extends PagingAndSortingRepository<Article, Long>, CrudRepository<Article, Long> {
 
     @Override
     @RestResource(exported = true)
