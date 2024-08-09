45b777268c6db84facd8890db8afba7f0abff2bc
Fedor Dudinsky <fdudinsk@redhat.com>
2024-08-09 10:08:48 +0200

    Reproducer for leaking transactions in XA mode
    https://issues.redhat.com/browse/QUARKUS-4185

--- sql-db/narayana-transactions/src/main/java/io/quarkus/ts/service/ServiceResource.java
+++ sql-db/narayana-transactions/src/main/java/io/quarkus/ts/service/ServiceResource.java
@@ -1,17 +1,23 @@
 package io.quarkus.ts.service;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
+
+import org.jboss.logging.Logger;
 
 import io.agroal.api.AgroalDataSource;
 
 @Path("/service")
 public class ServiceResource {
+    private static final Logger LOG = Logger.getLogger(ServiceResource.class);
+
     @Inject
     AgroalDataSource defaultDataSource;
 
@@ -23,6 +29,22 @@ public class ServiceResource {
                     ? Response.Status.CREATED
                     : Response.Status.NO_CONTENT;
             return Response.status(status).build();
+        }
+    }
+
+    @Path("/connections")
+    @GET
+    public Response connections() {
+        try (Statement statement = defaultDataSource.getConnection().createStatement()) {
+            ResultSet set = statement.executeQuery("SELECT COUNT(*) from sys.dm_exec_connections;");
+            if (set.next()) {
+                int count = set.getInt(1);
+                return Response.ok(count).build();
+            }
+            return Response.serverError().build();
+        } catch (Exception e) {
+            LOG.error("Failed to retrieve number of connections", e);
+            return Response.serverError().build();
         }
     }
 }
