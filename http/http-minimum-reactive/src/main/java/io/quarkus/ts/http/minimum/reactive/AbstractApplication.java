ba74843173777294efb9a9ccd58a1cfd7a5cfc40
Rostislav Svoboda <rsvoboda@redhat.com>
2024-09-02 20:14:59 +0200

    Ensure abstract jakarta.ws.rs.core.Application is ignored
    (cherry picked from commit 37be1a9b5b892bccd4dadc6c6a9232f341f3f681)

---
+++ http/http-minimum-reactive/src/main/java/io/quarkus/ts/http/minimum/reactive/AbstractApplication.java
@@ -0,0 +1,9 @@
+package io.quarkus.ts.http.minimum.reactive;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public abstract class AbstractApplication extends Application {
+    // Abstract jakarta.ws.rs.core.Application classes should be ignored
+}
