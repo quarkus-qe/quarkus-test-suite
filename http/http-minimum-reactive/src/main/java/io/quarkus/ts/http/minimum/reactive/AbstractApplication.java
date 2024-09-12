84489e0143f056ab2fcd20a7e6a0a72813ad2c80
Fedor Dudinsky <fdudinsk@redhat.com>
2024-09-12 09:57:21 +0200

    Revert "Ensure abstract jakarta.ws.rs.core.Application is ignored"
    This reverts commit 37be1a9b5b892bccd4dadc6c6a9232f341f3f681.

--- http/http-minimum-reactive/src/main/java/io/quarkus/ts/http/minimum/reactive/AbstractApplication.java
+++
@@ -1,9 +0,0 @@
-package io.quarkus.ts.http.minimum.reactive;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public abstract class AbstractApplication extends Application {
-    // Abstract jakarta.ws.rs.core.Application classes should be ignored
-}
