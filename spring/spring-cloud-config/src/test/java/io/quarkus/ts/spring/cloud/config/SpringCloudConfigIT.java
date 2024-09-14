422bf2e9c659336f0b3d630eae328f73a3ca9b7d
Michal Jurc <mjurc@redhat.com>
2024-09-14 14:46:55 +0200

    Disable Spring Cloud Server test on aarch64
    * Disabling Spring Cloud Server tests on aarch64 as there's no aarch64
      based container we can use as test service.
    
    (cherry picked from commit 4bd94a02ffbb0bb8a12ce0a76a69a3bc87709869)

--- spring/spring-cloud-config/src/test/java/io/quarkus/ts/spring/cloud/config/SpringCloudConfigIT.java
+++ spring/spring-cloud-config/src/test/java/io/quarkus/ts/spring/cloud/config/SpringCloudConfigIT.java
@@ -4,6 +4,7 @@ import static org.hamcrest.Matchers.is;
 
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -14,6 +15,7 @@ import io.quarkus.test.services.Container;
 import io.quarkus.test.services.QuarkusApplication;
 
 @QuarkusScenario
+@DisabledIfSystemProperty(named = "ts.arm.missing.services.excludes", matches = "true", disabledReason = "https://github.com/quarkus-qe/quarkus-test-suite/issues/2025")
 public class SpringCloudConfigIT {
 
     @Container(image = "${spring.cloud.server.image}", port = 8888, expectedLog = "Started ConfigServer")
