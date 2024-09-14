7526957a7039755f5928505e111373669771c1fd
Michal Jurc <mjurc@redhat.com>
2024-09-14 14:28:47 +0200

    Excluding MS SQL tests from aarch64 profile
    * Disabling MS SQL tests on aarch64 profile as MS SQL is not supported
      on aarch64 architecture, so our test service will not work.
    
    (cherry picked from commit fcb947e865810bf7f0736cf03d6faee89a584eb8)

--- sql-db/hibernate-reactive/src/test/java/io/quarkus/ts/hibernate/reactive/openshift/OpenShiftHibernateReactiveMsSQLIT.java
+++ sql-db/hibernate-reactive/src/test/java/io/quarkus/ts/hibernate/reactive/openshift/OpenShiftHibernateReactiveMsSQLIT.java
@@ -1,11 +1,13 @@
 package io.quarkus.ts.hibernate.reactive.openshift;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 
 import io.quarkus.test.scenarios.OpenShiftScenario;
 import io.quarkus.ts.hibernate.reactive.MsSQLDatabaseHibernateReactiveIT;
 
 @OpenShiftScenario
+@DisabledIfSystemProperty(named = "ts.arm.missing.services.excludes", matches = "true", disabledReason = "https://github.com/quarkus-qe/quarkus-test-suite/issues/2017")
 @Disabled("https://github.com/microsoft/mssql-docker/issues/769")
 public class OpenShiftHibernateReactiveMsSQLIT extends MsSQLDatabaseHibernateReactiveIT {
 }
