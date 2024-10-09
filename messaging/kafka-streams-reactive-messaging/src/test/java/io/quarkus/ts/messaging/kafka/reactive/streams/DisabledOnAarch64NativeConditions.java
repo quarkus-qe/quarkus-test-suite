fc22708320d3fa44125481c6b64c47c4d1575006
Martin Ocenas <mocenas@redhat.com>
2024-10-09 16:09:42 +0200

    Disable kafka streams test also on quarkus main aarch native
    (cherry picked from commit 47c9d88f6230ca6b1ffd9bd890641d9a14ff863b)

--- messaging/kafka-streams-reactive-messaging/src/test/java/io/quarkus/ts/messaging/kafka/reactive/streams/DisabledOnAarch64NativeConditions.java
+++ messaging/kafka-streams-reactive-messaging/src/test/java/io/quarkus/ts/messaging/kafka/reactive/streams/DisabledOnAarch64NativeConditions.java
@@ -6,17 +6,16 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 
 import io.quarkus.test.services.quarkus.model.QuarkusProperties;
 
-public class DisabledOnRHBQAarch64NativeConditions implements ExecutionCondition {
+public class DisabledOnAarch64NativeConditions implements ExecutionCondition {
 
     @Override
     public ConditionEvaluationResult evaluateExecutionCondition(ExtensionContext extensionContext) {
-        boolean isRHBQ = QuarkusProperties.getVersion().contains("redhat");
         boolean isNative = QuarkusProperties.isNativeEnabled();
         boolean isAarch64 = "true".equals(System.getProperty("ts.arm.missing.services.excludes"));
-        if (isRHBQ && isAarch64 && isNative) {
-            return ConditionEvaluationResult.disabled("It is RHBQ running on aarch64 in native mode.");
+        if (isAarch64 && isNative) {
+            return ConditionEvaluationResult.disabled("It is running on aarch64 in native mode.");
         } else {
-            return ConditionEvaluationResult.enabled("It is not RHBQ running on aarch64 in native mode.");
+            return ConditionEvaluationResult.enabled("It is not running on aarch64 in native mode.");
         }
     }
 }
--- messaging/kafka-streams-reactive-messaging/src/test/java/io/quarkus/ts/messaging/kafka/reactive/streams/OpenShiftStrimziKafkaStreamIT.java
+++ messaging/kafka-streams-reactive-messaging/src/test/java/io/quarkus/ts/messaging/kafka/reactive/streams/OpenShiftStrimziKafkaStreamIT.java
@@ -3,6 +3,6 @@ package io.quarkus.ts.messaging.kafka.reactive.streams;
 import io.quarkus.test.scenarios.OpenShiftScenario;
 
 @OpenShiftScenario
-@DisabledOnRHBQAarch64Native(reason = "https://issues.redhat.com/browse/QUARKUS-4321")
+@DisabledOnAarch64Native(reason = "https://issues.redhat.com/browse/QUARKUS-4321")
 public class OpenShiftStrimziKafkaStreamIT extends StrimziKafkaStreamIT {
 }
