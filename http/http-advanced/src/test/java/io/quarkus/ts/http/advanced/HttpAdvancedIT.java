ff0b1bd079f93abe535bd5277f3dcb2c9450df8d
jcarranzan <jcarranz@redhat.com>
2024-10-24 09:50:54 +0200

    set JAVA_OPTS_KC_HEAP in order to not override all JVM opts

--- http/http-advanced/src/test/java/io/quarkus/ts/http/advanced/HttpAdvancedIT.java
+++ http/http-advanced/src/test/java/io/quarkus/ts/http/advanced/HttpAdvancedIT.java
@@ -17,7 +17,7 @@ public class HttpAdvancedIT extends BaseHttpAdvancedIT {
     @KeycloakContainer(command = { "start-dev", "--import-realm", "--hostname-strict=false" })
     static KeycloakService keycloak = new KeycloakService(DEFAULT_REALM_FILE, DEFAULT_REALM, DEFAULT_REALM_BASE_PATH)
             // TODO remove this propery when this issue will be resolved: https://github.com/quarkus-qe/quarkus-test-suite/issues/2106
-            .withProperty("JAVA_OPTS", "-Xms512m -Xmx1g");
+            .withProperty("JAVA_OPTS_KC_HEAP", "-Xms512m -Xmx1g");
 
     @QuarkusApplication(ssl = true, certificates = @Certificate(configureKeystore = true, configureHttpServer = true, useTlsRegistry = false))
     static RestService app = new RestService().withProperty("quarkus.oidc.auth-server-url", keycloak::getRealmUrl);
