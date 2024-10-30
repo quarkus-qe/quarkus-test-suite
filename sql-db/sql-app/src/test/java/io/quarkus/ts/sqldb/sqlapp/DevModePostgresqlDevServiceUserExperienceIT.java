f601dcad4ff73cd0134097d6f541c3b946553cf8
Michal Vavřík <mvavrik@redhat.com>
2024-10-30 18:31:56 +0100

    Use Docker PostgreSQL image for Dev Svc on Aarch64
    (cherry picked from commit b7207281d1f888dab0db1a45d67e5026df983ee2)

--- sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/DevModePostgresqlDevServiceUserExperienceIT.java
+++ sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/DevModePostgresqlDevServiceUserExperienceIT.java
@@ -23,14 +23,27 @@ public class DevModePostgresqlDevServiceUserExperienceIT {
     // we use '-alpine' version as no other test is using it, which mitigates the fact that sometimes
     // io.quarkus.test.utils.DockerUtils.removeImage doesn't work as expected
     // TODO: drop suffix when https://github.com/quarkus-qe/quarkus-test-suite/issues/1227 is fixed
-    private static final String POSTGRESQL_VERSION = getImageVersion("postgresql.latest.image") + "-alpine";
-    private static final String POSTGRES_NAME = getImageName("postgresql.latest.image");
+    private static final String POSTGRESQL_VERSION;
+    private static final String POSTGRES_NAME;
+    private static final String POSTGRESQL_IMAGE_NAME;
+
+    static {
+        // TODO: simplify when https://github.com/quarkus-qe/quarkus-test-suite/pull/2147 is fixed
+        boolean useDevSvcSpecificImg = System.getProperty("postgresql.dev.svc.latest.image") != null;
+        if (useDevSvcSpecificImg) {
+            POSTGRESQL_IMAGE_NAME = "postgresql.dev.svc.latest.image";
+        } else {
+            POSTGRESQL_IMAGE_NAME = "postgresql.latest.image";
+        }
+        POSTGRESQL_VERSION = getImageVersion(POSTGRESQL_IMAGE_NAME) + "-alpine";
+        POSTGRES_NAME = getImageName(POSTGRESQL_IMAGE_NAME);
+    }
 
     @DevModeQuarkusApplication
     static RestService app = new RestService()
             .withProperty("quarkus.datasource.db-kind", "postgresql")
             .withProperty("quarkus.datasource.devservices.port", Integer.toString(SocketUtils.findAvailablePort()))
-            .withProperty("quarkus.datasource.devservices.image-name", "${postgresql.latest.image}-alpine")
+            .withProperty("quarkus.datasource.devservices.image-name", "${" + POSTGRESQL_IMAGE_NAME + "}-alpine")
             .withProperty("quarkus.hibernate-orm.database.generation", "none")
             .onPreStart(s -> DockerUtils.removeImage(POSTGRES_NAME, POSTGRESQL_VERSION));
 
