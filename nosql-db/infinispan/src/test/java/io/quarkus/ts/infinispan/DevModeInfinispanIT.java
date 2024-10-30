0b418fb0f82f430323e62bfac42a88ea51354d4d
Michal Vavřík <mvavrik@redhat.com>
2024-10-30 20:06:02 +0100

    Fix DevModeInfinispanIT on Aarch64
    (cherry picked from commit 8b19bdb6307edfb928389a1a4917ed6a69d47b5a)

--- nosql-db/infinispan/src/test/java/io/quarkus/ts/infinispan/DevModeInfinispanIT.java
+++ nosql-db/infinispan/src/test/java/io/quarkus/ts/infinispan/DevModeInfinispanIT.java
@@ -2,18 +2,44 @@ package io.quarkus.ts.infinispan;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Map;
+
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
 import io.quarkus.test.services.DevModeQuarkusApplication;
+import io.quarkus.test.utils.FileUtils;
 
 @QuarkusScenario
 public class DevModeInfinispanIT {
 
+    // TODO: drop this Aarch64 workaround when https://issues.redhat.com/browse/QUARKUS-5242 is fixed
+    private static final boolean IS_AARCH64_PLATFORM = Boolean.getBoolean("ts.arm.missing.services.excludes");
+
     @DevModeQuarkusApplication()
-    static RestService service = new RestService();
+    static RestService service = new RestService()
+            .withProperties(() -> {
+                if (IS_AARCH64_PLATFORM) {
+                    return Map.of("quarkus.infinispan-client.devservices.image-name", "${datagrid.image}");
+                }
+                return Map.of();
+            })
+            .onPreStart(service -> {
+                if (IS_AARCH64_PLATFORM) {
+                    // override default Ryuk used by Infinispan as it doesn't work on Aarch64
+                    RestService restService = (RestService) service;
+                    var testContainersPropsPath = restService
+                            .getServiceFolder()
+                            .resolve("src")
+                            .resolve("main")
+                            .resolve("resources")
+                            .resolve("testcontainers.properties");
+                    FileUtils.copyContentTo("ryuk.container.image=testcontainers/ryuk:0.3.3" + System.lineSeparator(),
+                            testContainersPropsPath);
+                }
+            });
 
     @Test
     void smoke() {
