4b746162b4610ccce4ffc2b481faf6c51a65b29e
Michal Jurc <mjurc@redhat.com>
2024-10-30 20:31:02 +0100

    Disabling Kafka Streams tests due to QUARKUS-4321
    * See https://issues.redhat.com/browse/QUARKUS-4321.

--- messaging/kafka-streams-reactive-messaging/src/test/java/io/quarkus/ts/messaging/kafka/reactive/streams/SslAlertMonitorIT.java
+++ messaging/kafka-streams-reactive-messaging/src/test/java/io/quarkus/ts/messaging/kafka/reactive/streams/SslAlertMonitorIT.java
@@ -4,6 +4,7 @@ import io.quarkus.test.bootstrap.KafkaService;
 import io.quarkus.test.bootstrap.Protocol;
 import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.scenarios.annotations.DisabledOnAarch64Native;
 import io.quarkus.test.scenarios.annotations.DisabledOnRHBQandWindows;
 import io.quarkus.test.services.KafkaContainer;
 import io.quarkus.test.services.QuarkusApplication;
@@ -12,6 +13,7 @@ import io.quarkus.test.services.containers.model.KafkaVendor;
 
 @QuarkusScenario
 @DisabledOnRHBQandWindows(reason = "QUARKUS-3434")
+@DisabledOnAarch64Native(reason = "QUARKUS-4321")
 public class SslAlertMonitorIT extends BaseKafkaStreamTest {
 
     @KafkaContainer(vendor = KafkaVendor.STRIMZI, protocol = KafkaProtocol.SSL)
