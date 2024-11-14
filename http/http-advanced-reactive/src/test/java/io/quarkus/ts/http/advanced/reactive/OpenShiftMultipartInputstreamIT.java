96331e76821708b6abefc67a368a19d700d0581c
jcarranzan <jcarranz@redhat.com>
2024-11-14 11:35:30 +0100

    Add test coverage for #43364 - InputStream when using multipart

---
+++ http/http-advanced-reactive/src/test/java/io/quarkus/ts/http/advanced/reactive/OpenShiftMultipartInputstreamIT.java
@@ -0,0 +1,8 @@
+package io.quarkus.ts.http.advanced.reactive;
+
+import io.quarkus.test.scenarios.OpenShiftScenario;
+
+@OpenShiftScenario
+public class OpenShiftMultipartInputstreamIT extends MultipartInputstreamIT {
+
+}
