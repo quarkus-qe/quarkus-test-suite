cb4cf0c8c9e2b81414d4421c6501c51f094d3a21
Jakub Jedlicka <jjedlick@redhat.com>
2024-12-03 14:28:52 +0100

    Dissable the testHttp10WithoutHostHeader on openshift
    The test needs to be disabled on openshift as for comunication outside the valid
    'Host' header needs to be set

--- http/http-advanced-reactive/src/test/java/io/quarkus/ts/http/advanced/reactive/OpenShiftHeadersIT.java
+++ http/http-advanced-reactive/src/test/java/io/quarkus/ts/http/advanced/reactive/OpenShiftHeadersIT.java
@@ -1,7 +1,16 @@
 package io.quarkus.ts.http.advanced.reactive;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
 import io.quarkus.test.scenarios.OpenShiftScenario;
 
 @OpenShiftScenario
 public class OpenShiftHeadersIT extends HeadersIT {
+
+    @Test
+    @Override
+    @Disabled("This test won't work on Openshift as the request expect valid `Host` header and this test check the http/1.0 without `Host` header")
+    public void testHttp10WithoutHostHeader() {
+    }
 }
