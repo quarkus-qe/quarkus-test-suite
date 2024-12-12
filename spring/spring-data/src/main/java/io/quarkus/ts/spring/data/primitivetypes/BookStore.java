111e98b38ba3a1925cc4420686c2a996c0efd9fa
Jakub Jedlicka <jjedlick@redhat.com>
2024-12-12 15:34:17 +0100

    Refactor the test disabled by https://github.com/quarkusio/quarkus/issues/41292
    The original test was based on serialization, which is no longer possible out of box.
    In quarkusio/quarkus#41292 was suggested use this modification to make the original code work.
    Now we checking if returned json, pageable item contains specific values.
    
    For more info see https://github.com/quarkusio/quarkus/issues/41292#issuecomment-2371862795

--- spring/spring-data/src/main/java/io/quarkus/ts/spring/data/primitivetypes/BookStore.java
+++ spring/spring-data/src/main/java/io/quarkus/ts/spring/data/primitivetypes/BookStore.java
@@ -1,5 +1,7 @@
 package io.quarkus.ts.spring.data.primitivetypes;
 
+import java.util.List;
+
 import jakarta.enterprise.context.ApplicationScoped;
 
 import org.springframework.data.domain.Page;
@@ -12,6 +14,7 @@ import io.quarkus.ts.spring.data.primitivetypes.model.Book;
 @ApplicationScoped
 public class BookStore implements PanacheRepository<Book> {
     public Page<Book> findPaged(Pageable pageable) {
-        return new PageImpl<>(this.findAll().page(pageable.getPageNumber(), pageable.getPageSize()).list());
+        List<Book> list = this.findAll().list();
+        return new PageImpl<>(list, pageable, list.size());
     }
 }
