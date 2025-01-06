bb3ebe3bceacf7ef3a2e7b93a5e1c535b561b31c
Jakub Jedlicka <jjedlick@redhat.com>
2025-01-06 12:05:00 +0100

    Refactoring the infinispan-client to use DataGridInfinispanInstance
    This is due to reason when the rest service fail the datagrid resources were not cleaned up.

--- infinispan-client/src/test/java/io/quarkus/ts/infinispan/client/BaseOpenShiftInfinispanIT.java
+++ infinispan-client/src/test/java/io/quarkus/ts/infinispan/client/BaseOpenShiftInfinispanIT.java
@@ -5,95 +5,39 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import jakarta.inject.Inject;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 
+import io.quarkus.test.bootstrap.DefaultService;
 import io.quarkus.test.bootstrap.inject.OpenShiftClient;
-import io.quarkus.test.utils.Command;
+import io.quarkus.test.bootstrap.service.OperatorOpenShiftInfinispan;
 
 public abstract class BaseOpenShiftInfinispanIT {
     protected static final String TARGET_RESOURCES = "target/test-classes/";
-    protected static final String CLUSTER_SECRET = "clientcert_secret.yaml";
-    protected static final String CLUSTER_CONFIG = "infinispan_cluster_config.yaml";
-    protected static final String CLUSTER_CONFIGMAP = "infinispan_cluster_configmap.yaml";
-    protected static final String CONNECT_SECRET = "connect_secret.yaml";
-    protected static final String TLS_SECRET = "tls_secret.yaml";
+    protected static final String CLIENT_CERT_SECRET = TARGET_RESOURCES + "clientcert_secret.yaml";
+    protected static final String CLUSTER_CONFIG = TARGET_RESOURCES + "infinispan_cluster_config.yaml";
+    protected static final String CLUSTER_CONFIGMAP = TARGET_RESOURCES + "infinispan_cluster_configmap.yaml";
+    protected static final String CONNECT_SECRET = TARGET_RESOURCES + "connect_secret.yaml";
+    protected static final String TLS_SECRET = TARGET_RESOURCES + "tls_secret.yaml";
     protected static final String CLUSTER_NAMESPACE_NAME = "datagrid-cluster";
-    protected static String NEW_CLUSTER_NAME = null;
-
-    private static final String ORIGIN_CLUSTER_NAME = "totally-random-infinispan-cluster-name";
 
     @Inject
     static OpenShiftClient ocClient;
 
-    @AfterAll
-    public static void deleteInfinispanCluster() {
-        deleteYaml(CLUSTER_CONFIGMAP);
-        deleteYaml(CLUSTER_CONFIG);
-        adjustYml(CLUSTER_CONFIG, NEW_CLUSTER_NAME, ORIGIN_CLUSTER_NAME);
-        adjustYml(CLUSTER_CONFIGMAP, NEW_CLUSTER_NAME, ORIGIN_CLUSTER_NAME);
-    }
+    @OperatorOpenShiftInfinispan(clientCertSecret = CLIENT_CERT_SECRET, clusterConfig = CLUSTER_CONFIG, clusterConfigMap = CLUSTER_CONFIGMAP, connectSecret = CONNECT_SECRET, tlsSecret = TLS_SECRET)
+    static DefaultService dataGridInfinispan = new DefaultService();
 
-    protected static void adjustYml(String yamlFile, String originString, String newString) {
+    protected static void adjustYml(Path yamlFile, String originString, String newString) {
         try {
-            Path yamlPath = Paths.get(TARGET_RESOURCES + yamlFile);
             Charset charset = StandardCharsets.UTF_8;
 
-            String yamlContent = Files.readString(yamlPath, charset);
+            String yamlContent = Files.readString(yamlFile, charset);
             yamlContent = yamlContent.replace(originString, newString);
-            Files.writeString(yamlPath, yamlContent, charset);
+            Files.writeString(yamlFile, yamlContent, charset);
         } catch (IOException ex) {
             Assertions.fail("Fail to adjust YAML file. Caused by: " + ex.getMessage());
-        }
-    }
-
-    /**
-     * Apply the YAML file.
-     */
-    protected static void applyYaml(String yamlFile) {
-        try {
-            new Command("oc", "apply", "-f", Paths.get(TARGET_RESOURCES + yamlFile).toString()).runAndWait();
-        } catch (Exception e) {
-            Assertions.fail("Failed to apply YAML file. Caused by: " + e.getMessage());
-        }
-    }
-
-    protected static void createInfinispanCluster() {
-        applyYaml(CLUSTER_SECRET);
-        applyYaml(CONNECT_SECRET);
-        applyYaml(TLS_SECRET);
-
-        // there should be unique name for every created infinispan cluster to be able parallel runs
-        NEW_CLUSTER_NAME = ocClient.project() + "-infinispan-cluster";
-
-        // rename infinispan cluster and configmap
-        adjustYml(CLUSTER_CONFIG, ORIGIN_CLUSTER_NAME, NEW_CLUSTER_NAME);
-        applyYaml(CLUSTER_CONFIG);
-        adjustYml(CLUSTER_CONFIGMAP, ORIGIN_CLUSTER_NAME, NEW_CLUSTER_NAME);
-        applyYaml(CLUSTER_CONFIGMAP);
-
-        try {
-            new Command("oc", "-n", CLUSTER_NAMESPACE_NAME, "wait", "--for", "condition=wellFormed", "--timeout=300s",
-                    "infinispan/" + NEW_CLUSTER_NAME).runAndWait();
-        } catch (Exception e) {
-            deleteInfinispanCluster();
-            Assertions.fail("Fail to wait Infinispan resources to start. Caused by: " + e.getMessage());
-        }
-    }
-
-    /**
-     *
-     * Delete the YAML file.
-     */
-    private static void deleteYaml(String yamlFile) {
-        try {
-            new Command("oc", "delete", "-f", Paths.get(TARGET_RESOURCES + yamlFile).toString()).runAndWait();
-        } catch (Exception e) {
-            Assertions.fail("Failed to delete YAML file. Caused by: " + e.getMessage());
         }
     }
 }
