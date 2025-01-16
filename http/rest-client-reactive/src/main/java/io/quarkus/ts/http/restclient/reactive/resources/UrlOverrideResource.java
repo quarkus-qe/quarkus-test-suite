299fead5f86bc93eb8a8832171b43f464e622214
Martin Ocenas <mocenas@redhat.com>
2025-01-16 13:57:47 +0100

    Tests for rest-client-reactive URL override feature

---
+++ http/rest-client-reactive/src/main/java/io/quarkus/ts/http/restclient/reactive/resources/UrlOverrideResource.java
@@ -0,0 +1,21 @@
+package io.quarkus.ts.http.restclient.reactive.resources;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@ApplicationScoped
+@Path("urlOverride")
+public class UrlOverrideResource {
+
+    @ConfigProperty(name = "ts.quarkus.urlOverride.response", defaultValue = "empty")
+    String response;
+
+    @GET
+    @Path("basic")
+    public String basic() {
+        return response;
+    }
+}
