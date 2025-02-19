0d4cbfd433d3642382b50d94021ca08f420f155e
Rostislav Svoboda <rsvoboda@redhat.com>
2025-02-19 10:22:57 +0100

    Enable MariaDB transaction tests after bump to mariadb-java-client to 3.5.2
    https://github.com/quarkusio/quarkus/pull/46221

--- sql-db/narayana-transactions/src/test/java/io/quarkus/ts/transactions/MariaDbTransactionGeneralUsageIT.java
+++ sql-db/narayana-transactions/src/test/java/io/quarkus/ts/transactions/MariaDbTransactionGeneralUsageIT.java
@@ -1,8 +1,5 @@
 package io.quarkus.ts.transactions;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-
 import io.quarkus.test.bootstrap.MariaDbService;
 import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
@@ -36,10 +33,4 @@ public class MariaDbTransactionGeneralUsageIT extends TransactionCommons {
         return TransactionExecutor.QUARKUS_TRANSACTION;
     }
 
-    @Test
-    @Disabled("https://github.com/quarkusio/quarkus/issues/44160")
-    @Override
-    public void testTransactionRecovery() {
-        // TODO: drop this method when https://github.com/quarkusio/quarkus/issues/44160 is fixed
-    }
 }
