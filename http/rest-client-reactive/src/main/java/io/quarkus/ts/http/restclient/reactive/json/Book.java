b45bce16edeb7df786ba2665ac5662b72962ddfe
Rostislav Svoboda <rosta.svo@gmail.com>
2025-03-28 15:07:09 +0100

    Convert Book class to record
    (cherry picked from commit aec241ac0db466ca1c1188e2a918d4d8115d8408)

--- http/rest-client-reactive/src/main/java/io/quarkus/ts/http/restclient/reactive/json/Book.java
+++ http/rest-client-reactive/src/main/java/io/quarkus/ts/http/restclient/reactive/json/Book.java
@@ -1,36 +1,4 @@
 package io.quarkus.ts.http.restclient.reactive.json;
 
-public class Book {
-
-    private String title;
-    private String author;
-
-    public Book() {
-    }
-
-    public Book(String title, String author) {
-        this.title = title;
-        this.author = author;
-    }
-
-    public Book(String title) {
-        this(title, null);
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    // TODO: required due to https://github.com/quarkusio/quarkus/issues/46458
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+public record Book(String title, String author) {
 }
