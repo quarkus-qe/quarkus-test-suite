4027738eb5a09138b84dfa2f9ded940c77da21d8
Fedor Dudinsky <fdudinsk@redhat.com>
2025-04-09 09:49:27 +0200

    Verify, that updates to 3.20 can be done automatically
    Some code is taken from this patch to 3.15:
    https://github.com/quarkus-qe/quarkus-test-suite/pull/2013/commits/b49034a70c5909296f4925ef217e386811fd81e7

---
+++ quarkus-cli/src/test/java/io/quarkus/ts/quarkus/cli/update/QuarkusCli320UpdatesIT.java
@@ -0,0 +1,59 @@
+package io.quarkus.ts.quarkus.cli.update;
+
+import static io.quarkus.test.util.QuarkusCLIUtils.checkDependenciesUpdate;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.surefire.shared.lang3.tuple.Pair;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.util.IQuarkusCLIAppManager;
+import io.quarkus.test.util.QuarkusCLIUtils;
+
+@Tag("quarkus-cli")
+public class QuarkusCli320UpdatesIT extends AbstractQuarkusCliUpdateIT {
+    private static final DefaultArtifactVersion oldVersion = new DefaultArtifactVersion("3.15");
+    private static final DefaultArtifactVersion newVersion = new DefaultArtifactVersion("3.20");
+
+    public QuarkusCli320UpdatesIT() {
+        super(oldVersion, newVersion);
+    }
+
+    @Test
+    /**
+     * Compatibility layer, that existed between 3.9 and 3.15 was removed. Let's check, that the upgrade can still be done
+     */
+    public void dependencyUpdate() throws IOException, XmlPullParserException {
+        IQuarkusCLIAppManager longUpdate = QuarkusCLIUtils.createAppManager(cliClient,
+                new DefaultArtifactVersion("3.8"),
+                newVersionStream);
+        Path greatRenameCSV = Paths.get("src/test/resources/quarkus315dependencyUpdates.csv");
+        List<Pair<Dependency, Dependency>> dependenciesToUpdate = loadDependencyPairsFromCSV(greatRenameCSV);
+        checkDependenciesUpdate(longUpdate, dependenciesToUpdate);
+    }
+
+    @Test
+    public void jdbcTracingProperty() throws IOException {
+        Properties oldProperties = new Properties();
+        Properties newProperties = new Properties();
+
+        oldProperties.put("quarkus.hibernate-orm.validation.enabled", "true");
+        newProperties.put("quarkus.hibernate-orm.validation.mode", "auto");
+
+        oldProperties.put("quarkus.http.cors", "true");
+        newProperties.put("quarkus.http.cors.enabled", "true");
+
+        oldProperties.put("quarkus.log.console.json", "true");
+        newProperties.put("quarkus.log.console.json.enabled", "true");
+
+        QuarkusCLIUtils.checkPropertiesUpdate(quarkusCLIAppManager, oldProperties, newProperties);
+    }
+}
