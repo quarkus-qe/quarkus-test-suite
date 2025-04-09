920770b08494bd98282d8c2b7b67a77b1ecd4ec2
Jakub Jedlicka <jjedlick@redhat.com>
2025-04-09 22:52:16 +0200

    Add coverage for QUARKUS-5614 (`@PermissionAllowed` annotation on meta-annotation)
    (cherry picked from commit 4f3622fb4c1fc5290c46c02b5314c940e1a1217a)

---
+++ security/permissions/src/main/java/io/quarkus/ts/openshift/security/basic/permissions/resources/ClassAnnotationPermissionAllowedInclusiveResource.java
@@ -0,0 +1,25 @@
+package io.quarkus.ts.openshift.security.basic.permissions.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+import io.quarkus.security.PermissionsAllowed;
+
+@Path("/class-permission-allowed")
+@PermissionsAllowed(value = { "read:all", "create" }, inclusive = true)
+public class ClassAnnotationPermissionAllowedInclusiveResource {
+
+    @GET
+    @Path("/additional-permission-inclusive")
+    @PermissionsAllowed("read:minimal")
+    public String additionalPermission() {
+        // The method permission should override the class permission
+        return "Permitted";
+    }
+
+    @GET
+    @Path("/no-additional-permission-inclusive")
+    public String noAdditionalPermission() {
+        return "Permitted by class annotation";
+    }
+}
---
+++ security/permissions/src/test/java/io/quarkus/ts/openshift/security/basic/CustomPermissionAllowedIT.java
@@ -0,0 +1,98 @@
+package io.quarkus.ts.openshift.security.basic;
+
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import io.quarkus.test.scenarios.QuarkusScenario;
+
+@QuarkusScenario
+public class CustomPermissionAllowedIT extends BasePermissionAllowedIT {
+
+    public static final String ROOT_CUSTOM_ANNOTATION_PERMISSION_ALLOWED = "/custom-annotation-permission-allowed";
+    public static final String ROOT_CLASS_CUSTOM_PERMISSION_ALLOWED = "/class-custom-permission-allowed";
+    public static final String ROOT_COMBINED_PERMISSION_ALLOWED = "/combined-permission-allowed";
+
+    @ParameterizedTest
+    @CsvSource({ USER + "," + HttpStatus.SC_OK,
+            CREATOR + "," + HttpStatus.SC_FORBIDDEN,
+            SERVICE + "," + HttpStatus.SC_OK,
+            ADMIN + "," + HttpStatus.SC_OK,
+            UNKNOWN_USER + "," + HttpStatus.SC_UNAUTHORIZED })
+    public void testClassAndMethodCustomPermissionAllowed(String user, int statusCode) {
+        sendGetRequest(user, ROOT_CLASS_CUSTOM_PERMISSION_ALLOWED + "/additional-permission", statusCode);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ USER + "," + HttpStatus.SC_FORBIDDEN,
+            CREATOR + "," + HttpStatus.SC_OK,
+            SERVICE + "," + HttpStatus.SC_OK,
+            ADMIN + "," + HttpStatus.SC_OK,
+            UNKNOWN_USER + "," + HttpStatus.SC_UNAUTHORIZED })
+    public void testClassCustomPermissionAllowed(String user, int statusCode) {
+        sendGetRequest(user, ROOT_CLASS_CUSTOM_PERMISSION_ALLOWED + "/no-additional-permission", statusCode,
+                "Permitted by class annotation");
+    }
+
+    @ParameterizedTest
+    @CsvSource({ USER + "," + HttpStatus.SC_OK,
+            CREATOR + "," + HttpStatus.SC_FORBIDDEN,
+            SERVICE + "," + HttpStatus.SC_OK,
+            ADMIN + "," + HttpStatus.SC_OK,
+            UNKNOWN_USER + "," + HttpStatus.SC_UNAUTHORIZED })
+    public void testClassAndMethodCustomPermissionAllowedInclusive(String user, int statusCode) {
+        sendGetRequest(user, ROOT_CLASS_CUSTOM_PERMISSION_ALLOWED + "/additional-permission-inclusive", statusCode);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ USER + "," + HttpStatus.SC_FORBIDDEN,
+            CREATOR + "," + HttpStatus.SC_OK,
+            SERVICE + "," + HttpStatus.SC_FORBIDDEN,
+            ADMIN + "," + HttpStatus.SC_OK,
+            UNKNOWN_USER + "," + HttpStatus.SC_UNAUTHORIZED })
+    public void testClassPermissionCustomAllowedInclusive(String user, int statusCode) {
+        sendGetRequest(user, ROOT_CLASS_CUSTOM_PERMISSION_ALLOWED + "/no-additional-permission-inclusive", statusCode,
+                "Permitted by class annotation");
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "/annotated-can-read-and-create," + HttpStatus.SC_FORBIDDEN,
+            "/annotated-can-read-and-create-inclusive," + HttpStatus.SC_FORBIDDEN,
+            "/annotated-can-read-minimal," + HttpStatus.SC_FORBIDDEN,
+            "/annotated-can-read-minimal-inclusive," + HttpStatus.SC_FORBIDDEN })
+    public void testUserCombinedPermission(String endpoint, int statusCode) {
+        sendGetRequest(USER, ROOT_COMBINED_PERMISSION_ALLOWED + endpoint, statusCode);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "/annotated-can-read-and-create," + HttpStatus.SC_FORBIDDEN,
+            "/annotated-can-read-and-create-inclusive," + HttpStatus.SC_FORBIDDEN,
+            "/annotated-can-read-minimal," + HttpStatus.SC_FORBIDDEN,
+            "/annotated-can-read-minimal-inclusive," + HttpStatus.SC_FORBIDDEN })
+    public void testCreatorCombinedPermission(String endpoint, int statusCode) {
+        sendGetRequest(CREATOR, ROOT_COMBINED_PERMISSION_ALLOWED + endpoint, statusCode);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "/annotated-can-read-and-create," + HttpStatus.SC_OK,
+            "/annotated-can-read-and-create-inclusive," + HttpStatus.SC_FORBIDDEN,
+            "/annotated-can-read-minimal," + HttpStatus.SC_OK,
+            "/annotated-can-read-minimal-inclusive," + HttpStatus.SC_FORBIDDEN })
+    public void testServiceCombinedPermission(String endpoint, int statusCode) {
+        sendGetRequest(SERVICE, ROOT_COMBINED_PERMISSION_ALLOWED + endpoint, statusCode);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "/annotated-can-read-and-create," + HttpStatus.SC_OK,
+            "/annotated-can-read-and-create-inclusive," + HttpStatus.SC_OK,
+            "/annotated-can-read-minimal," + HttpStatus.SC_OK,
+            "/annotated-can-read-minimal-inclusive," + HttpStatus.SC_OK })
+    public void testAdminCombinedPermission(String endpoint, int statusCode) {
+        sendGetRequest(ADMIN, ROOT_COMBINED_PERMISSION_ALLOWED + endpoint, statusCode);
+    }
+
+    @Override
+    protected String getRoot() {
+        return ROOT_CUSTOM_ANNOTATION_PERMISSION_ALLOWED;
+    }
+}
