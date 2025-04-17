20ca98a6cf9b7774ef3e2704f73082ffffd651b3
Michal Vavřík <mvavrik@redhat.com>
2025-04-17 16:59:41 +0200

    Fix Keycloak OpenShift scenarios on IBM Power Z
    (cherry picked from commit c4dbc55716cedfecb0aa5b481ac939580d249268)

--- security/keycloak-oidc-client-reactive-extended/src/test/java/io/quarkus/ts/security/keycloak/oidcclient/reactive/extended/TokenPropagationFilterIT.java
+++ security/keycloak-oidc-client-reactive-extended/src/test/java/io/quarkus/ts/security/keycloak/oidcclient/reactive/extended/TokenPropagationFilterIT.java
@@ -1,5 +1,10 @@
 package io.quarkus.ts.security.keycloak.oidcclient.reactive.extended;
 
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM;
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_BASE_PATH;
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_FILE;
+import static io.quarkus.ts.security.keycloak.oidcclient.reactive.extended.OidcItUtils.USER;
+import static io.quarkus.ts.security.keycloak.oidcclient.reactive.extended.OidcItUtils.createToken;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 
@@ -7,16 +12,27 @@ import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import io.quarkus.test.bootstrap.KeycloakService;
+import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.services.KeycloakContainer;
+import io.quarkus.test.services.QuarkusApplication;
 
 @Tag("QUARKUS-3680")
 @QuarkusScenario
-public class TokenPropagationFilterIT extends BaseOidcIT {
+public class TokenPropagationFilterIT {
+
+    @KeycloakContainer(command = { "start-dev", "--import-realm", "--features=token-exchange" })
+    static KeycloakService keycloak = new KeycloakService(DEFAULT_REALM_FILE, DEFAULT_REALM, DEFAULT_REALM_BASE_PATH);
+
+    @QuarkusApplication
+    static RestService app = new RestService()
+            .withProperty("quarkus.oidc.auth-server-url", () -> keycloak.getRealmUrl());
 
     @Test
     public void usernameTest() {
         given()
-                .auth().oauth2(createToken())
+                .auth().oauth2(createToken(keycloak))
                 .when().get("/token-propagation-filter")
                 .then().statusCode(HttpStatus.SC_OK)
                 .body(containsString(USER));
--- security/keycloak/src/test/java/io/quarkus/ts/security/keycloak/MultiMechanismAuthenticationIT.java
+++ security/keycloak/src/test/java/io/quarkus/ts/security/keycloak/MultiMechanismAuthenticationIT.java
@@ -3,121 +3,18 @@ package io.quarkus.ts.security.keycloak;
 import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM;
 import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_BASE_PATH;
 import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_FILE;
-import static io.restassured.RestAssured.given;
-import static java.util.Base64.getEncoder;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.bootstrap.KeycloakService;
-import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
 import io.quarkus.test.services.KeycloakContainer;
-import io.quarkus.test.services.QuarkusApplication;
-import io.restassured.http.Header;
-import io.restassured.http.Headers;
 
 @QuarkusScenario
 @Tag("QUARKUS-5617")
-public class MultiMechanismAuthenticationIT {
-
-    static final String KC_NORMAL_USER = "test-normal-user";
-    static final String KC_ADMIN_USER = "test-admin-user";
-    static final String BASIC_NORMAL_USER = "user";
-    static final String BASIC_ADMIN_USER = "admin";
-    static final String CLIENT_ID_DEFAULT = "test-application-client";
-    static final String CLIENT_SECRET_DEFAULT = "test-application-client-secret";
+public class MultiMechanismAuthenticationIT extends AbstractMultiMechanismAuthenticationIT {
 
     @KeycloakContainer(command = { "start-dev", "--import-realm" })
     static KeycloakService keycloak = new KeycloakService(DEFAULT_REALM_FILE, DEFAULT_REALM, DEFAULT_REALM_BASE_PATH);
 
-    @QuarkusApplication
-    static RestService app = new RestService()
-            .withProperty("quarkus.oidc.auth-server-url", () -> keycloak.getRealmUrl())
-            .withProperty("quarkus.oidc.client-id", CLIENT_ID_DEFAULT)
-            .withProperty("quarkus.oidc.credentials.secret", CLIENT_SECRET_DEFAULT)
-            .withProperties("multi-mechanism.properties");
-
-    @Test
-    public void testAdminBasicAndOidcTogetherOnAdminResource() {
-        given().headers(prepareHeader(KC_ADMIN_USER, BASIC_ADMIN_USER))
-                .when().get("/admin")
-                .then()
-                .statusCode(HttpStatus.SC_OK);
-    }
-
-    @Test
-    public void testAdminBasicAndOidcTogetherOnUserResource() {
-        given().headers(prepareHeader(KC_ADMIN_USER, BASIC_ADMIN_USER))
-                .when().get("/user")
-                .then()
-                .statusCode(HttpStatus.SC_OK);
-    }
-
-    @Test
-    public void testUserBasicAndOidcTogetherOnUserResource() {
-        given().headers(prepareHeader(KC_NORMAL_USER, BASIC_NORMAL_USER))
-                .when().get("/user")
-                .then()
-                .statusCode(HttpStatus.SC_OK);
-    }
-
-    @Test
-    public void testUserBasicAndOidcTogetherOnAdminResource() {
-        given().headers(prepareHeader(KC_NORMAL_USER, BASIC_NORMAL_USER))
-                .when().get("/admin")
-                .then()
-                .statusCode(HttpStatus.SC_FORBIDDEN);
-    }
-
-    @Test
-    public void testAdminOnlyOneMechanism() {
-        given().headers(prepareHeader(null, BASIC_ADMIN_USER))
-                .when().get("/admin")
-                .then()
-                .statusCode(HttpStatus.SC_UNAUTHORIZED);
-
-        given().headers(prepareHeader(KC_ADMIN_USER, null))
-                .when().get("/admin")
-                .then()
-                .statusCode(HttpStatus.SC_UNAUTHORIZED);
-    }
-
-    @Test
-    public void testUserOnlyOneMechanism() {
-        given().headers(prepareHeader(null, BASIC_NORMAL_USER))
-                .when().get("/user")
-                .then()
-                .statusCode(HttpStatus.SC_UNAUTHORIZED);
-
-        given().headers(prepareHeader(KC_NORMAL_USER, null))
-                .when().get("/user")
-                .then()
-                .statusCode(HttpStatus.SC_UNAUTHORIZED);
-    }
-
-    private static Headers prepareHeader(String oidcUser, String basicUser) {
-        List<Header> headers = new ArrayList<>();
-        if (oidcUser != null) {
-            headers.add(new Header("Authorization", "Bearer " + createToken(oidcUser, oidcUser)));
-        }
-        if (basicUser != null) {
-            headers.add(new Header("Authorization", "Basic " + encodeBasicAuthentication(basicUser, basicUser)));
-        }
-        return new Headers(headers);
-    }
-
-    private static String createToken(String username, String password) {
-        return keycloak.createAuthzClient(CLIENT_ID_DEFAULT, CLIENT_SECRET_DEFAULT)
-                .obtainAccessToken(username, password).getToken();
-    }
-
-    private static String encodeBasicAuthentication(String username, String password) {
-        String credentialString = username + ":" + password;
-        return getEncoder().encodeToString(credentialString.getBytes());
-    }
 }
