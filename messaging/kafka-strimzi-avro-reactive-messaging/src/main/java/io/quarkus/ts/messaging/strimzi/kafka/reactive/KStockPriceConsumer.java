ddce000e9cc5b38efa0ab9ac7215b500db8bece3
Fedor Dudinsky <fdudinsk@redhat.com>
2025-04-17 15:34:51 +0200

    Make sure, that metadata can be read for batch messages in kafka
    https://issues.redhat.com/browse/QUARKUS-4914
    (cherry picked from commit 506a36d27345d92044571e1223241f7fe4dcaf78)

--- messaging/kafka-strimzi-avro-reactive-messaging/src/main/java/io/quarkus/ts/messaging/strimzi/kafka/reactive/KStockPriceConsumer.java
+++ messaging/kafka-strimzi-avro-reactive-messaging/src/main/java/io/quarkus/ts/messaging/strimzi/kafka/reactive/KStockPriceConsumer.java
@@ -2,19 +2,26 @@ package io.quarkus.ts.messaging.strimzi.kafka.reactive;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.jboss.logging.Logger;
 
 import io.quarkus.ts.messaging.kafka.StockPrice;
 import io.quarkus.ts.messaging.kafka.status;
+import io.smallrye.reactive.messaging.TracingMetadata;
 import io.smallrye.reactive.messaging.annotations.Broadcast;
+import io.smallrye.reactive.messaging.kafka.api.IncomingKafkaRecordBatchMetadata;
 import io.vertx.core.json.JsonObject;
 
 @ApplicationScoped
 public class KStockPriceConsumer {
+    private static final Logger LOG = Logger.getLogger(KStockPriceConsumer.class);
 
     @Incoming("channel-stock-price")
     @Outgoing("price-stream")
@@ -27,8 +34,14 @@ public class KStockPriceConsumer {
     @Incoming("channel-stock-price-batch")
     @Outgoing("price-stream-batch")
     @Broadcast
-    public List<String> processBatch(List<StockPrice> next) {
-        return next.stream()
+    public List<String> processBatch(ConsumerRecords<Integer, StockPrice> records,
+            IncomingKafkaRecordBatchMetadata<Integer, StockPrice> metadata) {
+        return StreamSupport.stream(records.spliterator(), false)
+                .peek(record -> {
+                    TracingMetadata meta = metadata.getMetadataForRecord(record, TracingMetadata.class);
+                    LOG.debug(meta.getCurrentContext());
+                })
+                .map(ConsumerRecord::value)
                 .map(KStockPriceConsumer::eventCompleted)
                 .map(KStockPriceConsumer::toJson)
                 .collect(Collectors.toList());
