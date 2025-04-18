44d8fbee771379cb9f48f96de60cab8750759a72
Michal Jurc <mjurc@redhat.com>
2025-04-18 16:53:16 +0200

    QUARKUS-5658 WS.next + Otel tests
    * Adding a test verifying that websocket operations (open/close) are
      traced by OpenTelemetry, and that the traces refer to eachother.
    
    Issue: https://github.com/quarkusio/quarkus/issues/39143
    
    Code change: https://github.com/quarkusio/quarkus/pull/41956
    
    Documentation: https://quarkus.io/version/main/guides/websockets-next-reference#telemetry
    
    Test plan: https://github.com/quarkus-qe/quarkus-test-plans/blob/main/QUARKUS-5658.md

--- websockets/websocket-next/src/test/java/io/quarkus/ts/websockets/next/BaseWebSocketIT.java
+++ websockets/websocket-next/src/test/java/io/quarkus/ts/websockets/next/BaseWebSocketIT.java
@@ -9,16 +9,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.ByteBuffer;
 import java.util.Base64;
 import java.util.HexFormat;
 import java.util.List;
-import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
 
 import org.awaitility.Awaitility;
-import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.handshake.ServerHandshake;
 import org.jboss.logging.Logger;
 import org.junit.jupiter.api.Test;
 
@@ -348,57 +344,4 @@ public abstract class BaseWebSocketIT {
         return client;
     }
 
-    public static class Client extends WebSocketClient {
-        private final LinkedBlockingDeque<String> messages = new LinkedBlockingDeque<>();
-        private final LinkedBlockingDeque<ByteBuffer> binaryMessages = new LinkedBlockingDeque<>();
-        private final boolean ignoreJoinMessages;
-
-        public Client(URI serverUri, boolean ignoreJoinMessages) {
-            super(serverUri);
-            this.ignoreJoinMessages = ignoreJoinMessages;
-        }
-
-        public Client(URI serverUri) {
-            super(serverUri);
-            this.ignoreJoinMessages = true;
-        }
-
-        @Override
-        public void onOpen(ServerHandshake serverHandshake) {
-            LOG.debug("New connection opened");
-        }
-
-        @Override
-        public void onMessage(String message) {
-            LOG.debug("Message received: " + message);
-            if (!message.endsWith("joined") || !ignoreJoinMessages) {
-                messages.add(message);
-            }
-        }
-
-        // receive binary message
-        @Override
-        public void onMessage(ByteBuffer bytes) {
-            LOG.debug("Binary message received: " + bytes.toString());
-            binaryMessages.add(bytes);
-        }
-
-        @Override
-        public void onClose(int i, String reason, boolean b) {
-            LOG.debug("WS connection closed for reason: " + reason);
-        }
-
-        @Override
-        public void onError(Exception e) {
-            LOG.error("Websocket Exception thrown: " + e.getMessage(), e);
-        }
-
-        public String waitForAndGetMessage() throws InterruptedException {
-            return messages.poll(2, TimeUnit.SECONDS);
-        }
-
-        public ByteBuffer waitForAndGetBinaryMessage() throws InterruptedException {
-            return binaryMessages.poll(2, TimeUnit.SECONDS);
-        }
-    }
 }
