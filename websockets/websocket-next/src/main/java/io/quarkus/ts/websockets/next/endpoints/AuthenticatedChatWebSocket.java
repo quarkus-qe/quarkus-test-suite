fb006d7e43b4bcceb12dd5037955f7cb8d819549
Rostislav Svoboda <rosta.svo@gmail.com>
2025-04-21 22:34:03 +0200

    Enhancement with permission checkers for WebSockets Next
    Primary motivation is native coverage
    TP: https://github.com/quarkus-qe/quarkus-test-plans/blob/main/QUARKUS-5859.md

--- websockets/websocket-next/src/main/java/io/quarkus/ts/websockets/next/endpoints/AuthenticatedChatWebSocket.java
+++ websockets/websocket-next/src/main/java/io/quarkus/ts/websockets/next/endpoints/AuthenticatedChatWebSocket.java
@@ -3,7 +3,11 @@ package io.quarkus.ts.websockets.next.endpoints;
 import jakarta.inject.Inject;
 
 import io.quarkus.security.Authenticated;
+import io.quarkus.security.ForbiddenException;
+import io.quarkus.security.PermissionChecker;
+import io.quarkus.security.PermissionsAllowed;
 import io.quarkus.security.identity.SecurityIdentity;
+import io.quarkus.websockets.next.OnError;
 import io.quarkus.websockets.next.OnTextMessage;
 import io.quarkus.websockets.next.WebSocket;
 
@@ -14,7 +18,20 @@ public class AuthenticatedChatWebSocket {
     SecurityIdentity currentIdentity;
 
     @OnTextMessage(broadcast = true)
+    @PermissionsAllowed("endpoint:invoke")
     public String echo(String message) {
         return currentIdentity.getPrincipal().getName() + ": " + message;
     }
+
+    @OnError
+    String error(ForbiddenException t) {
+        return "forbidden:" + currentIdentity.getPrincipal().getName();
+    }
+
+    @PermissionChecker("endpoint:invoke")
+    boolean canInvokeEndpoint(String message) {
+        String principalName = currentIdentity.getPrincipal().getName();
+        return principalName.equals("alice") || principalName.equals("bob");
+    }
+
 }
