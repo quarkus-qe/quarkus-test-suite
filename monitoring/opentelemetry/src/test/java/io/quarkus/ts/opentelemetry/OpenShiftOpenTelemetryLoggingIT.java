f3a3510f3a02efb9ce9cca2cfd454c809c1c24bc
Michal Vavřík <mvavrik@redhat.com>
2025-04-22 15:33:35 +0200

    Disable the Opentelemetry logging tests for IBM
    (cherry picked from commit a16054806b791dd93903df431c562bbe6248fa75)

--- monitoring/opentelemetry/src/test/java/io/quarkus/ts/opentelemetry/OpenShiftOpenTelemetryLoggingIT.java
+++ monitoring/opentelemetry/src/test/java/io/quarkus/ts/opentelemetry/OpenShiftOpenTelemetryLoggingIT.java
@@ -4,10 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import jakarta.inject.Inject;
 
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+
 import io.quarkus.test.bootstrap.inject.OpenShiftClient;
 import io.quarkus.test.scenarios.OpenShiftScenario;
 
 @OpenShiftScenario
+@DisabledIfSystemProperty(named = "ts.ibm-z-p.missing.services.excludes", matches = "true", disabledReason = "grafana/otel-lgtm container not available on s390x & ppc64le.")
 public class OpenShiftOpenTelemetryLoggingIT extends OpenTelemetryLoggingIT {
     @Inject
     static OpenShiftClient ocClient;
