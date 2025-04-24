36bfe5b16d7ba96bcc89d83483c417454187a09a
Rostislav Svoboda <rosta.svo@gmail.com>
2025-04-24 09:53:43 +0200

    Revert "Disable shouldKeepUsingTheSameQuarkusVersionAfterReload test because of upstream regression"
    This reverts commit 002e50b51645cca103e45c2c8291f090824b48ea.

--- quarkus-cli/src/test/java/io/quarkus/ts/quarkus/cli/QuarkusCliCreateJvmApplicationIT.java
+++ quarkus-cli/src/test/java/io/quarkus/ts/quarkus/cli/QuarkusCliCreateJvmApplicationIT.java
@@ -312,7 +312,6 @@ public class QuarkusCliCreateJvmApplicationIT {
 
     @Tag("https://github.com/quarkusio/quarkus/issues/25184")
     @Test
-    @Disabled("https://github.com/quarkusio/quarkus/issues/47418")
     public void shouldKeepUsingTheSameQuarkusVersionAfterReload() {
         // Generate application using old community version
         QuarkusCliRestService app = cliClient.createApplication("app", defaults()
