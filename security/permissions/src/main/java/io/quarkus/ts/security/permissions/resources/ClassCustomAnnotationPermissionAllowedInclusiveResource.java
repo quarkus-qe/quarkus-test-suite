3b7c4f36b42b607ea76de4b0926e9aa3dc174988
Jakub Jedlicka <jjedlick@redhat.com>
2025-04-29 16:25:23 +0200

    Change name of package in security/permissions module

---
+++ security/permissions/src/main/java/io/quarkus/ts/security/permissions/resources/ClassCustomAnnotationPermissionAllowedInclusiveResource.java
@@ -0,0 +1,26 @@
+package io.quarkus.ts.security.permissions.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+import io.quarkus.ts.security.permissions.annotations.CanReadAllAndCreateInclusive;
+import io.quarkus.ts.security.permissions.annotations.CanReadMinimal;
+
+@Path("/class-custom-permission-allowed")
+@CanReadAllAndCreateInclusive
+public class ClassCustomAnnotationPermissionAllowedInclusiveResource {
+
+    @GET
+    @Path("/additional-permission-inclusive")
+    @CanReadMinimal
+    public String additionalPermission() {
+        // The method permission should override the class permission
+        return "Permitted";
+    }
+
+    @GET
+    @Path("/no-additional-permission-inclusive")
+    public String noAdditionalPermission() {
+        return "Permitted by class annotation";
+    }
+}
