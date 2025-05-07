f5bdfed19ca989a8d97028bf74d6b1b87cc2030f
Troitskii Georgii <gtroitsk@redhat.com>
2025-05-07 14:40:23 +0200

    Add test coverage for programmatic OIDC tenants configuration

---
+++ security/keycloak-multitenant/src/test/java/io/quarkus/ts/security/keycloak/multitenant/KeycloakStaticMultiTenantUserInfoIT.java
@@ -0,0 +1,19 @@
+package io.quarkus.ts.security.keycloak.multitenant;
+
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM;
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_BASE_PATH;
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_FILE;
+
+import org.junit.jupiter.api.Tag;
+
+import io.quarkus.test.bootstrap.KeycloakService;
+import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.services.KeycloakContainer;
+
+@Tag("QUARKUS-5660")
+@QuarkusScenario
+public class KeycloakStaticMultiTenantUserInfoIT extends AbstractStaticMultiTenantUserInfoIT {
+
+    @KeycloakContainer(command = { "start-dev", "--import-realm" })
+    static KeycloakService keycloak = new KeycloakService(DEFAULT_REALM_FILE, DEFAULT_REALM, DEFAULT_REALM_BASE_PATH);
+}
