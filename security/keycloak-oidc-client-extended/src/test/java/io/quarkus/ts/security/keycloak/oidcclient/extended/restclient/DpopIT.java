ba1909896d5bd5ecc5bceb957a5bbe55af2e8a62
Martin Ocenas <mocenas@redhat.com>
2025-05-26 15:14:52 +0200

    Add test coverage for QUARKUS-5858
    (cherry picked from commit 529e52caf7fe6babd3c346bb50474b0c2deb9bc4)

---
+++ security/keycloak-oidc-client-extended/src/test/java/io/quarkus/ts/security/keycloak/oidcclient/extended/restclient/DpopIT.java
@@ -0,0 +1,15 @@
+package io.quarkus.ts.security.keycloak.oidcclient.extended.restclient;
+
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM;
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_BASE_PATH;
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_FILE;
+
+import io.quarkus.test.bootstrap.KeycloakService;
+import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.services.KeycloakContainer;
+
+@QuarkusScenario
+public class DpopIT extends AbstractDpopIT {
+    @KeycloakContainer(command = { "start-dev", "--import-realm", "--features=dpop" })
+    static KeycloakService keycloak = new KeycloakService(DEFAULT_REALM_FILE, DEFAULT_REALM, DEFAULT_REALM_BASE_PATH);
+}
