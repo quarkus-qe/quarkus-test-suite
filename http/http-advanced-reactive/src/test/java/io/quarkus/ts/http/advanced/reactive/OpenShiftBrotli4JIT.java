affce50eeb3e38bcb12ba46c46b769a277cc996a
Jakub Jedlicka <jjedlick@redhat.com>
2025-05-27 10:16:53 +0200

    Disable OpenShiftBrotli4JIT for IBM execution as the bug was fixed in 3.20 but won't be backported

--- http/http-advanced-reactive/src/test/java/io/quarkus/ts/http/advanced/reactive/OpenShiftBrotli4JIT.java
+++ http/http-advanced-reactive/src/test/java/io/quarkus/ts/http/advanced/reactive/OpenShiftBrotli4JIT.java
@@ -6,5 +6,6 @@ import io.quarkus.test.scenarios.OpenShiftScenario;
 
 @OpenShiftScenario
 @DisabledIfSystemProperty(named = "ts.arm.missing.services.excludes", matches = "true", disabledReason = "https://github.com/quarkusio/quarkus/issues/43770")
+@DisabledIfSystemProperty(named = "ts.ibm-z-p.missing.services.excludes", matches = "true", disabledReason = "https://github.com/quarkusio/quarkus/issues/43770")
 public class OpenShiftBrotli4JIT extends Brotli4JHttpIT {
 }
