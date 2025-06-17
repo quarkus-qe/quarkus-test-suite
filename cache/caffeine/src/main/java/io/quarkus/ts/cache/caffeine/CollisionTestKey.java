7a5859d2302eb68b6bcfdece0b0d5facb1313865
jcarranzan <jcarranz@redhat.com>
2025-06-17 15:08:46 +0200

    Add test coverage to ensure failed Unis are not cached QQE-674

---
+++ cache/caffeine/src/main/java/io/quarkus/ts/cache/caffeine/CollisionTestKey.java
@@ -0,0 +1,39 @@
+package io.quarkus.ts.cache.caffeine;
+
+import java.util.Objects;
+
+public class CollisionTestKey {
+    private String id;
+
+    public CollisionTestKey() {
+    }
+
+    public CollisionTestKey(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        return 42;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        CollisionTestKey collisionTestKey = (CollisionTestKey) obj;
+        return Objects.equals(id, collisionTestKey.id);
+    }
+}
