bfc5a0fc14997056b6a78cac528b147ce0516689
Michal Vavřík <mvavrik@redhat.com>
2025-07-18 16:48:18 +0200

    Add Hibernate Search 8.0 test coverage

---
+++ sql-db/hibernate-fulltext-search/src/main/java/io/quarkus/ts/hibernate/search/FruitProducer.java
@@ -0,0 +1,85 @@
+package io.quarkus.ts.hibernate.search;
+
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.GenericField;
+
+@Entity
+@Table(name = "fruit_producer")
+public class FruitProducer {
+
+    @Id
+    @MyCustomIdGeneratorAnnotation
+    private Integer id;
+
+    @GenericField
+    @Column(length = 40, unique = true)
+    private String name;
+
+    @JoinColumn(name = "fruit_id")
+    @ManyToOne(targetEntity = Fruit.class)
+    private Fruit fruit;
+
+    public FruitProducer(Integer id, String name, Fruit fruit) {
+        this.id = id;
+        this.name = name;
+        this.fruit = fruit;
+    }
+
+    public FruitProducer() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Fruit getFruit() {
+        // we need to break circular reference during serialization
+        return new Fruit(fruit.getId(), fruit.getName(), fruit.getPrice(), List.of());
+    }
+
+    public void setFruit(Fruit fruit) {
+        this.fruit = fruit;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass())
+            return false;
+        FruitProducer that = (FruitProducer) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "FruitProducer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
--- sql-db/hibernate-fulltext-search/src/test/resources/db/mysql/V1.0.0__init.sql
+++ sql-db/hibernate-fulltext-search/src/test/resources/db/mysql/V1.0.0__init.sql
@@ -1,1 +1,3 @@
-CREATE TABLE known_fruits (id INT PRIMARY KEY AUTO_INCREMENT,name VARCHAR(40));
+CREATE TABLE known_fruits (id INT PRIMARY KEY AUTO_INCREMENT,name VARCHAR(40), price FLOAT);
+CREATE TABLE known_vegetables (id INT PRIMARY KEY,name VARCHAR(40));
+CREATE TABLE fruit_producer (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(40) UNIQUE, fruit_id INT, PRIMARY KEY (id), CONSTRAINT FK_fruit_producer_fruit_id FOREIGN KEY (fruit_id) REFERENCES known_fruits (id));
