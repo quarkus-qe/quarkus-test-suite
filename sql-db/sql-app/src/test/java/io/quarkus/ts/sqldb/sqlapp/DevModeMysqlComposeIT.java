b97365ee0b71837615ad9972f7600904c424d3c9
Rostislav Svoboda <rosta.svo@gmail.com>
2025-07-24 17:08:22 +0200

    Compose Dev Services coverage for PostgreSQL, MariaDB, MySQL

---
+++ sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/DevModeMysqlComposeIT.java
@@ -0,0 +1,24 @@
+package io.quarkus.ts.sqldb.sqlapp;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.bootstrap.RestService;
+import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.services.DevModeQuarkusApplication;
+
+@Tag("fips-incompatible") // TODO: enable when https://github.com/quarkusio/quarkus/issues/40526 gets fixed
+@Tag("QUARKUS-6234")
+@QuarkusScenario
+public class DevModeMysqlComposeIT extends AbstractSqlDatabaseIT {
+
+    @DevModeQuarkusApplication(properties = "mysql.properties")
+    static RestService app = new RestService()
+            .withProperty("quarkus.compose.devservices.files", "src/main/resources/mysql-compose-devservices.yml");
+
+    @Test
+    public void composeDevServicesAreUsed() {
+        app.logs().assertContains("Compose is running command");
+        app.logs().assertContains("library/mysql:9.4");
+    }
+}
