262d537827ed9379f731bfd06a003e87b0d8bbdf
Jakub Jedlicka <jjedlick@redhat.com>
2025-08-04 14:48:12 +0200

    Bump RHBK version to the latest one and enable ARM tests
    This version is first which support ARM64 architecture

--- security/oidc-client-mutual-tls/src/test/java/io/quarkus/ts/security/oidcclient/mtls/OpenShiftRhSsoOidcMtlsIT.java
+++ security/oidc-client-mutual-tls/src/test/java/io/quarkus/ts/security/oidcclient/mtls/OpenShiftRhSsoOidcMtlsIT.java
@@ -6,7 +6,6 @@ import static org.keycloak.representations.idm.CredentialRepresentation.PASSWORD
 import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 
 import io.quarkus.test.bootstrap.KeycloakService;
@@ -21,7 +20,6 @@ Enabling this may require fixing config map names created for resources
  */
 @Disabled("https://github.com/quarkus-qe/quarkus-test-framework/issues/1052")
 @OpenShiftScenario
-@DisabledIfSystemProperty(named = "ts.arm.missing.services.excludes", matches = "true", disabledReason = "https://github.com/quarkus-qe/quarkus-test-suite/issues/1145")
 @EnabledIfSystemProperty(named = "ts.redhat.registry.enabled", matches = "true")
 public class OpenShiftRhSsoOidcMtlsIT extends KeycloakMtlsAuthN {
 
