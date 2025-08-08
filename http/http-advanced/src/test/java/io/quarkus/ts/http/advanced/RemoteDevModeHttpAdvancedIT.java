f11da0a334fbc7bb216838b74ec3725cac10abc1
Michal Vavřík <mvavrik@redhat.com>
2025-08-08 15:04:25 +0200

    Workaorund remote dev Windows issue

--- http/http-advanced/src/test/java/io/quarkus/ts/http/advanced/RemoteDevModeHttpAdvancedIT.java
+++ http/http-advanced/src/test/java/io/quarkus/ts/http/advanced/RemoteDevModeHttpAdvancedIT.java
@@ -11,6 +11,8 @@ import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
 import io.quarkus.test.services.IsRunningCheck;
 import io.quarkus.test.services.RemoteDevModeQuarkusApplication;
+import io.quarkus.test.utils.AwaitilityUtils;
+import io.smallrye.common.os.OS;
 
 @QuarkusScenario
 public class RemoteDevModeHttpAdvancedIT {
@@ -23,12 +25,23 @@ public class RemoteDevModeHttpAdvancedIT {
 
     @Test
     public void serviceShouldBeUpAndRunning() {
-        given().get("/api/hello").then().statusCode(HttpStatus.SC_OK).body("content", is("Hello, World!"));
+        // TODO: drop Windows workaround when https://github.com/quarkusio/quarkus/issues/49434 is fixed
+        if (OS.WINDOWS.isCurrent()) {
+            AwaitilityUtils.untilAsserted(
+                    () -> given().get("/api/hello").then().statusCode(HttpStatus.SC_OK).body("content", is("Hello, World!")));
+        } else {
+            given().get("/api/hello").then().statusCode(HttpStatus.SC_OK).body("content", is("Hello, World!"));
+        }
     }
 
     @Tag("QUARKUS-834")
     @Test
     public void devUiShouldBeNotFound() {
-        app.given().get("/q/dev").then().statusCode(HttpStatus.SC_NOT_FOUND);
+        // TODO: drop Windows workaround when https://github.com/quarkusio/quarkus/issues/49434 is fixed
+        if (OS.WINDOWS.isCurrent()) {
+            AwaitilityUtils.untilAsserted(() -> app.given().get("/q/dev").then().statusCode(HttpStatus.SC_NOT_FOUND));
+        } else {
+            app.given().get("/q/dev").then().statusCode(HttpStatus.SC_NOT_FOUND);
+        }
     }
 }
