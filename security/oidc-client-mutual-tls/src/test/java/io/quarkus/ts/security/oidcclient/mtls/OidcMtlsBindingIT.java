7ec914fa66457ac3c22d8972753e42193ea66d8f
Jakub Jedlicka <jjedlick@redhat.com>
2025-08-18 09:01:34 +0200

    Remove `LocalHostKeycloakContainerManagedResourceBuilder` custom builder as the FW setting it now

--- security/oidc-client-mutual-tls/src/test/java/io/quarkus/ts/security/oidcclient/mtls/OidcMtlsBindingIT.java
+++ security/oidc-client-mutual-tls/src/test/java/io/quarkus/ts/security/oidcclient/mtls/OidcMtlsBindingIT.java
@@ -10,12 +10,12 @@ import io.quarkus.test.services.KeycloakContainer;
 @QuarkusScenario
 public class OidcMtlsBindingIT extends AbstractOidcMtlsBindingIT {
 
-    @KeycloakContainer(command = { "start", "--import-realm", "--hostname-strict=false",
+    @KeycloakContainer(exposeRawTls = true, command = { "start", "--import-realm", "--hostname-strict=false",
             "--features=token-exchange",
             "--hostname=localhost",
             "--https-client-auth=required", "--https-key-store-file=/etc/server-keystore.p12",
             "--https-trust-store-file=/etc/server-truststore.p12",
-            "--https-trust-store-password=password" }, port = KEYCLOAK_PORT, builder = LocalHostKeycloakContainerManagedResourceBuilder.class)
+            "--https-trust-store-password=password" }, port = KEYCLOAK_PORT)
     static KeycloakService keycloak = newKeycloakInstance(DEFAULT_REALM_FILE, REALM_DEFAULT, DEFAULT_REALM_BASE_PATH)
             .withProperty("HTTPS_KEYSTORE", "resource_with_destination::/etc/|server-keystore.p12")
             .withProperty("HTTPS_TRUSTSTORE", "resource_with_destination::/etc/|server-truststore.p12");
