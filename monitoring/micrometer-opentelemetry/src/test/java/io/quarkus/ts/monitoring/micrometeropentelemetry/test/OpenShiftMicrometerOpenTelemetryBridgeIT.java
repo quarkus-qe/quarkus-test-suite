08048bafd3bbb379fa96c94a6c34da397321ec18
swasinghat <swasingh@redhat.com>
2025-09-04 17:29:02 +0530

    Updated z/p profile
    1. Updated z/p profile
    2. Disabled OpenShiftMicrometerOpenTelemetryBridgeIT test for IBM Z/P with no supporting images
    
    (cherry picked from commit f5ae480174e459eeb51d615fef687d25bb3da8a7)

--- monitoring/micrometer-opentelemetry/src/test/java/io/quarkus/ts/monitoring/micrometeropentelemetry/test/OpenShiftMicrometerOpenTelemetryBridgeIT.java
+++ monitoring/micrometer-opentelemetry/src/test/java/io/quarkus/ts/monitoring/micrometeropentelemetry/test/OpenShiftMicrometerOpenTelemetryBridgeIT.java
@@ -1,8 +1,10 @@
 package io.quarkus.ts.monitoring.micrometeropentelemetry.test;
 
 import io.quarkus.test.scenarios.OpenShiftScenario;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 
 @OpenShiftScenario
+@DisabledIfSystemProperty(named = "ts.ibm-z-p.missing.services.excludes", matches = "true", disabledReason = "grafana/otel-lgtm container not available on s390x & ppc64le.")
 public class OpenShiftMicrometerOpenTelemetryBridgeIT extends MicrometerOpenTelemetryBridgeIT {
 
 }
