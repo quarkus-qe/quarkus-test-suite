3ea0f073ac1cb5ecd197ea19191d393b8b8a200d
Michal Vavřík <michal.vavrik@aol.com>
2025-09-04 15:10:23 +0200

    Fix OpenShiftMicrometerOpenTelemetryBridgeIT formatting

--- monitoring/micrometer-opentelemetry/src/test/java/io/quarkus/ts/monitoring/micrometeropentelemetry/test/OpenShiftMicrometerOpenTelemetryBridgeIT.java
+++ monitoring/micrometer-opentelemetry/src/test/java/io/quarkus/ts/monitoring/micrometeropentelemetry/test/OpenShiftMicrometerOpenTelemetryBridgeIT.java
@@ -1,7 +1,8 @@
 package io.quarkus.ts.monitoring.micrometeropentelemetry.test;
 
-import io.quarkus.test.scenarios.OpenShiftScenario;
 import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+
+import io.quarkus.test.scenarios.OpenShiftScenario;
 
 @OpenShiftScenario
 @DisabledIfSystemProperty(named = "ts.ibm-z-p.missing.services.excludes", matches = "true", disabledReason = "grafana/otel-lgtm container not available on s390x & ppc64le.")
