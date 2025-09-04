ba0e65da39277b9cf2d29e4fa01a77828ed8ee6f
Michal Vavřík <michal.vavrik@aol.com>
2025-09-04 16:53:45 +0200

    Test enabling recovery for individual datasources
    (cherry picked from commit cea391cad1046bfa91e832a99507b9c5262d3c8a)

--- sql-db/narayana-transactions/src/test/java/io/quarkus/ts/transactions/MariaDbTransactionGeneralUsageIT.java
+++ sql-db/narayana-transactions/src/test/java/io/quarkus/ts/transactions/MariaDbTransactionGeneralUsageIT.java
@@ -1,5 +1,7 @@
 package io.quarkus.ts.transactions;
 
+import org.junit.jupiter.api.Tag;
+
 import io.quarkus.test.bootstrap.MariaDbService;
 import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
@@ -10,6 +12,7 @@ import io.quarkus.ts.transactions.recovery.TransactionExecutor;
 @QuarkusScenario
 public class MariaDbTransactionGeneralUsageIT extends TransactionCommons {
 
+    private static final String DATASOURCE_JDBC_ENABLE_RECOVERY = "quarkus.datasource.%s.jdbc.enable-recovery";
     static final int MARIADB_PORT = 3306;
 
     @Container(image = "${mariadb.11.image}", port = MARIADB_PORT, expectedLog = "socket: '.*/mysql.*sock'  port: "
@@ -33,4 +36,21 @@ public class MariaDbTransactionGeneralUsageIT extends TransactionCommons {
         return TransactionExecutor.QUARKUS_TRANSACTION;
     }
 
+    @Tag("QUARKUS-5709")
+    @Override
+    protected void enableTransactionRecovery() {
+        app.withProperty(getDsJdbcEnableRecovery("xa-ds-1"), Boolean.TRUE.toString());
+        app.withProperty(getDsJdbcEnableRecovery("xa-ds-2"), Boolean.TRUE.toString());
+    }
+
+    @Tag("QUARKUS-5709")
+    @Override
+    protected void disableTransactionRecovery() {
+        app.withProperty(getDsJdbcEnableRecovery("xa-ds-1"), Boolean.FALSE.toString());
+        app.withProperty(getDsJdbcEnableRecovery("xa-ds-2"), Boolean.FALSE.toString());
+    }
+
+    private String getDsJdbcEnableRecovery(String datasourceName) {
+        return DATASOURCE_JDBC_ENABLE_RECOVERY.formatted(datasourceName);
+    }
 }
