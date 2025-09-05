e14542966ace82b3c37f2ee3d83fb859119ba8f3
jcarranzan <jcarranz@redhat.com>
2025-09-05 12:05:52 +0200

    Add test coverage for QUARKUS-6263 OIDC Health Checks
    (cherry picked from commit a033894c81606ac58651c9bb8bf4b546ca33953e)

---
+++ security/keycloak-oidc-client-extended/src/test/java/io/quarkus/ts/security/keycloak/oidcclient/extended/restclient/OpenShiftMultiTenantHealthCheckIT.java
@@ -0,0 +1,17 @@
+package io.quarkus.ts.security.keycloak.oidcclient.extended.restclient;
+
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM;
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_BASE_PATH;
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_FILE;
+
+import io.quarkus.test.bootstrap.KeycloakService;
+import io.quarkus.test.scenarios.OpenShiftScenario;
+import io.quarkus.test.scenarios.annotations.DisabledOnNative;
+import io.quarkus.test.services.KeycloakContainer;
+
+@OpenShiftScenario
+@DisabledOnNative(reason = "Only for JVM verification")
+public class OpenShiftMultiTenantHealthCheckIT extends AbstractMultiTenantHealthCheckIT {
+    @KeycloakContainer(runKeycloakInProdMode = true, image = "${rhbk.image}")
+    static KeycloakService keycloak = new KeycloakService(DEFAULT_REALM_FILE, DEFAULT_REALM, DEFAULT_REALM_BASE_PATH);
+}
