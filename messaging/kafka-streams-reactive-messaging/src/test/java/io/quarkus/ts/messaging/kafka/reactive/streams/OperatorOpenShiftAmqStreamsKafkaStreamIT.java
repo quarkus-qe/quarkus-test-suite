e1b32259c347bdc94d504fffc62c2a131e477279
Martin Ocenas <mocenas@redhat.com>
2025-09-09 15:39:27 +0200

    Bump kafka to 4.0.0
    (cherry picked from commit c755ce88ca16be3c032d4ef32e05d8013532a931)

--- messaging/kafka-streams-reactive-messaging/src/test/java/io/quarkus/ts/messaging/kafka/reactive/streams/OperatorOpenShiftAmqStreamsKafkaStreamIT.java
+++ messaging/kafka-streams-reactive-messaging/src/test/java/io/quarkus/ts/messaging/kafka/reactive/streams/OperatorOpenShiftAmqStreamsKafkaStreamIT.java
@@ -10,9 +10,8 @@ import io.quarkus.test.services.operator.KafkaInstance;
 @OpenShiftScenario
 public class OperatorOpenShiftAmqStreamsKafkaStreamIT extends BaseKafkaStreamTest {
 
-    // TODO: use default KafkaInstance constructor when amq streams operator will support kafka 3.9.0+
     @Operator(name = "amq-streams", source = "redhat-operators")
-    static KafkaInstance kafka = new KafkaInstance("kafka-instance", "/amq-streams-operator-kafka-instance.yaml");
+    static KafkaInstance kafka = new KafkaInstance();
 
     @QuarkusApplication
     static RestService app = new RestService()
