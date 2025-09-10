a2e14e004aa9573dd30fe1815ab3ed8fe8bca912
Michal Vavřík <michal.vavrik@aol.com>
2025-09-10 15:25:16 +0200

    Test Infinispan cache CDI request ctx propagation
    (cherry picked from commit 932b2ff8c3c35620d30cc88f08cc054e195445d4)

--- cache/infinispan/src/main/java/io/quarkus/ts/cache/infinispan/BaseServiceWithCache.java
+++ cache/infinispan/src/main/java/io/quarkus/ts/cache/infinispan/BaseServiceWithCache.java
@@ -1,6 +1,10 @@
 package io.quarkus.ts.cache.infinispan;
 
+import java.time.Duration;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import jakarta.inject.Inject;
 
 import org.infinispan.protostream.GeneratedSchema;
 import org.infinispan.protostream.annotations.Proto;
@@ -10,12 +14,17 @@ import io.quarkus.cache.CacheInvalidate;
 import io.quarkus.cache.CacheInvalidateAll;
 import io.quarkus.cache.CacheKey;
 import io.quarkus.cache.CacheResult;
+import io.quarkus.ts.cache.infinispan.cdi.request.context.RequestContextState;
+import io.smallrye.mutiny.Uni;
 
 public abstract class BaseServiceWithCache {
 
     private static final String CACHE_NAME = "service-cache";
 
     private static final AtomicInteger counter = new AtomicInteger(0);
+
+    @Inject
+    RequestContextState requestContextState;
 
     @CacheResult(cacheName = CACHE_NAME)
     public String getValue() {
@@ -29,7 +38,9 @@ public abstract class BaseServiceWithCache {
 
     @CacheResult(cacheName = CACHE_NAME)
     public ExpensiveResponse getValueWithPrefix(@CacheKey String prefix) {
-        return new ExpensiveResponse(prefix + ": " + counter.getAndIncrement());
+        String result = prefix + ": " + counter.getAndIncrement();
+        requestContextState.setState(result);
+        return new ExpensiveResponse(result);
     }
 
     @CacheInvalidate(cacheName = CACHE_NAME)
@@ -48,5 +59,16 @@ public abstract class BaseServiceWithCache {
 
     @ProtoSchema(includeClasses = { ExpensiveResponse.class })
     interface Schema extends GeneratedSchema {
+    }
+
+    @CacheResult(cacheName = CACHE_NAME)
+    public CompletionStage<ExpensiveResponse> getCompletionStageValueWithPrefix(@CacheKey String prefix) {
+        String result = prefix + ": " + counter.getAndIncrement();
+        requestContextState.setState(result);
+        var expensiveResponse = new ExpensiveResponse(result);
+        return Uni.createFrom().item(expensiveResponse)
+                // delay so that the response is not completed immediately
+                .onItem().delayIt().by(Duration.ofSeconds(2))
+                .subscribeAsCompletionStage();
     }
 }
