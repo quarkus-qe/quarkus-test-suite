f92e1368ff42a6f2087b17f4d8915b6d3d14d7d5
Troitskii Georgii <gtroitsk@redhat.com>
2025-09-17 00:35:15 +0200

    Add reproducer for #48983: Kafka consumer hangs on REST client call
    (cherry picked from commit 19759dcebf8e7a2b1e6623b2ef2f4899e5971a54)

--- messaging/kafka-confluent-avro-reactive-messaging/src/test/java/io/quarkus/ts/messaging/kafka/ConfluentKafkaAvroIT.java
+++ messaging/kafka-confluent-avro-reactive-messaging/src/test/java/io/quarkus/ts/messaging/kafka/ConfluentKafkaAvroIT.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Tag;
 import io.quarkus.test.bootstrap.KafkaService;
 import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.services.Dependency;
 import io.quarkus.test.services.KafkaContainer;
 import io.quarkus.test.services.QuarkusApplication;
 import io.quarkus.test.services.containers.model.KafkaVendor;
@@ -16,7 +17,7 @@ public class ConfluentKafkaAvroIT extends BaseKafkaAvroIT {
     @KafkaContainer(vendor = KafkaVendor.CONFLUENT, withRegistry = true)
     static final KafkaService kafka = new KafkaService();
 
-    @QuarkusApplication
+    @QuarkusApplication(dependencies = @Dependency(artifactId = "quarkus-resteasy-jsonb"))
     static final RestService app = new RestService()
             .withProperty("kafka.bootstrap.servers", kafka::getBootstrapUrl)
             .withProperty("confluent.registry.url", kafka::getRegistryUrl);
