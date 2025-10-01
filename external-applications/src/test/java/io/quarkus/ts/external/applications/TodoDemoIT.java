e77ff23f464028479c35298b75407ad3afe16fc7
Martin Ocenas <mocenas@redhat.com>
2025-10-01 17:04:50 +0200

    Pin TodoDemoIT app to specific commit

--- external-applications/src/test/java/io/quarkus/ts/external/applications/TodoDemoIT.java
+++ external-applications/src/test/java/io/quarkus/ts/external/applications/TodoDemoIT.java
@@ -17,6 +17,7 @@ import io.restassured.http.ContentType;
 @QuarkusScenario
 public class TodoDemoIT {
     private static final String TODO_REPO = "https://github.com/quarkusio/todo-demo-app.git";
+    private static final String COMMIT = "a7cde7554b3eb445991522afd8949cdfdcc0547c";
     private static final String VERSIONS = "-Dquarkus.platform.group-id=${QUARKUS_PLATFORM_GROUP-ID} -Dquarkus.platform.version=${QUARKUS_PLATFORM_VERSION} ";
     private static final String DEFAULT_OPTIONS = " -DskipTests=true " + VERSIONS;
 
@@ -25,13 +26,14 @@ public class TodoDemoIT {
             // store data in /tmp/psql as in OpenShift we don't have permissions to /var/lib/postgresql/data
             .withProperty("PGDATA", "/tmp/psql");
 
-    @GitRepositoryQuarkusApplication(repo = TODO_REPO, mavenArgs = "-Dquarkus.package.jar.type=uber-jar" + DEFAULT_OPTIONS)
+    @GitRepositoryQuarkusApplication(repo = TODO_REPO, branch = COMMIT, mavenArgs = "-Dquarkus.package.jar.type=uber-jar"
+            + DEFAULT_OPTIONS)
     static final RestService app = new RestService()
             .withProperty("quarkus.datasource.username", database.getUser())
             .withProperty("quarkus.datasource.password", database.getPassword())
             .withProperty("quarkus.datasource.jdbc.url", database::getJdbcUrl);
 
-    @GitRepositoryQuarkusApplication(repo = TODO_REPO, artifact = "todo-backend-1.0-SNAPSHOT.jar", mavenArgs = "-Dquarkus.package.jar.type=uber-jar -Dquarkus.package.add-runner-suffix=false"
+    @GitRepositoryQuarkusApplication(repo = TODO_REPO, branch = COMMIT, artifact = "todo-backend-1.0-SNAPSHOT.jar", mavenArgs = "-Dquarkus.package.jar.type=uber-jar -Dquarkus.package.add-runner-suffix=false"
             + DEFAULT_OPTIONS)
     static final RestService replaced = new RestService()
             .withProperty("quarkus.datasource.username", database.getUser())
