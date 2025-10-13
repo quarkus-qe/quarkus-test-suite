7f31c980110dc66f34d2f54e449c21e983f55ee6
Rostislav Svoboda <rosta.svo@gmail.com>
2025-10-13 15:34:11 +0200

    Set up path-specific authorization programmatically
    TD for https://issues.redhat.com/browse/QUARKUS-6472

---
+++ security/keycloak-oidc-client-reactive-extended/src/main/java/io/quarkus/ts/security/keycloak/oidcclient/reactive/extended/HttpSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package io.quarkus.ts.security.keycloak.oidcclient.reactive.extended;
+
+import jakarta.enterprise.event.Observes;
+
+import io.quarkus.vertx.http.security.HttpSecurity;
+
+public class HttpSecurityConfiguration {
+
+    void configure(@Observes HttpSecurity httpSecurity) {
+        httpSecurity
+                .path("/generate-token/*", "/token/*", "/filter-messages/*", "/token-refresh-public/*",
+                        "/filter-customization-messages/*", "/client-registration/*")
+                .permit()
+                .path("/*").authenticated();
+    }
+
+    void configureLogout(@Observes HttpSecurity httpSecurity) {
+        httpSecurity
+                .get("/code-flow").permit()
+                .path("/code-flow/logout").authenticated();
+    }
+
+}
