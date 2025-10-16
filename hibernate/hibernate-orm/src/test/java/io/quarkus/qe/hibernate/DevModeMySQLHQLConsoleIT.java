02d112899f7232987c9e23839d33f2743c51e8eb
Michal Vavřík <michal.vavrik@aol.com>
2025-10-16 18:44:49 +0200

    Fix HQL DEV UI TD on Windows
    (cherry picked from commit ed3dc00397569a689f74536e848e265bba5b50f6)

--- hibernate/hibernate-orm/src/test/java/io/quarkus/qe/hibernate/DevModeMySQLHQLConsoleIT.java
+++ hibernate/hibernate-orm/src/test/java/io/quarkus/qe/hibernate/DevModeMySQLHQLConsoleIT.java
@@ -1,11 +1,14 @@
 package io.quarkus.qe.hibernate;
 
+import java.util.Map;
+
 import org.junit.jupiter.api.Tag;
 
 import io.quarkus.test.bootstrap.DevModeQuarkusService;
 import io.quarkus.test.scenarios.QuarkusScenario;
 import io.quarkus.test.services.Dependency;
 import io.quarkus.test.services.DevModeQuarkusApplication;
+import io.smallrye.common.os.OS;
 
 @QuarkusScenario
 @Tag("QUARKUS-6243")
@@ -13,5 +16,15 @@ public class DevModeMySQLHQLConsoleIT extends AbstractHQLConsoleIT {
 
     @DevModeQuarkusApplication(dependencies = @Dependency(artifactId = "quarkus-jdbc-mysql"))
     static DevModeQuarkusService app = (DevModeQuarkusService) new DevModeQuarkusService()
-            .withProperties("mysql.properties");
+            .withProperties("mysql.properties")
+            .withProperties(() -> {
+                if (OS.WINDOWS.isCurrent() && DockerUtils.isNotPodman()) {
+                    String dockerIp = System.getenv(DockerUtils.DOCKER_IP);
+                    if (dockerIp != null && !dockerIp.isEmpty()) {
+                        // container IP in Minikube is not localhost
+                        return Map.of("quarkus.datasource.dev-ui.allowed-db-host", dockerIp);
+                    }
+                }
+                return Map.of();
+            });
 }
