ed3dc00397569a689f74536e848e265bba5b50f6
Michal Vavřík <michal.vavrik@aol.com>
2025-10-16 18:44:49 +0200

    Fix HQL DEV UI TD on Windows

--- hibernate/hibernate-orm/src/test/java/io/quarkus/qe/hibernate/DevModePostgresqlHQLConsoleIT.java
+++ hibernate/hibernate-orm/src/test/java/io/quarkus/qe/hibernate/DevModePostgresqlHQLConsoleIT.java
@@ -1,15 +1,28 @@
 package io.quarkus.qe.hibernate;
 
+import java.util.Map;
+
 import org.junit.jupiter.api.Tag;
 
 import io.quarkus.test.bootstrap.DevModeQuarkusService;
 import io.quarkus.test.scenarios.QuarkusScenario;
 import io.quarkus.test.services.DevModeQuarkusApplication;
+import io.smallrye.common.os.OS;
 
 @QuarkusScenario
 @Tag("QUARKUS-6243")
 public class DevModePostgresqlHQLConsoleIT extends AbstractHQLConsoleIT {
 
     @DevModeQuarkusApplication
-    static DevModeQuarkusService app = new DevModeQuarkusService();
+    static DevModeQuarkusService app = (DevModeQuarkusService) new DevModeQuarkusService()
+            .withProperties(() -> {
+                if (OS.WINDOWS.isCurrent() && DockerUtils.isNotPodman()) {
+                    String dockerIp = System.getenv(DockerUtils.DOCKER_IP);
+                    if (dockerIp != null && !dockerIp.isEmpty()) {
+                        // container IP in Minikube is not localhost
+                        return Map.of("quarkus.datasource.dev-ui.allowed-db-host", dockerIp);
+                    }
+                }
+                return Map.of();
+            });
 }
