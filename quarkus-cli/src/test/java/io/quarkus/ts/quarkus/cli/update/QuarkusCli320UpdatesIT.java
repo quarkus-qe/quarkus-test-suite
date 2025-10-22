2bf97c3cf881de6d1a5f698ffd3cce2819890a55
Michal Vavřík <michal.vavrik@aol.com>
2025-10-22 22:17:10 +0200

    Fix CLI update scenario testing app properties

--- quarkus-cli/src/test/java/io/quarkus/ts/quarkus/cli/update/QuarkusCli320UpdatesIT.java
+++ quarkus-cli/src/test/java/io/quarkus/ts/quarkus/cli/update/QuarkusCli320UpdatesIT.java
@@ -52,6 +52,9 @@ public class QuarkusCli320UpdatesIT extends AbstractQuarkusCliUpdateIT {
         oldProperties.put("quarkus.log.console.json", "true");
         newProperties.put("quarkus.log.console.json.enabled", "true");
 
-        QuarkusCLIUtils.checkPropertiesUpdate(quarkusCLIAppManager, oldProperties, newProperties);
+        // TODO: drop using temp dir when https://github.com/quarkusio/quarkus-updates/issues/394 is fixed
+        try (var ignored = cliClient.useTemporaryDirectory()) {
+            QuarkusCLIUtils.checkPropertiesUpdate(quarkusCLIAppManager, oldProperties, newProperties);
+        }
     }
 }
