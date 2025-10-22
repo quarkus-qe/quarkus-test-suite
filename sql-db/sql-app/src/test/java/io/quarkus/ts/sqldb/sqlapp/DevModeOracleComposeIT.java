3c738d8aaddd260a1f4181a0b101eafb05b9c041
Martin Ocenas <mocenas@redhat.com>
2025-10-22 11:33:32 +0200

    Disable Oracle compose dev service on win and RHBQ

--- sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/DevModeOracleComposeIT.java
+++ sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/DevModeOracleComposeIT.java
@@ -3,6 +3,8 @@ package io.quarkus.ts.sqldb.sqlapp;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
@@ -12,6 +14,7 @@ import io.quarkus.test.services.DevModeQuarkusApplication;
 @Tag("podman-incompatible") // TODO: https://github.com/quarkusio/quarkus/issues/38003
 @QuarkusScenario
 @DisabledIfSystemProperty(named = "ts.arm.missing.services.excludes", matches = "true", disabledReason = "https://github.com/quarkusio/quarkus/issues/43375")
+@DisabledOnOs(value = OS.WINDOWS, disabledReason = "QUARKUS-6784")
 public class DevModeOracleComposeIT extends AbstractSqlDatabaseIT {
 
     @DevModeQuarkusApplication(properties = "oracle.properties")
