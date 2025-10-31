12e87cdba43af0754c5a3259eca11a340804a69b
Fedor Dudinsky <fdudinsk@redhat.com>
2025-10-31 09:43:39 +0100

    Verify, that Pool can be manually initilised in Native mode
    Checks, that https://github.com/quarkusio/quarkus/issues/50140 was fixed.
    Also, fix a typo

--- sql-db/vertx-sql/src/test/java/io/quarkus/ts/vertx/sql/handlers/PostgresqlHandlerIT.java
+++ sql-db/vertx-sql/src/test/java/io/quarkus/ts/vertx/sql/handlers/PostgresqlHandlerIT.java
@@ -1,10 +1,17 @@
 package io.quarkus.ts.vertx.sql.handlers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
 import io.quarkus.test.bootstrap.PostgresqlService;
 import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.scenarios.annotations.DisabledOnQuarkusVersion;
 import io.quarkus.test.services.Container;
 import io.quarkus.test.services.QuarkusApplication;
+import io.restassured.response.Response;
 
 @QuarkusScenario
 public class PostgresqlHandlerIT extends CommonTestCases {
@@ -23,4 +30,13 @@ public class PostgresqlHandlerIT extends CommonTestCases {
             .withProperty("app.selected.db", "postgresql")
             // Enable Flyway for Postgresql
             .withProperty("quarkus.flyway.migrate-at-start", "true");
+
+    @Test
+    @Tag("https://github.com/quarkusio/quarkus/issues/50140")
+    @DisabledOnQuarkusVersion(version = "3.27.0.*", reason = "This issue affected native and was fixed in 3.27.1")
+    void manuallyCreatedPool() {
+        Response response = app.given().get("pool/");
+        assertEquals(200, response.statusCode());
+        assertEquals("Total cities: 9", response.body().asString());
+    }
 }
