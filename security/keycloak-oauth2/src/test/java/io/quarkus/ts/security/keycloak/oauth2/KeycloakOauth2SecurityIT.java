4bd04e147661e5f0c27b56d6a2ce50d0a3f00770
Jakub Jedlicka <jjedlick@redhat.com>
2025-11-08 23:32:42 +0100

    Switch Keycloak from dev mode to prod mode
    The updated modules:
    - security/keycloak-oauth2
    - security/keycloak-oidc-client-reactive-extended
    
    (cherry picked from commit 64b42e60e0c68045405f9aae542047a6439beacb)

--- security/keycloak-oauth2/src/test/java/io/quarkus/ts/security/keycloak/oauth2/KeycloakOauth2SecurityIT.java
+++ security/keycloak-oauth2/src/test/java/io/quarkus/ts/security/keycloak/oauth2/KeycloakOauth2SecurityIT.java
@@ -7,19 +7,23 @@ import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_FILE;
 import io.quarkus.test.bootstrap.KeycloakService;
 import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.services.Certificate;
 import io.quarkus.test.services.KeycloakContainer;
 import io.quarkus.test.services.QuarkusApplication;
 
 @QuarkusScenario
 public class KeycloakOauth2SecurityIT extends BaseOauth2SecurityIT {
 
-    @KeycloakContainer(command = { "start-dev", "--import-realm", "--hostname-strict=false" })
+    @KeycloakContainer(runKeycloakInProdMode = true, certificateFormat = Certificate.Format.PEM)
     static KeycloakService keycloak = new KeycloakService(DEFAULT_REALM_FILE, DEFAULT_REALM, DEFAULT_REALM_BASE_PATH);
 
     @QuarkusApplication
     static RestService app = new RestService()
             .withProperty("quarkus.oauth2.introspection-url",
-                    () -> keycloak.getRealmUrl() + "/protocol/openid-connect/token/introspect");
+                    () -> keycloak.getRealmUrl() + "/protocol/openid-connect/token/introspect")
+            .withProperties(() -> keycloak.getTlsProperties())
+            // The Oauth2 not supporting the TLS registry so this needs to be set
+            .withProperty("quarkus.oauth2.ca-cert-file", "${quarkus.tls.keycloak.trust-store.pem.certs}");
 
     @Override
     protected KeycloakService getKeycloak() {
