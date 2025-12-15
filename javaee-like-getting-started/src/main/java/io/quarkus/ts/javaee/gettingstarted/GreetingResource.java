c28cf3ca25cc1ad2197530f51b39f58707952620
George Gastaldi <gegastaldi@gmail.com>
2025-12-15 17:21:43 -0300

    Remove MP Metrics

--- javaee-like-getting-started/src/main/java/io/quarkus/ts/javaee/gettingstarted/GreetingResource.java
+++ javaee-like-getting-started/src/main/java/io/quarkus/ts/javaee/gettingstarted/GreetingResource.java
@@ -5,11 +5,15 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import io.micrometer.core.annotation.Counted;
+
 @Path("/hello")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+
+    @Counted(value = "hello_invocation_count")
     public String hello() {
         return "hello";
     }
