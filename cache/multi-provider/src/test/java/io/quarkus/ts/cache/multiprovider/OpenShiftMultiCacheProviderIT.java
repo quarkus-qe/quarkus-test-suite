3325558f9babcdf82ff93bb3e924abe44b463d40
Jakub Jedlicka <jjedlick@redhat.com>
2025-12-16 15:12:00 +0100

    Add coverage for QUARKUS-6964 (Add support for multiple cache types (backends))

---
+++ cache/multi-provider/src/test/java/io/quarkus/ts/cache/multiprovider/OpenShiftMultiCacheProviderIT.java
@@ -0,0 +1,7 @@
+package io.quarkus.ts.cache.multiprovider;
+
+import io.quarkus.test.scenarios.OpenShiftScenario;
+
+@OpenShiftScenario
+public class OpenShiftMultiCacheProviderIT extends MultiCacheProviderIT {
+}
