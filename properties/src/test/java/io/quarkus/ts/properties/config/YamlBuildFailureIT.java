d18041681bf8d15bf8f3b65e0bf7a37c35d08ed7
Rostislav Svoboda <rosta.svo@gmail.com>
2026-01-06 09:00:15 +0100

    Keep <extensions>true</extensions> in YamlBuildFailureIT to handle quarkus packaging

--- properties/src/test/java/io/quarkus/ts/properties/config/YamlBuildFailureIT.java
+++ properties/src/test/java/io/quarkus/ts/properties/config/YamlBuildFailureIT.java
@@ -3,17 +3,13 @@ package io.quarkus.ts.properties.config;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.nio.file.StandardOpenOption;
-import java.util.stream.Stream;
 
 import org.apache.http.HttpStatus;
-import org.jboss.logging.Logger;
 import org.junit.jupiter.api.Tag;
 
 import io.quarkus.test.bootstrap.QuarkusCliClient;
@@ -25,8 +21,6 @@ import io.quarkus.test.scenarios.TestQuarkusCli;
 @QuarkusScenario
 @Tag("quarkus-cli")
 public class YamlBuildFailureIT {
-    private static final Logger LOG = Logger.getLogger(YamlBuildFailureIT.class);
-    private static final String FOR_REMOVAL = "<extensions>true</extensions>";
 
     @TestQuarkusCli
     public void verify(QuarkusVersionAwareCliClient cliClient) throws IOException {
@@ -34,50 +28,26 @@ public class YamlBuildFailureIT {
                 QuarkusCliClient.CreateApplicationRequest.defaults()
                         .withExtensions("quarkus-config-yaml", "quarkus-rest"));
 
-        Path pom = app.getFileFromApplication("pom.xml").toPath();
-        Path newPom = withoutLine(pom, FOR_REMOVAL);
-        Files.copy(newPom, pom, StandardCopyOption.REPLACE_EXISTING);
+        // Copy template file from test/resources to the app
         Path configFolder = app.getServiceFolder().resolve("src/main/resources/");
-
-        //copy template file from test/resources to the app
         ClassLoader loader = YamlBuildFailureIT.class.getClassLoader();
         Files.copy(loader.getResourceAsStream("application-profile.yml"), configFolder.resolve("application-dev.yml"));
         Files.copy(loader.getResourceAsStream("application-profile.yml"), configFolder.resolve("application-prod.yml"));
 
-        // Make sure, that HTTP endpoint uses the content of the file
+        // Copy GreetingResource file from test/java/org/acme to the app
         Path resourceFile = app.getFileFromApplication("src/main/java/org/acme/", "GreetingResource.java").toPath();
         Path modifiedResourceFile = Paths.get("").resolve("src/test/java/org/acme/GreetingResource.java");
         Files.copy(modifiedResourceFile, resourceFile, StandardCopyOption.REPLACE_EXISTING);
+
+        // Build the app
         QuarkusCliClient.Result result = app.buildOnJvm("--no-tests");
         assertTrue(result.isSuccessful(), "The application didn't build on JVM. Output: " + result.getOutput());
-        // Start using DEV mode;
+        // Start the DEV mode
         app.start();
         app.given()
                 .get("/hello")
                 .then()
                 .statusCode(HttpStatus.SC_OK)
                 .body(is("Hello from modified file"));
-    }
-
-    private static Path withoutLine(Path source, String forRemoval) {
-        Path pom = source.toAbsolutePath();
-        Path temporaryPom = pom.resolveSibling(pom.getFileName() + ".tmp");
-        LOG.info("Removing " + forRemoval + " from " + pom + " using " + temporaryPom);
-        try (Stream<String> lines = Files.lines(pom);
-                BufferedWriter writer = Files.newBufferedWriter(temporaryPom, StandardOpenOption.CREATE_NEW)) {
-            lines
-                    .filter(line -> !line.contains(forRemoval))
-                    .forEach(line -> {
-                        try {
-                            writer.write(line);
-                            writer.newLine();
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    });
-        } catch (IOException | RuntimeException e) {
-            throw new RuntimeException("Failed to remove " + forRemoval + " from " + pom, e);
-        }
-        return temporaryPom;
     }
 }
