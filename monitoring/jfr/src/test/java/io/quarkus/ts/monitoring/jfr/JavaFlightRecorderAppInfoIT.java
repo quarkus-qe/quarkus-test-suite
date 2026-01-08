1babe64da4f5c00d476036776f81a96e90ae0c53
Jakub Jedlicka <jjedlick@redhat.com>
2026-01-08 23:12:53 +0100

    Add coverage for QUARKUS-6552 (Add functionality to capture Quarkus application runtime data using JFR extension)

---
+++ monitoring/jfr/src/test/java/io/quarkus/ts/monitoring/jfr/JavaFlightRecorderAppInfoIT.java
@@ -0,0 +1,19 @@
+package io.quarkus.ts.monitoring.jfr;
+
+import org.junit.jupiter.api.Tag;
+
+import io.quarkus.test.bootstrap.RestService;
+import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.scenarios.annotations.DisabledOnSemeruJdk;
+import io.quarkus.test.services.QuarkusApplication;
+
+@Tag("QUARKUS-6552")
+@QuarkusScenario
+@DisabledOnSemeruJdk(reason = "Semeru don't have full support for JFR yet")
+public class JavaFlightRecorderAppInfoIT extends AbstractJavaFlightRecorderAppInfoIT {
+
+    @QuarkusApplication
+    static final RestService app = new RestService()
+            .withProperty("-XX:StartFlightRecording", "dumponexit=true,filename=" + RECORDING_PATH)
+            .setAutoStart(false);
+}
