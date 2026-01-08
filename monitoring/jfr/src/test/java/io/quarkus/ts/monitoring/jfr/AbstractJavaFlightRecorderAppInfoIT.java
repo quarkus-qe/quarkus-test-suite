9c8e6f8d1f1960b70595b1ac8e1517a6ca91c14c
Jakub Jedlicka <jjedlick@redhat.com>
2026-01-08 23:13:54 +0100

    Add coverage for QUARKUS-6553 (Properly capture Quarkus information for JFR events)
    (cherry picked from commit b28803abc76c4eeec82d4b7182762e56b62086b5)

--- monitoring/jfr/src/test/java/io/quarkus/ts/monitoring/jfr/AbstractJavaFlightRecorderAppInfoIT.java
+++ monitoring/jfr/src/test/java/io/quarkus/ts/monitoring/jfr/AbstractJavaFlightRecorderAppInfoIT.java
@@ -1,5 +1,7 @@
 package io.quarkus.ts.monitoring.jfr;
 
+import static io.quarkus.ts.monitoring.jfr.JfrUtils.RECORDING_PATH;
+import static io.quarkus.ts.monitoring.jfr.JfrUtils.getRecordedEventsByName;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.StringContains.containsString;
@@ -8,28 +10,37 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
-import java.nio.file.Path;
+import java.nio.file.Files;
 import java.util.List;
 
 import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+import io.quarkus.runtime.ImageMode;
 import io.quarkus.test.bootstrap.LookupService;
 import io.quarkus.test.bootstrap.RestService;
 import io.restassured.response.Response;
 
 import jdk.jfr.consumer.RecordedEvent;
-import jdk.jfr.consumer.RecordingFile;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public abstract class AbstractJavaFlightRecorderAppInfoIT {
-
-    public static final Path RECORDING_PATH = Path.of(System.getProperty("java.io.tmpdir"), "jfrRecording.jfr");
 
     @LookupService
     static RestService app;
 
+    @AfterEach
+    public void cleanJfrRecording() throws IOException {
+        Files.deleteIfExists(RECORDING_PATH);
+    }
+
     @Test
+    @Order(1)
     public void checkAppInfo() throws IOException {
         app.start();
 
@@ -47,6 +58,7 @@ public abstract class AbstractJavaFlightRecorderAppInfoIT {
     }
 
     @Test
+    @Order(2)
     public void checkRuntimeInfo() throws IOException {
         app.start();
 
@@ -66,6 +78,7 @@ public abstract class AbstractJavaFlightRecorderAppInfoIT {
     }
 
     @Test
+    @Order(3)
     public void checkExtensionInfo() throws IOException {
         app.start();
         given().get("/hello").then().statusCode(HttpStatus.SC_OK);
@@ -84,10 +97,34 @@ public abstract class AbstractJavaFlightRecorderAppInfoIT {
         }
     }
 
-    private List<RecordedEvent> getRecordedEventsByName(String eventName) throws IOException {
-        return RecordingFile.readAllEvents(RECORDING_PATH).stream()
-                .filter(e -> e.getEventType().getName().equals(eventName))
-                .toList();
+    @Tag("QUARKUS-6553")
+    @Test
+    @Order(4)
+    public void checkRuntimeInfoMultipleProfile() throws IOException {
+        app.withProperty("quarkus.profile", "prod,myProfile");
+        app.start();
+
+        String quarkusVersion = executeGetRequestForInfo("/runtime-info/quarkus-version");
+        String quarkusImageMode = executeGetRequestForInfo("/runtime-info/quarkus-image-mode");
+        String quarkusProfiles = executeGetRequestForInfo("/runtime-info/quarkus-profiles");
+
+        app.stop();
+
+        // Determinate which image mode the record should contain. For native ImageMode.NATIVE_BUILD should not be present.
+        String expectedImageMode = quarkusImageMode.contains("NATIVE") ? ImageMode.NATIVE_RUN.name() : ImageMode.JVM.name();
+
+        // Ensuring that runtime profiles are same as profile set by Quarkus property
+        assertEquals("myProfile,prod", quarkusProfiles, "Quarkus runtime profiles not match the set profiles");
+
+        var appInfoRecordList = getRecordedEventsByName("quarkus.runtime");
+        assertFalse(appInfoRecordList.isEmpty());
+
+        RecordedEvent appInfoRecord = appInfoRecordList.get(0);
+        assertEquals(quarkusVersion, appInfoRecord.getString("version"), "Quarkus version in record not match runtime version");
+        assertEquals(expectedImageMode, appInfoRecord.getString("imageMode"),
+                "Quarkus image mode in record not match runtime image mode");
+        assertThat("Quarkus profiles in record not match runtime profiles", quarkusProfiles,
+                containsString(appInfoRecord.getString("profiles")));
     }
 
     private String executeGetRequestForInfo(String path) {
