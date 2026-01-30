28be5028b872da5ba4119fae4fdcbcd6199ebf51
Troitskii Georgii <gtroitsk@redhat.com>
2026-01-30 14:21:27 +0100

    Make QuickstartIT use 3.20 branch of quickstarts repository
    Temporary disable OpenShiftQuickstartIT due to https://github.com/quarkus-qe/quarkus-test-framework/issues/1817

--- external-applications/src/test/java/io/quarkus/ts/external/applications/OpenShiftQuickstartIT.java
+++ external-applications/src/test/java/io/quarkus/ts/external/applications/OpenShiftQuickstartIT.java
@@ -1,8 +1,11 @@
 package io.quarkus.ts.external.applications;
 
+import org.junit.jupiter.api.Disabled;
+
 import io.quarkus.test.scenarios.OpenShiftScenario;
 import io.quarkus.test.scenarios.annotations.DisabledOnNative;
 
+@Disabled("https://github.com/quarkus-qe/quarkus-test-framework/issues/1817")
 @DisabledOnNative(reason = "Native + s2i not supported")
 @OpenShiftScenario
 public class OpenShiftQuickstartIT extends QuickstartIT {
