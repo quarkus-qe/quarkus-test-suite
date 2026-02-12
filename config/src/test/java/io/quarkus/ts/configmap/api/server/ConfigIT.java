ad818118edea3bc085029a7b9c5b990c790bf769
Troitskii Georgii <gtroitsk@redhat.com>
2026-02-12 19:39:24 +0100

    Remove DisabledOnFipsAndNative annotation implementation from test suite
    DisabledOnFipsAndNative annotation is the part of test framework now
    
    (cherry picked from commit f4421dcbee3b381b7de6ef6ef2433b13c6f23e9d)

--- config/src/test/java/io/quarkus/ts/configmap/api/server/ConfigIT.java
+++ config/src/test/java/io/quarkus/ts/configmap/api/server/ConfigIT.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.scenarios.annotations.DisabledOnFipsAndNative;
 import io.quarkus.test.services.QuarkusApplication;
 
 @DisabledOnFipsAndNative(reason = "https://issues.redhat.com/browse/MANDREL-245")
