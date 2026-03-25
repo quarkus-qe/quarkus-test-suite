7c6fca6064aeebc65801db322cce6dd636900701
Fedor Dudinsky <fdudinsk@redhat.com>
2026-03-25 13:24:02 +0100

    Use the same sample branch for all git-based tests of Quarkus AI
    (cherry picked from commit 94fb5c33ffffe1f7dd643025f139a66e955e545f)

--- ai/langchain4j/src/test/java/io/quarkus/ts/langchain4j/OpenShiftToolsIT.java
+++ ai/langchain4j/src/test/java/io/quarkus/ts/langchain4j/OpenShiftToolsIT.java
@@ -1,6 +1,7 @@
 package io.quarkus.ts.langchain4j;
 
 import static io.quarkus.ts.langchain4j.auxiliary.CommonTools.DEFAULT_ARGS;
+import static io.quarkus.ts.langchain4j.auxiliary.CommonTools.SAMPLE_BRANCH;
 import static io.quarkus.ts.langchain4j.auxiliary.CommonTools.getKey;
 
 import io.quarkus.test.bootstrap.Protocol;
@@ -22,7 +23,7 @@ public class OpenShiftToolsIT extends AbstractToolsIT {
             .withProperty("_ignored", "resource_with_destination::/tmp/playground/|playground/longword.txt")
             .withProperty("working.folder", "/tmp/playground/");
 
-    @GitRepositoryQuarkusApplication(repo = "https://github.com/quarkiverse/quarkus-langchain4j.git", branch = "1.7", contextDir = "samples/mcp-tools", mavenArgs = DEFAULT_ARGS
+    @GitRepositoryQuarkusApplication(repo = "https://github.com/quarkiverse/quarkus-langchain4j.git", branch = SAMPLE_BRANCH, contextDir = "samples/mcp-tools", mavenArgs = DEFAULT_ARGS
             + " -Dsamples -Dplatform-deps -Dquarkus.langchain4j.mcp.filesystem.transport-type=streamable-http")
     static final RestService client = new RestService()
             .withProperty("quarkus.langchain4j.mcp.filesystem.transport-type", "streamable-http")
