6997844ae9801ac0bc97246312c1141c94200a12
Rostislav Svoboda <rosta.svo@gmail.com>
2026-03-30 21:23:49 +0200

    Disable CLI extension test until QuarkusExtensionTest is released
    Due to Use new QuarkusExtensionTest everywhere quarkusio/quarkus#53251 extensions generated with Quarkus 3.34 from registry have different test API than ones in main. The test should be disabled until this change is in release.

--- quarkus-cli/src/test/java/io/quarkus/ts/quarkus/cli/QuarkusCliCreateExtensionIT.java
+++ quarkus-cli/src/test/java/io/quarkus/ts/quarkus/cli/QuarkusCliCreateExtensionIT.java
@@ -2,6 +2,8 @@ package io.quarkus.ts.quarkus.cli;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Disabled;
+
 import io.quarkus.test.bootstrap.QuarkusCliClient;
 import io.quarkus.test.bootstrap.QuarkusCliDefaultService;
 import io.quarkus.test.bootstrap.QuarkusVersionAwareCliClient;
@@ -14,6 +16,8 @@ import io.quarkus.test.scenarios.annotations.DisabledOnNative;
 public class QuarkusCliCreateExtensionIT {
 
     @TestQuarkusCli
+    // TODO enable when Quarkus 3.35 is out
+    @Disabled("https://github.com/quarkusio/quarkus/pull/53251 introduces breaking change in main tests compared to Quarkus release in registry")
     public void shouldCreateAndBuildExtension(QuarkusVersionAwareCliClient cliClient) {
         // Create extension
         QuarkusCliDefaultService app = cliClient.createExtension("extension-abc");
