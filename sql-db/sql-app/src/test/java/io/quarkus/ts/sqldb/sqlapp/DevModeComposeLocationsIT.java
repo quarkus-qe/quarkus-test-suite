a9bb1aebefa97792d433a544a149e945d53a7cc7
Jakub Jedlicka <jjedlick@redhat.com>
2026-04-24 11:51:09 +0200

    Decrease nuber of started Dev mode apps
    Running 5 devmode apps at the same time need lot of resources.
    Running this tests on 8GB system cause some unstability when the system needs some amount of ddle memory.
    For comparison our RHEL8 VM needs using around 300ory and RHEL 9 using 650MB.
    
    The VMs running on the edge of out of memory when running this single test.
    When runnign the full module I belive there is some allocated memory which won't be freed and causing OOM for system.
    
    (cherry picked from commit fd2dbffd90f5937c03e33cc1147b7d1ccbc412e9)

--- sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/DevModeComposeLocationsIT.java
+++ sql-db/sql-app/src/test/java/io/quarkus/ts/sqldb/sqlapp/DevModeComposeLocationsIT.java
@@ -1,5 +1,7 @@
 package io.quarkus.ts.sqldb.sqlapp;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -20,29 +22,35 @@ public class DevModeComposeLocationsIT extends AbstractSqlDatabaseIT {
     @DevModeQuarkusApplication(properties = "postgresql.properties")
     static DevModeQuarkusService dockerComposeDevserviceApp = (DevModeQuarkusService) new DevModeQuarkusService()
             .withProperty("quarkus.compose.devservices.env-variables.IMAGE", "${postgresql.latest.image}")
+            .setAutoStart(false)
             .onPreStart(service -> ((DevModeQuarkusService) service)
                     .copyFile("src/test/resources/postgresql-compose-devservices.yml", "docker-compose-devservice.yml"));
 
     @DevModeQuarkusApplication(properties = "postgresql.properties")
     static DevModeQuarkusService composeDevserviceApp = (DevModeQuarkusService) new DevModeQuarkusService()
             .withProperty("quarkus.compose.devservices.env-variables.IMAGE", "${postgresql.latest.image}")
+            .setAutoStart(false)
             .onPreStart(service -> ((DevModeQuarkusService) service)
                     .copyFile("src/test/resources/postgresql-compose-devservices.yml", "compose-devservice.yml"));
 
     @DevModeQuarkusApplication(properties = "postgresql.properties")
     static DevModeQuarkusService dockerComposeDevserviceYamlApp = (DevModeQuarkusService) new DevModeQuarkusService()
             .withProperty("quarkus.compose.devservices.env-variables.IMAGE", "${postgresql.latest.image}")
+            .setAutoStart(false)
             .onPreStart(service -> ((DevModeQuarkusService) service)
                     .copyFile("src/test/resources/postgresql-compose-devservices.yml", "docker-compose-devservice.yaml"));
 
     @DevModeQuarkusApplication(properties = "postgresql.properties")
     static DevModeQuarkusService suffixedDockerComposeDevserviceApp = (DevModeQuarkusService) new DevModeQuarkusService()
             .withProperty("quarkus.compose.devservices.env-variables.IMAGE", "${postgresql.latest.image}")
+            .setAutoStart(false)
             .onPreStart(service -> ((DevModeQuarkusService) service)
                     .copyFile("src/test/resources/postgresql-compose-devservices.yml", "docker-compose-devservice-foo.yml"));
 
     @Test
-    public void composeDevServicesAreUsed() {
+    public void composeDevServicesAreReused() {
+        startStopDevserviceApps(List.of(dockerComposeDevserviceApp, composeDevserviceApp), true);
+
         // Compose dev service is triggered
         dockerComposeDevservicesApp.logs().assertContains("Compose is running command");
         dockerComposeDevservicesApp.logs().assertContains(System.getProperty("postgresql.latest.image"));
@@ -54,10 +62,27 @@ public class DevModeComposeLocationsIT extends AbstractSqlDatabaseIT {
         composeDevserviceApp.logs().assertContains("Compose Dev Service container found");
         composeDevserviceApp.logs().assertContains(System.getProperty("postgresql.latest.image"));
 
+        startStopDevserviceApps(List.of(dockerComposeDevserviceApp, composeDevserviceApp), false);
+    }
+
+    @Test
+    public void composeDevServicesAreUsed() {
+        startStopDevserviceApps(List.of(dockerComposeDevserviceYamlApp, suffixedDockerComposeDevserviceApp), true);
+
         dockerComposeDevserviceYamlApp.logs().assertContains("Compose Dev Service container found");
         dockerComposeDevserviceYamlApp.logs().assertContains(System.getProperty("postgresql.latest.image"));
 
         suffixedDockerComposeDevserviceApp.logs().assertContains("Compose Dev Service container found");
         suffixedDockerComposeDevserviceApp.logs().assertContains(System.getProperty("postgresql.latest.image"));
+
+        startStopDevserviceApps(List.of(dockerComposeDevserviceYamlApp, suffixedDockerComposeDevserviceApp), false);
+    }
+
+    private void startStopDevserviceApps(List<DevModeQuarkusService> services, boolean start) {
+        if (start) {
+            services.forEach(DevModeQuarkusService::start);
+        } else {
+            services.forEach(DevModeQuarkusService::stop);
+        }
     }
 }
