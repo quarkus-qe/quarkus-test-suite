1f83e36668a5f5e5d69878f80c61b3226abe6bd4
jcarranzan <jcarranz@redhat.com>
2026-05-21 15:41:55 +0200

    Add coverage for QUARKUS-7728 @ServerExceptionMapper

---
+++ http/jakarta-rest-reactive/src/main/java/io/quarkus/ts/http/jakartarest/reactive/exceptions/GenericMapperException.java
@@ -0,0 +1,8 @@
+package io.quarkus.ts.http.jakartarest.reactive.exceptions;
+
+public class GenericMapperException extends RuntimeException {
+
+    public GenericMapperException(String message) {
+        super(message);
+    }
+}
