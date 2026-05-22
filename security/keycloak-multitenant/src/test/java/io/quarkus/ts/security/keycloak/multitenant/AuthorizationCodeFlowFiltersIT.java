10f7e7056293979e5efb1c3ae6092a6ea8ee35e6
Jakub Jedlicka <jjedlick@redhat.com>
2026-05-22 10:19:51 +0200

    Refactor Openshift Keycloak tests as they need to use rhbk image

--- security/keycloak-multitenant/src/test/java/io/quarkus/ts/security/keycloak/multitenant/AuthorizationCodeFlowFiltersIT.java
+++ security/keycloak-multitenant/src/test/java/io/quarkus/ts/security/keycloak/multitenant/AuthorizationCodeFlowFiltersIT.java
@@ -1,7 +1,17 @@
 package io.quarkus.ts.security.keycloak.multitenant;
 
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM;
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_BASE_PATH;
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_FILE;
+
+import io.quarkus.test.bootstrap.KeycloakService;
 import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.services.KeycloakContainer;
 
 @QuarkusScenario
 public class AuthorizationCodeFlowFiltersIT extends BaseAuthorizationCodeFlowFiltersIT {
+
+    @KeycloakContainer(runKeycloakInProdMode = true)
+    static KeycloakService keycloak = new KeycloakService(
+            DEFAULT_REALM_FILE, DEFAULT_REALM, DEFAULT_REALM_BASE_PATH);
 }
