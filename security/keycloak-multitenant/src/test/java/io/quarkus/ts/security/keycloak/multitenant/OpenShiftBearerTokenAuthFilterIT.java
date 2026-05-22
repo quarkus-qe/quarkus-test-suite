13a024970be8e6bf710c0c5f7ff0beaf9e2b8dd0
Jakub Jedlicka <jjedlick@redhat.com>
2026-05-22 10:19:51 +0200

    Refactor Openshift Keycloak tests as they need to use rhbk image

--- security/keycloak-multitenant/src/test/java/io/quarkus/ts/security/keycloak/multitenant/OpenShiftBearerTokenAuthFilterIT.java
+++ security/keycloak-multitenant/src/test/java/io/quarkus/ts/security/keycloak/multitenant/OpenShiftBearerTokenAuthFilterIT.java
@@ -1,7 +1,17 @@
 package io.quarkus.ts.security.keycloak.multitenant;
 
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM;
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_BASE_PATH;
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_FILE;
+
+import io.quarkus.test.bootstrap.KeycloakService;
 import io.quarkus.test.scenarios.OpenShiftScenario;
+import io.quarkus.test.services.KeycloakContainer;
 
 @OpenShiftScenario
-public class OpenShiftBearerTokenAuthFilterIT extends BearerTokenAuthFilterIT {
+public class OpenShiftBearerTokenAuthFilterIT extends BaseBearerTokenAuthFilterIT {
+
+    @KeycloakContainer(runKeycloakInProdMode = true, image = "${rhbk.image}")
+    static KeycloakService keycloak = new KeycloakService(
+            DEFAULT_REALM_FILE, DEFAULT_REALM, DEFAULT_REALM_BASE_PATH);
 }
