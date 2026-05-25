d9c527e39c3665b4c6c2a107ccd6905fdc7d95bd
Fedor Dudinsky <fdudinsk@redhat.com>
2026-05-25 11:53:30 +0200

    Switch AI tests from registry to build strategy
    Runners for ARM jobs are based on x86 and build x86-based images,
    which fail to run or ARM executors

--- ai/langchain4j/src/test/java/io/quarkus/ts/langchain4j/OpenShiftMoviesIT.java
+++ ai/langchain4j/src/test/java/io/quarkus/ts/langchain4j/OpenShiftMoviesIT.java
@@ -3,7 +3,7 @@ package io.quarkus.ts.langchain4j;
 import io.quarkus.test.scenarios.OpenShiftDeploymentStrategy;
 import io.quarkus.test.scenarios.OpenShiftScenario;
 
-@OpenShiftScenario(deployment = OpenShiftDeploymentStrategy.UsingContainerRegistry)
+@OpenShiftScenario(deployment = OpenShiftDeploymentStrategy.Build)
 public class OpenShiftMoviesIT extends MoviesIT {
 
 }
