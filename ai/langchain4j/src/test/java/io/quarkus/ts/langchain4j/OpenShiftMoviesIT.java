34c1204dd8785713838b3b7d88e210185ccc0b6e
Jakub Jedlicka <jjedlick@redhat.com>
2026-06-03 11:36:57 +0200

    Disable OpenshiftMoviesIT for IBM z and P as pgvector container is not available for s390x & ppc64le
    (cherry picked from commit cb6c48d71475b9f409f824896cffdeaa5fe7b4c4)

--- ai/langchain4j/src/test/java/io/quarkus/ts/langchain4j/OpenShiftMoviesIT.java
+++ ai/langchain4j/src/test/java/io/quarkus/ts/langchain4j/OpenShiftMoviesIT.java
@@ -1,8 +1,11 @@
 package io.quarkus.ts.langchain4j;
 
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+
 import io.quarkus.test.scenarios.OpenShiftDeploymentStrategy;
 import io.quarkus.test.scenarios.OpenShiftScenario;
 
+@DisabledIfSystemProperty(named = "ts.ibm-z-p.missing.services.excludes", matches = "true", disabledReason = "pgvector container not available on s390x & ppc64le.")
 @OpenShiftScenario(deployment = OpenShiftDeploymentStrategy.Build)
 public class OpenShiftMoviesIT extends MoviesIT {
 
