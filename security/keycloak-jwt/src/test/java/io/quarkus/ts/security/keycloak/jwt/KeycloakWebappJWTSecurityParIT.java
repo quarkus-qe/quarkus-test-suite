81efc48f9f988261acc404146052cf17af9bfd3e
Jakub Jedlicka <jjedlick@redhat.com>
2026-06-26 10:26:13 +0200

    Add coverage for QUARKUS-7174 ( Add support for OAuth 2.0 Pushed Authorization Requests to OIDC extension)

---
+++ security/keycloak-jwt/src/test/java/io/quarkus/ts/security/keycloak/jwt/KeycloakWebappJWTSecurityParIT.java
@@ -0,0 +1,49 @@
+package io.quarkus.ts.security.keycloak.jwt;
+
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM;
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_BASE_PATH;
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_FILE;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.bootstrap.KeycloakService;
+import io.quarkus.test.bootstrap.RestService;
+import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.services.KeycloakContainer;
+import io.quarkus.test.services.QuarkusApplication;
+
+@Tag("QUARKUS-7174")
+@QuarkusScenario
+public class KeycloakWebappJWTSecurityParIT extends BaseWebappJWTSecurityParIT {
+
+    @KeycloakContainer(runKeycloakInProdMode = true)
+    static KeycloakService keycloak = new KeycloakService(DEFAULT_REALM_FILE, DEFAULT_REALM, DEFAULT_REALM_BASE_PATH);
+
+    @QuarkusApplication
+    static RestService app = new RestService()
+            .withProperty("quarkus.oidc.auth-server-url", () -> keycloak.getRealmUrl())
+            .withProperty("quarkus.oidc.client-id", CLIENT_ID_DEFAULT)
+            .withProperty("quarkus.oidc.authentication.par.enabled", "true")
+            .withProperties(() -> keycloak.getTlsProperties());
+
+    @Override
+    protected KeycloakService getKeycloak() {
+        return keycloak;
+    }
+
+    @Override
+    protected RestService getApp() {
+        return app;
+    }
+
+    @Test
+    public void testWrongParPath() throws Exception {
+        app.stop();
+        app.withProperty("quarkus.oidc.authentication.par.path",
+                () -> keycloak.getRealmUrl() + "/protocol/openid-connect/ext/par/requests");
+        app.start();
+
+        goToAndExpectInternalError("/secured/user");
+    }
+}
---
+++ security/keycloak-jwt/src/test/java/io/quarkus/ts/security/keycloak/jwt/OpenShiftRhSsoWebappJWTSecurityParIT.java
@@ -0,0 +1,40 @@
+package io.quarkus.ts.security.keycloak.jwt;
+
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM;
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_BASE_PATH;
+import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_FILE;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+
+import io.quarkus.test.bootstrap.KeycloakService;
+import io.quarkus.test.bootstrap.RestService;
+import io.quarkus.test.scenarios.OpenShiftScenario;
+import io.quarkus.test.services.KeycloakContainer;
+import io.quarkus.test.services.QuarkusApplication;
+
+@Tag("QUARKUS-7174")
+@OpenShiftScenario
+@EnabledIfSystemProperty(named = "ts.redhat.registry.enabled", matches = "true")
+public class OpenShiftRhSsoWebappJWTSecurityParIT extends BaseWebappJWTSecurityParIT {
+
+    @KeycloakContainer(runKeycloakInProdMode = true, image = "${rhbk.image}")
+    static KeycloakService keycloak = new KeycloakService(DEFAULT_REALM_FILE, DEFAULT_REALM, DEFAULT_REALM_BASE_PATH);
+
+    @QuarkusApplication
+    static RestService app = new RestService()
+            .withProperty("quarkus.oidc.auth-server-url", keycloak::getRealmUrl)
+            .withProperty("quarkus.oidc.client-id", CLIENT_ID_DEFAULT)
+            .withProperty("quarkus.oidc.authentication.par.enabled", "true")
+            .withProperties(keycloak::getTlsProperties);
+
+    @Override
+    protected KeycloakService getKeycloak() {
+        return keycloak;
+    }
+
+    @Override
+    protected RestService getApp() {
+        return app;
+    }
+}
